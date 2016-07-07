1687ef3952d758fce75c92151ef6dbd8802391c3
James Strachan <james.strachan@gmail.com>
2016-07-07 09:08:48 +0100

    lets limit the projects we try to update the parent pom

--- release.groovy
+++ release.groovy
@@ -45,9 +45,7 @@ def mergePullRequest(prId){
 
 def pushDependencyUpdates(newVersion){
   def parentPomProjects = ['fabric8-quickstarts/funktion-nodejs-example','fabric8-quickstarts/funktion-kotlin-example',
-                           'fabric8-quickstarts/funktion-java-example','fabric8-quickstarts/funktion-groovy-example',
-                           'fabric8-quickstarts/spring-boot-camel','fabric8-quickstarts/spring-boot-camel-xml',
-                           'fabric8-quickstarts/spring-boot-ribbon']
+                           'fabric8-quickstarts/funktion-java-example','fabric8-quickstarts/funktion-groovy-example']
   pushParentPomVersionChangePR{
     projects = parentPomProjects
     version = newVersion
