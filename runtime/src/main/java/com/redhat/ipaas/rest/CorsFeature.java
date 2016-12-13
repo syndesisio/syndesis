d8add1c0d459d1e86cb84f2f660d8475885e5d6f
Jimmi Dyson <jimmidyson@gmail.com>
2016-12-13 20:42:03 +0000

    Add open CORS filter - let anyone in!

---
+++ runtime/src/main/java/com/redhat/ipaas/rest/CorsFeature.java
@@ -0,0 +1,20 @@
+package com.redhat.ipaas.rest;
+
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
+
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CorsFeature implements Feature {
+
+    @Override
+    public boolean configure(FeatureContext context) {
+        CorsFilter corsFilter = new CorsFilter();
+        corsFilter.getAllowedOrigins().add("*");
+        context.register(corsFilter);
+        return true;
+    }
+
+}
