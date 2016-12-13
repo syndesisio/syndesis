b4d2a2d29666ad3008bedd6fe209a48cc15e2f3b
Jimmi Dyson <jimmidyson@gmail.com>
2016-12-13 20:39:21 +0000

    Add configured Jackson2 ObjectMapper

---
+++ runtime/src/main/java/com/redhat/ipaas/rest/JacksonContextResolver.java
@@ -0,0 +1,27 @@
+package com.redhat.ipaas.rest;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+// Customized {@code ContextResolver} implementation to pass ObjectMapper to use
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class JacksonContextResolver implements ContextResolver<ObjectMapper> {
+    private ObjectMapper objectMapper;
+
+    public JacksonContextResolver() throws Exception {
+        this.objectMapper = new ObjectMapper().
+            registerModule(new Jdk8Module()).
+            setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+    }
+
+    public ObjectMapper getContext(Class<?> objectType) {
+        return objectMapper;
+    }
+}
