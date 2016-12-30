2e3a8d9beef963dd2ebd1fff503710ace0a40d6c
Jimmi Dyson <jimmidyson@gmail.com>
2016-12-30 00:14:47 +0000

    Add preloading all modules router strategy

--- src/app/approuting/approuting.module.ts
+++ src/app/approuting/approuting.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -8,7 +8,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [
     RouterModule
