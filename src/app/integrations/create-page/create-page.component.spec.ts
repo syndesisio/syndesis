ba9ee0f7e5ae0a71504a6d70c4baf6dd2b04047f
Stan Lewis <gashcrumb@gmail.com>
2017-02-09 15:09:54 -0500

    Fix tests

--- src/app/integrations/create-page/create-page.component.spec.ts
+++ src/app/integrations/create-page/create-page.component.spec.ts
@@ -1,5 +1,18 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockBackend } from '@angular/http/testing';
+import { RequestOptions, BaseRequestOptions, Http } from '@angular/http';
+import { RestangularModule } from 'ng2-restangular';
+
+import { RouterModule } from '@angular/router';
+import { IPaaSCommonModule } from '../../common/common.module';
+import { FlowViewComponent } from './flow-view/flow-view.component';
+import { ConnectionsListComponent } from '../../connections/list/list.component';
+import { ConnectionsListToolbarComponent } from '../../connections/list-toolbar/list-toolbar.component';
+import { StoreModule } from '../../store/store.module';
 
 import { IntegrationsCreatePage } from './create-page.component';
 
@@ -9,7 +22,32 @@ describe('IntegrationsCreateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [IntegrationsCreatePage],
+      imports: [
+        CommonModule,
+        FormsModule,
+        IPaaSCommonModule, 
+        StoreModule,
+        RouterTestingModule.withRoutes([]), 
+        RestangularModule.forRoot()
+      ],
+      declarations: [
+        IntegrationsCreatePage,
+        ConnectionsListComponent,
+        ConnectionsListToolbarComponent,
+        FlowViewComponent
+      ],
+      providers: [
+        MockBackend,
+        { 
+          provide: RequestOptions, 
+          useClass: BaseRequestOptions 
+        },
+        {
+          provide: Http, useFactory: (backend, options) => {
+            return new Http(backend, options);
+          }, deps: [MockBackend, RequestOptions],
+        },
+      ],      
     })
       .compileComponents();
   }));
--- src/app/integrations/create-page/create-page.component.ts
+++ src/app/integrations/create-page/create-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router'
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
