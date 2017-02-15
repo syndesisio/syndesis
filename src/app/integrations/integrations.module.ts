5a28993f1b1375a84b42f491b620430df2cc904b
kahboom <rachelyordan@gmail.com>
2017-02-15 08:55:12 -0500

    Rm .forRoot() from import

--- src/app/integrations/integrations.module.ts
+++ src/app/integrations/integrations.module.ts
@@ -41,7 +41,7 @@ const routes: Routes = [
     ConnectionsModule,
     TabsModule,
     IPaaSCommonModule,
-    CollapseModule.forRoot(),
+    CollapseModule,
   ],
   declarations: [
     IntegrationsCreatePage,
