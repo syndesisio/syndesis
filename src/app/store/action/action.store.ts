c862095b025576340b236f7790ebf9722c6d5640
Stan Lewis <gashcrumb@gmail.com>
2017-03-07 10:59:59 -0500

    Fix obvious typo that has nothing to do with search/replace

--- src/app/store/action/action.store.ts
+++ src/app/store/action/action.store.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActionService } from './Action.service';
+import { ActionService } from './action.service';
 import { Actions, Action } from '../../model';
 
 import { AbstractStore } from '../entity/entity.store';
