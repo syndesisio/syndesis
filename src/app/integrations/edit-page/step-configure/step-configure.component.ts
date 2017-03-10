523caec01ba2b81a18ef8ce4b3c4d0ee0790ea66
kahboom <rachelyordan@gmail.com>
2017-03-10 01:00:12 -0500

    Res lint errors

--- src/app/integrations/edit-page/step-configure/step-configure.component.ts
+++ src/app/integrations/edit-page/step-configure/step-configure.component.ts
@@ -27,8 +27,6 @@ export class IntegrationsStepConfigureComponent implements OnInit, OnDestroy {
   formGroup: FormGroup;
   formConfig: any;
 
-  public html:string = `<span class="btn btn-danger">Never trust not sanitized HTML!!!</span>`;
-
   constructor(
     private currentFlow: CurrentFlow,
     private route: ActivatedRoute,
