f97604d65fc1d7bd2d4b2db7f4a77e779c7c58c6
kahboom <rachelyordan@gmail.com>
2017-03-09 20:43:42 -0500

    Add SCSS for step configure and select components

--- src/app/integrations/edit-page/step-configure/step-configure.component.ts
+++ src/app/integrations/edit-page/step-configure/step-configure.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ipaas-integrations-step-configure',
   templateUrl: './step-configure.component.html',
+  styleUrls: ['./step-configure.component.scss'],
 })
 export class IntegrationsStepConfigureComponent implements OnInit {
   constructor() { }
