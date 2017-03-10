7faf3d66e80adf386e79f9cb857e87e2eef9aaa2
Stan Lewis <gashcrumb@gmail.com>
2017-03-09 23:07:42 -0500

    Implement the add step and configure step pages

--- src/app/integrations/edit-page/step-select/step-select.component.ts
+++ src/app/integrations/edit-page/step-select/step-select.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+import { CurrentFlow, FlowEvent } from '../current-flow.service';
+import { Step, Steps, TypeFactory } from '../../../model';
 
 @Component({
   selector: 'ipaas-integrations-step-select',
@@ -6,7 +11,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./step-select.component.scss'],
 })
 export class IntegrationsStepSelectComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit() { }
+  // TODO not this here
+  steps: Step[] = [
+    {
+      id: undefined,
+      connection: undefined,
+      action: undefined,
+      stepKind: 'log',
+      configuredProperties: JSON.stringify({
+        message: {
+          type: 'string',
+          displayName: 'Log Message',
+          required: true,
+        },
+        loggingLevel: {
+          type: 'string',
+          displayName: 'Level',
+          required: true,
+        },
+      }),
+    }, {
+      id: undefined,
+      connection: undefined,
+      action: undefined,
+      stepKind: 'datamapper',
+      configuredProperties: JSON.stringify({
+
+      }),
+    },
+  ];
+
+  routeSubscription: Subscription;
+  position: number;
+
+  constructor(
+    private currentFlow: CurrentFlow,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  cancel() {
+    this.router.navigate(['integrations']);
+  }
+
+  goBack() {
+    this.router.navigate(['save-or-add-step'], { relativeTo: this.route.parent });
+  }
+
+  getName(step: Step) {
+    switch (step.stepKind) {
+      case 'log':
+        return 'Log';
+      case 'datamapper':
+        return 'Data Mapper';
+    }
+  }
+
+  getDescription(step: Step) {
+    switch (step.stepKind) {
+      case 'log':
+        return 'Sends a message to the integration\'s log';
+      case 'datamapper':
+        return 'Map fields from the input type to the output type';
+
+    }
+  }
+
+  isSelected(step: Step) {
+    const _step = this.currentFlow.getStep(this.position);
+    return _step && step.stepKind === _step.stepKind;
+  }
+
+  onSelect(step: Step) {
+    // TODO hack to use the existing step in the flow if it matches
+    const _step = this.currentFlow.getStep(this.position);
+    if (_step && _step.stepKind === step.stepKind) {
+      step = _step;
+    }
+    this.currentFlow.events.emit({
+      kind: 'integration-set-step',
+      position: this.position,
+      step: step,
+      onSave: () => {
+        this.router.navigate(['step-configure', this.position], { relativeTo: this.route.parent });
+      },
+    });
+  }
+
+  ngOnInit() {
+    this.routeSubscription = this.route.params.pluck<Params, string>('position')
+      .map((position: string) => {
+        this.position = Number.parseInt(position);
+        this.currentFlow.events.emit({
+          kind: 'integration-step-select',
+          position: this.position,
+        });
+      })
+      .subscribe();
+   }
 }
