1df49a40be09f1190f3942377ff8725d967d8125
Ioannis Canellos <iocanel@gmail.com>
2017-03-11 16:33:24 +0200

    Bump kubernetes and arquillian version. Use shell scripts for setup and teardown of the test suite.

---
+++ src/test/resources/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+oc update -f https://raw.githubusercontent.com/redhat-ipaas/openshift-templates/master/serviceaccount-as-oauthclient-single-tenant.yml || oc create -f https://raw.githubusercontent.com/redhat-ipaas/openshift-templates/master/serviceaccount-as-oauthclient-single-tenant.yml
+oc update -f https://raw.githubusercontent.com/redhat-ipaas/openshift-templates/master/redhat-ipaas-dev-single-tenant.yml || oc create -f https://raw.githubusercontent.com/redhat-ipaas/openshift-templates/master/redhat-ipaas-dev-single-tenant.yml
+oc new-app redhat-ipaas-dev-single-tenant \
+    -p ROUTE_HOSTNAME=ipaas-testing.b6ff.rh-idev.openshiftapps.com \
+    -p OPENSHIFT_MASTER=$(oc whoami --show-server) \
+    -p OPENSHIFT_OAUTH_CLIENT_ID=system:serviceaccount:$(oc project -q):ipaas-oauth-client \
+    -p OPENSHIFT_OAUTH_CLIENT_SECRET=$(oc sa get-token ipaas-oauth-client) \
+    -p OPENSHIFT_OAUTH_DEFAULT_SCOPES="user:info user:check-access role:edit:$(oc project -q):! role:system:build-strategy-source:$(oc project -q)" \
+    -p GITHUB_OAUTH_CLIENT_ID="iocanel" \
+    -p GITHUB_OAUTH_CLIENT_SECRET="changeme"
---
+++ src/test/resources/teardown.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+oc process redhat-ipaas-dev-single-tenant \
+    ROUTE_HOSTNAME=ipaas-testing.b6ff.rh-idev.openshiftapps.com \
+    OPENSHIFT_MASTER=$(oc whoami --show-server) \
+    OPENSHIFT_OAUTH_CLIENT_ID=system:serviceaccount:$(oc project -q):ipaas-oauth-client \
+    OPENSHIFT_OAUTH_CLIENT_SECRET=$(oc sa get-token ipaas-oauth-client) \
+    OPENSHIFT_OAUTH_DEFAULT_SCOPES="user:info user:check-access role:edit:$(oc project -q):! role:system:build-strategy-source:$(oc project -q)" \
+    GITHUB_OAUTH_CLIENT_ID="iocanel" \
+    GITHUB_OAUTH_CLIENT_SECRET="changeme" | oc delete -f -
