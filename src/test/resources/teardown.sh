0f96313fffa1745bc0ab5e4abc6850b8a3b48105
Ioannis Canellos <iocanel@gmail.com>
2017-03-12 21:23:54 +0200

    Fix namespace handling.

--- src/test/resources/teardown.sh
+++ src/test/resources/teardown.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Removing IPaaS from ${KUBERNETES_NAMESPACE}"
-oc project ${KUBERNETES_NAMESPACE}
 oc process redhat-ipaas-dev-single-tenant  \
     ROUTE_HOSTNAME=ipaas-testing.b6ff.rh-idev.openshiftapps.com \
     OPENSHIFT_MASTER=$(oc whoami --show-server) \
@@ -9,4 +8,4 @@ oc process redhat-ipaas-dev-single-tenant  \
     OPENSHIFT_OAUTH_CLIENT_SECRET=$(oc sa get-token ipaas-oauth-client) \
     OPENSHIFT_OAUTH_DEFAULT_SCOPES="user:info user:check-access role:edit:$(oc project -q):! role:system:build-strategy-source:$(oc project -q)" \
     GITHUB_OAUTH_CLIENT_ID="iocanel" \
-    GITHUB_OAUTH_CLIENT_SECRET="changeme" | oc delete -f -
+    GITHUB_OAUTH_CLIENT_SECRET="changeme" -n ${KUBERNETES_NAMESPACE}  | oc delete -f - -n ${KUBERNETES_NAMESPACE}
