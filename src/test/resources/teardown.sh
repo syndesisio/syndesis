47d3dd481978c963a01c967db7f8289009890fc0
Ioannis Canellos <iocanel@gmail.com>
2017-03-16 13:03:19 +0200

    Fix namespace handling on setup.sh and teardown.sh

--- src/test/resources/teardown.sh
+++ src/test/resources/teardown.sh
@@ -5,7 +5,7 @@ oc process redhat-ipaas-dev-single-tenant  \
     ROUTE_HOSTNAME=ipaas-testing.b6ff.rh-idev.openshiftapps.com \
     OPENSHIFT_MASTER=$(oc whoami --show-server) \
     OPENSHIFT_OAUTH_CLIENT_ID=system:serviceaccount:$(oc project -q):ipaas-oauth-client \
-    OPENSHIFT_OAUTH_CLIENT_SECRET=$(oc sa get-token ipaas-oauth-client) \
+    OPENSHIFT_OAUTH_CLIENT_SECRET=$(oc sa get-token ipaas-oauth-client -n ${KUBERNETES_NAMESPACE}) \
     OPENSHIFT_OAUTH_DEFAULT_SCOPES="user:info user:check-access role:edit:$(oc project -q):! role:system:build-strategy-source:$(oc project -q)" \
     GITHUB_OAUTH_CLIENT_ID="iocanel" \
     GITHUB_OAUTH_CLIENT_SECRET="changeme" -n ${KUBERNETES_NAMESPACE}  | oc delete -f - -n ${KUBERNETES_NAMESPACE}
