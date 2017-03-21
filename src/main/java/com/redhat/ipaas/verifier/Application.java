01df2ad9accadb3f296568e6b3a259b18c95caf4
Roland Huß <roland@ro14nd.de>
2017-03-21 20:49:58 +0100

    Initial commit

---
+++ src/main/java/com/redhat/ipaas/verifier/Application.java
@@ -0,0 +1,16 @@
+package com.redhat.ipaas.verifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author roland
+ * @since 20/03/2017
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/redhat/ipaas/verifier/VerifierResult.java
@@ -0,0 +1,17 @@
+package com.redhat.ipaas.verifier;
+
+/**
+ * @author roland
+ * @since 20/03/2017
+ */
+public class VerifierResult {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public VerifierResult(String result) {
+        this.result = result;
+    }
+}
