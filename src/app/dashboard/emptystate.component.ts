49d1953a2b98a4aa1ff3fada9e219aaba4efc047
kahboom <rachelyordan@gmail.com>
2017-03-28 12:40:09 -0400

    re #300 Move Connections List directive to empty state component

--- src/app/dashboard/emptystate.component.ts
+++ src/app/dashboard/emptystate.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { log, getCategory } from '../logging';
+
+import { Connection, Connections, Integration, Integrations } from '../model';
+import { ConnectionStore } from '../store/connection/connection.store';
+import { IntegrationStore } from '../store/integration/integration.store';
+
+const category = getCategory('Dashboard');
 
 @Component({
   selector: 'ipaas-dashboard-empty-state',
   templateUrl: './emptystate.component.html',
   styleUrls: ['./emptystate.component.scss'],
 })
-export class EmptyStateComponent {
+export class EmptyStateComponent implements OnInit {
+  connections: Observable<Connections>;
+  @Input() loading: boolean;
+  selectedId = undefined;
+  truncateLimit = 80;
+  truncateTrail = '…';
+
+  constructor(private connectionStore: ConnectionStore) {
+    this.connections = this.connectionStore.list;
+  }
+
+  onSelect(item) {
+    log.debugc(() => 'Selected item (list): ' + item.name, category);
+  }
+
+  isSelected(item) {
+    return item.id === this.selectedId;
+  }
+
+  ngOnInit() {
+    this.connectionStore.loadAll();
+  }
 }
