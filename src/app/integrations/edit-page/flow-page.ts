dd6dcf824ba504654fb4dbd8968f761b2c5e56bc
Stan Lewis <gashcrumb@gmail.com>
2017-04-10 15:43:16 -0400

    Fix #405 and also simplify the buttons on most views

--- src/app/integrations/edit-page/flow-page.ts
+++ src/app/integrations/edit-page/flow-page.ts
@@ -2,10 +2,12 @@ import { OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CurrentFlow, FlowEvent } from './current-flow.service';
+import { Integration, Step, TypeFactory } from '../../model';
 
 export abstract class FlowPage implements OnDestroy {
 
   flowSubscription: Subscription;
+  errorMessage: any = undefined;
 
   constructor(
     public currentFlow: CurrentFlow,
@@ -28,6 +30,38 @@ export abstract class FlowPage implements OnDestroy {
   handleFlowEvent(event: FlowEvent) {
     /* no-op */
   }
+
+  doSave() {
+    if (!this.currentFlow.integration.name || this.currentFlow.integration.name === '') {
+      this.router.navigate(['integration-basics'], { relativeTo: this.route.parent });
+      return;
+    }
+    const router = this.router;
+    this.currentFlow.events.emit({
+      kind: 'integration-save',
+      action: (i: Integration) => {
+        router.navigate(['/integrations']);
+      },
+      error: (error) => {
+        setTimeout(() => {
+          this.errorMessage = error;
+        }, 10);
+      },
+    });
+  }
+
+  save() {
+    if (!this.currentFlow.integration.desiredStatus) {
+      this.currentFlow.integration.desiredStatus = 'Draft';
+    }
+    this.doSave();
+  }
+
+  publish() {
+    this.currentFlow.integration.desiredStatus = 'Activated';
+    this.doSave();
+  }
+
 
   ngOnDestroy() {
     this.flowSubscription.unsubscribe();
