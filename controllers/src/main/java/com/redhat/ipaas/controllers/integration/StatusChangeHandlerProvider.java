babe11545be73c04178d9914ff8ada8ed80ea517
Roland Huß <roland@ro14nd.de>
2017-04-19 12:52:50 +0200

    Re-Add accidentally removed package statement

--- controllers/src/main/java/com/redhat/ipaas/controllers/integration/StatusChangeHandlerProvider.java
+++ controllers/src/main/java/com/redhat/ipaas/controllers/integration/StatusChangeHandlerProvider.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.redhat.ipaas.controllers.integration;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
