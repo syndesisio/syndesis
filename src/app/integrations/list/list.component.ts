5c09702c2541bfaaff14673b0a11ba4572bc1e15
kahboom <rachelyordan@gmail.com>
2017-04-20 17:32:40 -0400

    Fix lint error

--- src/app/integrations/list/list.component.ts
+++ src/app/integrations/list/list.component.ts
@@ -29,7 +29,7 @@ export class IntegrationsListComponent {
   constructor(public store: IntegrationStore,
               public toasterService: ToasterService,
               public route: ActivatedRoute,
-              public router: Router,) {
+              public router: Router) {
 
   }
 
