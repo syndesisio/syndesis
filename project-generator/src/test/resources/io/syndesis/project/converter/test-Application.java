2093264f42807b842370dd6686e684865f127ff3
Jimmi Dyson <jimmidyson@gmail.com>
2017-05-09 13:31:49 +0100

    fix(project-generator): Fix up package name in test for generated project

--- project-generator/src/test/resources/io/syndesis/project/converter/test-Application.java
+++ project-generator/src/test/resources/io/syndesis/project/converter/test-Application.java
@@ -1,4 +1,4 @@
-package io.syndesis.project.converter;
+package io.syndesis.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
