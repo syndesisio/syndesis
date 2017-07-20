453ce7bc69fb6c2eed4dd7e94579d2feee01211c
Stan Lewis <gashcrumb@gmail.com>
2017-07-20 13:29:44 -0400

    feat(settings): Stub out the settings module
    relates to #546

---
+++ src/app/settings/oauth-clients/oauth-clients.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oauth-clients',
+  templateUrl: 'oauth-clients.component.html',
+})
+export class OAuthClientsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
