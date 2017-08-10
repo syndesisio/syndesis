a9e0e6f4071fb56b1f43ed0f4692dde745ff60c8
Stan Lewis <gashcrumb@gmail.com>
2017-08-10 13:10:02 -0400

    fix(integrations): filter out null steps, delete filterExpression from step

--- src/app/integrations/edit-page/current-flow.service.ts
+++ src/app/integrations/edit-page/current-flow.service.ts
@@ -454,10 +454,12 @@ export class CurrentFlow {
   set integration(i: Integration) {
     this._integration = <Integration>i;
     if (i && i.steps && i.steps.length) {
+      i.steps = i.steps.filter((step) => step !== null);
       i.steps.forEach((step) => {
         if ('filterExpression' in step) {
           step.stepKind = 'rule-filter';
           step.name = 'Rule Filter';
+          delete step['filterExpression'];
         }
       });
     }
