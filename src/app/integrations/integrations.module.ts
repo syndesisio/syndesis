9635d491eb89a38d59c6ff1be65bf43a3ce0a628
Antonin Stefanutti <antonin@stefanutti.fr>
2017-08-17 10:17:50 +0200

    refactor(integrations): Remove unused integrations pipe

--- src/app/integrations/integrations.module.ts
+++ src/app/integrations/integrations.module.ts
@@ -28,7 +28,6 @@ import { BasicFilterComponent } from './edit-page/step-configure/filter-steps/ba
 import { ListActionsComponent } from './edit-page/list-actions/list-actions.component';
 import { CancelAddStepComponent } from './edit-page/common/cancel-add-step.component';
 import { IntegrationsListPage } from './list-page/list-page.component';
-import { IntegrationsFilterPipe } from './integrations-filter.pipe';
 import { FlowViewComponent } from './edit-page/flow-view/flow-view.component';
 import { FlowViewStepComponent } from './edit-page/flow-view/flow-view-step.component';
 import { CurrentFlow } from './edit-page/current-flow.service';
@@ -95,7 +94,6 @@ const routes: Routes = [
     IntegrationsConfigureActionComponent,
     IntegrationsEditPage,
     IntegrationBasicsComponent,
-    IntegrationsFilterPipe,
     IntegrationsSelectConnectionComponent,
     IntegrationsSaveOrAddStepComponent,
     IntegrationsStepSelectComponent,
