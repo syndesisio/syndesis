81788fe8910fe701031e90c41ab9a1118c07c353
Alexandre Kieling <abrianik@redhat.com>
2017-08-25 19:24:11 -0300

    feat(editable-*.component): add support to validation in editable-text component and editable-textar
    fix #823

--- src/app/common/editable/editable-tags.component.ts
+++ src/app/common/editable/editable-tags.component.ts
@@ -13,13 +13,14 @@ import { EditableComponent } from './editable.component';
           <span class="label label-primary">{{tag}}</span>
         </ng-container>
       </ng-container>
-      <button type="button" class="btn btn-link" (click)="startEditing()">
-        <i class="fa fa-pencil" aria-hidden="true"></i>
+      <button type="button" class="btn btn-link" (click)="editing = true">
+        <i class="fa fa-pencil" aria-hidden="true" title="Click to edit"></i>
       </button>
     </ng-container>
     <ng-container *ngIf="editing">
       <div class="form-group">
-        <tag-input [(ngModel)]="tempValue"
+        <tag-input #tagInput
+                   [ngModel]="value"
                    name="tags"
                    data-id="tags"
                    theme="bootstrap"
@@ -28,7 +29,7 @@ import { EditableComponent } from './editable.component';
                    [ripple]="false"
                    [inputId]="tagsInput"></tag-input>
       </div>
-      <button class="btn btn-primary" (click)="save()">Save</button>
+      <button class="btn btn-primary" (click)="submit(tagInput.value)">Save</button>
       <button class="btn btn-default" (click)="cancel()">Cancel</button>
     </ng-container>
   `,
--- src/app/common/editable/editable-text.component.ts
+++ src/app/common/editable/editable-text.component.ts
@@ -11,18 +11,26 @@ import { EditableComponent } from './editable.component';
       <ng-container *ngIf="value">
         {{ value }}
       </ng-container>
-      <button type="button" class="btn btn-link" (click)="startEditing()">
-        <i class="fa fa-pencil" aria-hidden="true"></i>
+      <button type="button" class="btn btn-link" (click)="editing = true">
+        <i class="fa fa-pencil" aria-hidden="true" title="Click to edit"></i>
       </button>
     </ng-container>
     <ng-container *ngIf="editing">
-      <div class="form-group">
-        <input type="text" class="form-control" [(ngModel)]="tempValue">
+      <div class="form-group" [ngClass]="{'has-error': errorMessage}">
+        <input #textInput type="text" class="form-control" [ngModel]="value">
+        <span class="help-block" *ngIf="errorMessage">{{errorMessage}}</span>
       </div>
-      <button class="btn btn-primary" (click)="save()">Save</button>
+      <button class="btn btn-primary" (click)="submit(textInput.value)">Save</button>
       <button class="btn btn-default" (click)="cancel()">Cancel</button>
     </ng-container>
   `,
+  styles: [`
+    input {
+      font-size: inherit;
+      height: inherit;
+      line-height: inherit;
+    }
+  `],
 })
 export class EditableTextComponent extends EditableComponent {
 }
