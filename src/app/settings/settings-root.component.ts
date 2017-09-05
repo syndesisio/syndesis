3f36219b264a28939a64fc92c45ca9ef11e77802
Antonin Stefanutti <antonin@stefanutti.fr>
2017-09-05 11:56:05 +0200

    refactor(settings): Fixed settings tabs

--- src/app/settings/settings-root.component.ts
+++ src/app/settings/settings-root.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'syndesis-settings-root',
   templateUrl: 'settings-root.component.html',
+  styleUrls: ['./settings-root.component.scss'],
 })
 export class SettingsRootComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router) {}
