4813863ee8d367512967d73eb0988a2db2ef6d94
Zoran Regvart <zregvart@apache.org>
2017-09-13 11:53:33 +0200

    chore: use correct deploy trigger
    The `deploy` is a Maven property that activates two Maven profiles
    `flash` and `deploy` so this can be simpler.

--- tools/minishift-build/rest.sh
+++ tools/minishift-build/rest.sh
@@ -3,5 +3,5 @@
 . $(dirname `realpath $0`)/vars.sh
 
 prepare_dir syndesis-rest
-./mvnw clean install -Pflash,deploy -Dfabric8.mode=kubernetes
+./mvnw clean install -Ddeploy -Dfabric8.mode=kubernetes
 oc delete pod $(pod rest)
