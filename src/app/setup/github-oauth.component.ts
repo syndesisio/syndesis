159b4a1dfab33a656a26beb530b2167ea31948ae
kahboom <rachelyordan@gmail.com>
2017-09-22 15:30:38 +0100

    Minor changes

--- src/app/setup/github-oauth.component.ts
+++ src/app/setup/github-oauth.component.ts
@@ -1,14 +1,9 @@
 import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 
-/**
- * The GitHub account model
- */
-//import { GitHubAccount } from '../model';
 import { Observable } from 'rxjs/Observable';
 
 export interface OAuthAppListItem {
   expanded: boolean;
-  //account: GitHubAccount;
 }
 
 @Component({
@@ -19,9 +14,10 @@ export interface OAuthAppListItem {
 export class GitHubOAuthSetupComponent implements OnInit {
 
   loading: Observable<boolean>;
-  isLoading = true;
 
   constructor(public detector: ChangeDetectorRef) {}
+
+  connectGitHub() {}
 
   /**
    * Returns whether or not this item has stored credentials
