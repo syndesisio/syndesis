7d719e0fa7bd2f1bb5fb0ef9385f7f7167f601d2
Jimmi Dyson <jimmidyson@gmail.com>
2017-09-23 22:05:42 +0100

    feat: Add migrate images script to migrate images between two docker registries on openshift
    Note that this requires being logged in to both registries via `docker login` before this script is
    run.

---
+++ tools/images/migrate-images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+FROM_REGISTRY_PREFIX=${1:-docker-registry.engineering.redhat.com/jboss-fuse-7-tech-preview}
+TO_REGISTRY_PREFIX=${2:-registry.fuse-ignite.openshift.com/jboss-fuse-7-tech-preview}
+VERSION=${3:-1.0}
+IMAGES=${4:-"fuse-ignite-java-openshift fuse-ignite-karaf-openshift fuse-ignite-mapper fuse-ignite-mapper fuse-ignite-rest fuse-ignite-token-rp fuse-ignite-ui fuse-ignite-verifier"}
+
+for image in ${IMAGES}; do
+  docker pull ${FROM_REGISTRY_PREFIX}/${image}:${VERSION}
+  docker tag ${FROM_REGISTRY_PREFIX}/${image}:${VERSION} ${TO_REGISTRY_PREFIX}/${image}:${VERSION}
+  docker push ${TO_REGISTRY_PREFIX}/${image}:${VERSION}
+done
