4eb5cd77c8df091aa010632065f247e6cf1479dd
kahboom <rachelyordan@gmail.com>
2017-09-25 11:11:54 +0100

    Add form

--- src/app/setup/github-oauth.component.ts
+++ src/app/setup/github-oauth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
+import { NotificationType } from 'patternfly-ng';
 
 export interface OAuthAppListItem {
   expanded: boolean;
@@ -13,11 +14,26 @@ export interface OAuthAppListItem {
 })
 export class GitHubOAuthSetupComponent implements OnInit {
 
+  header: string = 'Default Header.';
+  message: string = 'Default Message.';
+  dismissible: false;
+  type: NotificationType;
+  types: NotificationType[];
+  hidden: string;
+  actionText: string = '';
   loading: Observable<boolean>;
 
   constructor(public detector: ChangeDetectorRef) {}
 
+  /**
+   * Connects an account to GitHub via OAuth.
+   */
   connectGitHub() {}
+
+  /**
+   * Disconnects a previously connected GitHub account
+   */
+  disconnectGitHub() {}
 
   /**
    * Returns whether or not this item has stored credentials
@@ -34,5 +50,17 @@ export class GitHubOAuthSetupComponent implements OnInit {
   /**
    * View initialization
     */
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.types = [
+      NotificationType.SUCCESS,
+      NotificationType.INFO,
+      NotificationType.DANGER,
+      NotificationType.WARNING,
+    ];
+    this.type = this.types[0];
+  }
+
+  handleType(item: string): void {
+    this.type = item;
+  }
 }
