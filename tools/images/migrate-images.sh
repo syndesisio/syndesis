7c0cfd390bfc2ae4b16d080be124a4d84b4b5b85
Jimmi Dyson <jimmidyson@gmail.com>
2017-09-26 15:47:29 +0100

    fix: Correct image names for migration

--- tools/images/migrate-images.sh
+++ tools/images/migrate-images.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 FROM_REGISTRY_PREFIX=${1:-docker-registry.engineering.redhat.com/jboss-fuse-7-tech-preview}
-TO_REGISTRY_PREFIX=${2:-registry.fuse-ignite.openshift.com/jboss-fuse-7-tech-preview}
+TO_REGISTRY_PREFIX=${2:-registry.fuse-ignite.openshift.com/fuse-ignite}
 VERSION=${3:-1.0}
-IMAGES=${4:-"fuse-ignite-java-openshift fuse-ignite-karaf-openshift fuse-ignite-mapper fuse-ignite-mapper fuse-ignite-rest fuse-ignite-token-rp fuse-ignite-ui fuse-ignite-verifier"}
+IMAGES=${4:-"fuse-ignite-java-openshift fuse-ignite-karaf-openshift fuse-ignite-mapper fuse-ignite-pemtokeystore fuse-ignite-rest fuse-ignite-token-rp fuse-ignite-ui fuse-ignite-verifier"}
 
 for image in ${IMAGES}; do
   docker pull ${FROM_REGISTRY_PREFIX}/${image}:${VERSION}
