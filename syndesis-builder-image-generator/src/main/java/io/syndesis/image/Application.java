7beb6c0bc0ba913dca7ef85e67694384895b856f
Hiram Chirino <hiram@hiramchirino.com>
2017-10-09 14:11:09 -0400

    Applying roland’s feedback to simplify the generate method.

--- syndesis-builder-image-generator/src/main/java/io/syndesis/image/Application.java
+++ syndesis-builder-image-generator/src/main/java/io/syndesis/image/Application.java
@@ -31,8 +31,6 @@ import io.syndesis.project.converter.DefaultProjectGenerator;
 import io.syndesis.project.converter.GenerateProjectRequest;
 import io.syndesis.project.converter.ProjectGeneratorProperties;
 import io.syndesis.project.converter.visitor.*;
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -40,6 +38,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -70,7 +69,7 @@ public class Application implements ApplicationRunner {
         ArrayList<Step> steps = new ArrayList<>();
         HashMap<String, Connector> connectors = new HashMap<String, Connector>();
 
-        String deploymentText = null;
+        String deploymentText;
         try (InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("io/syndesis/dao/deployment.json")) {
             ReadApiClientData readApiClientData = new ReadApiClientData();
             deploymentText = readApiClientData.from(is);
@@ -113,67 +112,37 @@ public class Application implements ApplicationRunner {
     static private void generate(GenerateProjectRequest request, File targetDir) throws IOException {
 
         ProjectGeneratorProperties generatorProperties = new ProjectGeneratorProperties();
-        final Map<String, String> repositories = new HashMap<>();
-        repositories.put("maven.central", "https://repo1.maven.org/maven2");
-        repositories.put("redhat.ga", "https://maven.repository.redhat.com/ga");
-        repositories.put("jboss.ea", "https://repository.jboss.org/nexus/content/groups/ea");
-
         ConnectorCatalogProperties catalogProperties = new ConnectorCatalogProperties();
-        catalogProperties.setMavenRepos(repositories);
+        catalogProperties.setMavenRepos(new HashMap<>());
+        StepVisitorFactoryRegistry registry = new StepVisitorFactoryRegistry(Arrays.asList());
+        DefaultProjectGenerator generator = new DefaultProjectGenerator(new ConnectorCatalog(catalogProperties), generatorProperties, registry);
 
-        // We don't need to generate a real syndesis.yml, so use this dumb step registry.
-        StepVisitorFactoryRegistry registry = new StepVisitorFactoryRegistry(
-            Arrays.asList(
-                new StepVisitorFactory(){
-                    @Override
-                    public String getStepKind() {
-                        return "endpoint";
-                    }
+        Path dir =targetDir.toPath();
+        Files.createDirectories( dir);
+        Files.write(dir.resolve("pom.xml"), generator.generatePom(request.getIntegration()));
 
-                    @Override
-                    public StepVisitor create(GeneratorContext generatorContext) {
-                        return new StepVisitor(){
+        dir = dir.resolve("src/main/java/io/syndesis/example");
+        Files.createDirectories( dir);
 
-                            @Override
-                            public io.syndesis.integration.model.steps.Step visit(StepVisitorContext stepContext) {
-                                return null;
-                            }
-                        };
-                    }
-                }
-            )
-        );
+        String resourceFile = "io/syndesis/project/converter/templates/Application.java.mustache";
+        try (InputStream is = Application.class.getClassLoader().getResourceAsStream(resourceFile) )  {
+            Files.write(dir.resolve("Application.java"), readAllBytes(is));
+        }
 
-        try (InputStream is = new DefaultProjectGenerator(new ConnectorCatalog(catalogProperties), generatorProperties, registry).generate(request)) {
-            Path ret = Files.createDirectories( targetDir.toPath() );
-            try (TarArchiveInputStream tis = new TarArchiveInputStream(is)) {
+    }
 
-                TarArchiveEntry tarEntry = tis.getNextTarEntry();
-                // tarIn is a TarArchiveInputStream
-                while (tarEntry != null) {// create a file with the same name as the tarEntry
-                    File destPath = new File(ret.toFile(), tarEntry.getName());
-                    if (tarEntry.isDirectory()) {
-                        destPath.mkdirs();
-                    } else {
-                        destPath.getParentFile().mkdirs();
-                        destPath.createNewFile();
-                        byte[] btoRead = new byte[8129];
-                        BufferedOutputStream bout = new BufferedOutputStream(new FileOutputStream(destPath));
-                        int len = tis.read(btoRead);
-                        while (len != -1) {
-                            bout.write(btoRead, 0, len);
-                            len = tis.read(btoRead);
-                        }
-                        bout.close();
-                    }
-                    tarEntry = tis.getNextTarEntry();
-                }
-            }
+    public static byte[] readAllBytes(InputStream is) throws IOException {
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[1024 * 4];
+            for (int len; (len = is.read(buffer)) != -1; )
+                os.write(buffer, 0, len);
+            os.flush();
+            return os.toByteArray();
         }
     }
 
 
-    // Helper method to help constuct maps with concise syntax
+    // Helper method to help construct maps with concise syntax
     static private Map<String, String> map(Object... values) {
         HashMap<String, String> rc = new HashMap<>();
         for (int i = 0; i + 1 < values.length; i += 2) {
