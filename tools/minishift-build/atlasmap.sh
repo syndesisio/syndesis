18561fa5883adb1acf1bcd2e1df69fcd07067158
Zoran Regvart <zregvart@apache.org>
2017-10-12 12:03:59 +0200

    fix: adapt to Atlasmap aggregated repository
    Atlasmap git repository is now aggregated repository containing all
    other repositories as git submodules, due to that some paths have
    changed this updates the script to reflect that.

--- tools/minishift-build/atlasmap.sh
+++ tools/minishift-build/atlasmap.sh
@@ -3,6 +3,7 @@
 . "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/vars.sh"
 
 prepare_dir atlasmap
+cd runtime
 ./mvnw clean install -DskipTests
 cd runtime
 ../mvnw clean package fabric8:build -Dfabric8.mode=kubernetes -DskipTests
