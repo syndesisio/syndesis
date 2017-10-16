d3a41ac28228122d9e44d1c5890ce4503dc950c8
Hiram Chirino <hiram@hiramchirino.com>
2017-10-16 08:52:38 -0400

    Updating the developer mode getting started instructions so that it works via redirecting the syndesis-ui service deployed in minishift.
    This allows you to run in dev mode even when your using an oauth-proxy in front of the UI service.

---
+++ scripts/minishift-setup.sh
@@ -0,0 +1,66 @@
+
+#!/bin/bash
+#
+#
+
+set -euo pipefail
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+cd ..
+
+# Lets try to detect the local machine ip..
+LOCAL_IP="$(nslookup $(hostname) | tail -n +4 | grep Address | cut -d " " -f 2)"
+LOCAL_IP="${1:-$LOCAL_IP}"
+
+sed "s/192.168.64.2/$(minishift ip)/" src/config.json.minishift > src/config.json
+oc replace --force -f - <<EOF
+{
+  "kind": "List",
+  "apiVersion": "v1",
+  "items": [
+    {
+      "apiVersion": "v1",
+      "kind": "Service",
+      "metadata": {
+        "labels": {
+          "app": "syndesis",
+          "component": "syndesis-ui"
+        },
+        "name": "syndesis-ui"
+      },
+      "spec": {
+        "ports": [
+          {
+            "port": 80,
+            "protocol": "TCP",
+            "targetPort": 4200,
+            "name": "http"
+          }
+        ],
+        "selector": {}
+      }
+    },
+    {
+      "apiVersion": "v1",
+      "kind": "Endpoints",
+      "metadata": {
+        "name": "syndesis-ui"
+      },
+      "subsets": [
+        {
+          "addresses": [
+            {
+              "ip": "${LOCAL_IP}"
+            }
+          ],
+          "ports": [
+            {
+              "name": "http",
+              "port": 4200
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+EOF
