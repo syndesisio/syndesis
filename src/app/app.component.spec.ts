2f1e142928c6739ce3bef57e17ecad0f9eeb1cd2
kahboom <rachelyordan@gmail.com>
2017-10-31 11:38:37 -0400

    Cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,7 +13,6 @@ import { UserService } from './common/user.service';
 import { ConfigService } from './config.service';
 import { StoreModule } from './store/store.module';
 import { TestSupportService } from './store/test-support.service';
-import { TourNgxBootstrapModule, TourService } from 'ngx-tour-ngx-bootstrap';
 
 /* tslint:disable:no-unused-variable */
 
@@ -29,14 +28,12 @@ describe('AppComponent', () => {
         CollapseModule.forRoot(),
         BsDropdownModule.forRoot(),
         NotificationModule,
-        TourNgxBootstrapModule.forRoot(),
       ],
       providers: [
         ConfigService,
         UserService,
         TestSupportService,
         NavigationService,
-        TourService,
         MockBackend,
         { provide: RequestOptions, useClass: BaseRequestOptions },
         {
