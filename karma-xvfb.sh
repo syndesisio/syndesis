9590637ea4a34ad80adb7d8b4edf50647e4f6fee
Stan Lewis <gashcrumb@gmail.com>
2017-11-02 10:13:33 -0400

    kill off Xvfb when the tests are complete

--- karma-xvfb.sh
+++ karma-xvfb.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-nohup /usr/bin/Xvfb :99 -ac -screen 0 1280x720x24 &
+nohup /usr/bin/Xvfb :99 -ac -screen 0 1280x720x24 > /dev/null 2>&1 &
+PID=$!
+echo "PID: $PID"
+
 export DISPLAY=:99
 #yarn --no-progress
-#npm rebuild node-sass
-exec yarn ng test --watch=false -cc --no-progress
+npm rebuild node-sass
+yarn ng test --watch=false --log-level=DEBUG -cc --no-progress
+
+pkill Xvfb
+waitpid $PID
