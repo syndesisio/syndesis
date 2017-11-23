d00a153956e795da95ec76174989b88ef003e6f3
kahboom <rachelyordan@gmail.com>
2017-11-23 10:17:50 -0500

    Add deps to API connector list component

--- ui/src/app/customizations/api-connectors/api-connector-list.component.ts
+++ ui/src/app/customizations/api-connectors/api-connector-list.component.ts
@@ -1,6 +1,18 @@
-import { Component } from '@angular/core';
-import { log, getCategory } from '../../logging';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+
 import { ConfigService } from '../../config.service';
+import { ConnectorStore } from '../../store/connector/connector.store';
+import { log, getCategory } from '../../logging';
+import { Connectors } from '../../model';
+
+import {
+  Action,
+  ActionConfig,
+  ListConfig,
+  ListEvent,
+  EmptyStateConfig,
+} from 'patternfly-ng';
 
 const category = getCategory('ApiConnectors');
 
@@ -9,8 +21,42 @@ const category = getCategory('ApiConnectors');
   templateUrl: './api-connector-list.component.html',
   styleUrls: ['./api-connector-list.component.scss']
 })
-export class ApiConnectorListComponent {
+export class ApiConnectorListComponent implements OnInit {
+  connectors: Observable<Connectors>;
+  filteredConnectors: Subject<Connectors> = new BehaviorSubject(<Connectors>{});
+  loading: Observable<boolean>;
+  listConfig: ListConfig;
+
   constructor(
-    public config: ConfigService
-  ) {}
+    public config: ConfigService,
+    private store: ConnectorStore,
+  ) {
+    this.loading = this.store.loading;
+    this.listConfig = {
+      dblClick: false,
+      multiSelect: false,
+      selectItems: false,
+      showCheckbox: false,
+      emptyStateConfig: {
+        iconStyleClass: 'pficon pficon-add-circle-o',
+        title: 'Create API Connector',
+        info:
+          'There are currently no API connectors available. Please click on the button below to create one.',
+        actions: {
+          primaryActions: [
+            {
+              id: 'importApiConnector',
+              title: 'Create API Connector',
+              tooltip: 'Create API Connector'
+            }
+          ],
+          moreActions: []
+        } as ActionConfig
+      } as EmptyStateConfig
+    };
+  }
+
+  ngOnInit() {
+    this.store.loadAll();
+  }
 }
