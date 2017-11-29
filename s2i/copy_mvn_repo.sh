bffe1dc30efe781823bc3b5f950845179e906944
Hiram Chirino <hiram@hiramchirino.com>
2017-11-29 10:12:48 -0500

    fixes: s2i image build clean up.  We now generate all sources to target.

--- s2i/copy_mvn_repo.sh
+++ s2i/copy_mvn_repo.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION="$1"
 TARGET_DIR="$2"
 
-mkdir -p "$TARGET_DIR/.m2/repository"
+mkdir -p "$TARGET_DIR/repo"
 cd ~/.m2/repository
-find ./io/syndesis | grep -F -- "/${VERSION}/" | xargs tar -c | tar -x -C "$TARGET_DIR/.m2/repository"
+find ./io/syndesis | grep -F -- "/${VERSION}/" | xargs tar -c | tar -x -C "$TARGET_DIR/repo"
+
+cp "${PROJECT_DIR}/src/main/docker/Dockerfile" $TARGET_DIR
 
