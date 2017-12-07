6451370de4bd111abd848c4b42ed91147f7ba7af
kahboom <rachelyordan@gmail.com>
2017-12-07 14:28:52 -0500

    Lint fixes

--- app/ui/src/app/customizations/api-connector/api-connector-list.component.ts
+++ app/ui/src/app/customizations/api-connector/api-connector-list.component.ts
@@ -34,7 +34,6 @@ export class ApiConnectorListComponent implements OnInit {
     'other': '<strong>#</strong> times'
   };
 
-
   constructor(
     public config: ConfigService,
     private apiConnectorStore: ApiConnectorStore,
