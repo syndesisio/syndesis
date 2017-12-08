4fcca5791edf958f90ba184dca6a169376dc0b35
kahboom <rachelyordan@gmail.com>
2017-12-08 12:07:30 -0500

    Update component for detail view, add support for i18n plural

--- app/ui/src/app/customizations/api-connector/api-connector-detail.component.ts
+++ app/ui/src/app/customizations/api-connector/api-connector-detail.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { ConnectorStore } from '../../store/connector/connector.store';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import { ApiConnector } from './api-connector.model';
+import { ApiConnectorStore } from './api-connector.store';
 
 @Component({
   selector: 'syndesis-api-connector-detail',
-  templateUrl: 'api-connector-detail.component.html'
+  templateUrl: 'api-connector-detail.component.html',
+  styleUrls: ['api-connector-detail.component.scss']
 })
 export class ApiConnectorDetailComponent implements OnInit {
-  constructor(private store: ConnectorStore) { }
+  apiConnector$: Observable<ApiConnector>;
+  loading$: Observable<boolean>;
+  integrationLengthMapping: { [valueComparator: string]: string } = {
+    '=0': 'No integrations are using this extension.',
+    '=1': 'Currently used by <strong>1</strong> integration.',
+    'other': 'Currently used by <strong>#</strong> integrations.'
+  };
+
+  constructor(private apiConnectorStore: ApiConnectorStore,
+              private router: Router,
+              private route: ActivatedRoute) {
+    this.loading$ = this.apiConnectorStore.loading;
+    this.apiConnector$ = this.apiConnectorStore.resource;
+  }
 
   ngOnInit() {
-    // Happening soon!
+    this.route.paramMap.first(params => params.has('id')).subscribe(params => {
+      const id = params.get('id');
+      this.apiConnectorStore.load(id);
+    });
   }
 }
