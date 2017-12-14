1c116cb45b72475ce68964deb5af4b1b1f95005b
kahboom <rachelyordan@gmail.com>
2017-12-14 13:20:08 -0500

    Shorten path to RESTService

--- app/ui/src/app/customizations/api-connector/api-connector.service.ts
+++ app/ui/src/app/customizations/api-connector/api-connector.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Restangular } from 'ngx-restangular';
 
-import { RESTService } from '../../store/entity/rest.service';
+import { RESTService } from '../../store/entity';
 import { ApiConnector, ApiConnectors } from './api-connector.models';
 
 @Injectable()
