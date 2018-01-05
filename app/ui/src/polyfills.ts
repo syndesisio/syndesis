0cad05e54e826a20262e29593cdce967f67b4ac7
Stan Lewis <gashcrumb@gmail.com>
2018-01-05 16:21:16 -0500

    Bump core-js and zone.js dependencies

--- app/ui/src/polyfills.ts
+++ app/ui/src/polyfills.ts
@@ -13,9 +13,13 @@ import 'core-js/es6/array';
 import 'core-js/es6/regexp';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
-import 'core-js/es6/reflect';
 
+/*
+import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+*/
+import 'reflect-metadata';
+
 import 'zone.js/dist/zone';
 
 // If you need to support the browsers/features below, uncomment the import
