a84dfd711b0d4ed429940a9b1693e343beb50a2a
Pablo Deeleman <deeleman@gmail.com>
2018-01-09 19:42:12 +0100

    fix: undesired null or empty parameters are now removed from configuredProperties upon submitting POST and PUT requests

--- app/ui/src/app/customizations/api-connector/api-connector.service.ts
+++ app/ui/src/app/customizations/api-connector/api-connector.service.ts
@@ -2,10 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { ApiHttpService } from '@syndesis/ui/platform';
-import {
-  ApiConnectors,
-  ApiConnectorData, CustomConnectorRequest
-} from './api-connector.models';
+import { ApiConnectors, ApiConnectorData, CustomConnectorRequest } from './api-connector.models';
 
 @Injectable()
 export class ApiConnectorService {
@@ -40,11 +37,13 @@ export class ApiConnectorService {
 
   createCustomConnector(customConnectorRequest: CustomConnectorRequest): Observable<any> {
     const apiHttpService = this.apiHttpService.setEndpointUrl('submitCustomConnector');
-    const [connectorSettings, icon, specification] = [
+    const [rawConnectorSettings, icon, specification] = [
       customConnectorRequest,
       customConnectorRequest.iconFile,
       customConnectorRequest.specificationFile
     ];
+
+    const connectorSettings = this.sanitizeCustomConnectorRequest(rawConnectorSettings);
 
     if (specification || icon) {
       const payload = specification && icon ? { specification, icon } : { specification } || { icon };
@@ -56,10 +55,12 @@ export class ApiConnectorService {
 
   updateCustomConnector(customConnectorRequest: CustomConnectorRequest): Observable<any> {
     const apiHttpService = this.apiHttpService.setEndpointUrl('selectApiConnector', { id: customConnectorRequest.id });
-    const [connector, icon] = [
+    const [rawConnector, icon] = [
       customConnectorRequest,
       customConnectorRequest.iconFile
     ];
+
+    const connector = this.sanitizeCustomConnectorRequest(rawConnector);
 
     if (icon) {
       return apiHttpService.upload({ icon }, { connector }, 'PUT');
@@ -72,5 +73,23 @@ export class ApiConnectorService {
     return this.apiHttpService
       .setEndpointUrl('selectApiConnector', { id })
       .delete<any>();
+  }
+
+  // Custom connector requests need to be sanitized prior to be submitted to the API
+  // Please read: https://github.com/syndesisio/syndesis/issues/980
+  private sanitizeCustomConnectorRequest(customConnector: CustomConnectorRequest): CustomConnectorRequest {
+    const configuredProperties = { ...customConnector.configuredProperties };
+
+    for (const key in configuredProperties) {
+      if (configuredProperties.hasOwnProperty(key)) {
+        if (configuredProperties[key] == null
+          || configuredProperties[key] == undefined
+          || configuredProperties[key] === '') {
+          delete configuredProperties[key];
+        }
+      }
+    }
+
+    return { ...customConnector, configuredProperties };
   }
 }
