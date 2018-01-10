c05343fcd81ab003bed80e0ec5152459761ec13d
Zoran Regvart <zregvart@apache.org>
2018-01-10 18:49:20 +0100

    fix(rest): use Java inspections generated by th...
    ...e Maven helper plugin
    
    The Java inspections that are generated as static resources are now
    available through `/integrations/filters/options` endpoint.
    
    Fixes #970

---
+++ app/rest/inspector/src/main/java/io/syndesis/inspector/DataMapperBaseInspector.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.syndesis.inspector;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import io.syndesis.core.Json;
+import io.syndesis.core.SyndesisServerException;
+
+import org.springframework.util.StringUtils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+abstract class DataMapperBaseInspector implements Inspector {
+
+    private static final String CLASSNAME = "className";
+    private static final String DEFAULT_SEPARATOR = ".";
+    private static final String JAVA_CLASS = "JavaClass";
+
+    private static final String JAVA_FIELD = "javaField";
+    private static final String JAVA_FIELDS = "javaFields";
+    private static final String JAVA_LANG = "java.lang";
+
+    private static final String JAVA_UTIL = "java.util";
+
+    private static final String NAME = "name";
+    private static final String PRIMITIVE = "primitive";
+
+    private final boolean strict;
+
+    /* default */ DataMapperBaseInspector(final boolean strict) {
+        this.strict = strict;
+    }
+
+    @Override
+    public List<String> getPaths(final String kind, final String type, final String specification, final Optional<byte[]> exemplar) {
+        return getPathsForJavaClassName("", type, new ArrayList<>());
+    }
+
+    @Override
+    public final boolean supports(final String kind, final String type, final String specification, final Optional<byte[]> exemplar) {
+        return "java".equals(kind) && !StringUtils.isEmpty(type);
+    }
+
+    protected abstract String fetchJsonFor(String fullyQualifiedName) throws Exception;
+
+    protected final List<String> getPathsForJavaClassName(final String prefix, final String fullyQualifiedName,
+        final List<String> visited) {
+        if (visited.contains(fullyQualifiedName)) {
+            return Collections.emptyList();
+        }
+
+        visited.add(fullyQualifiedName);
+
+        String json;
+        try {
+            json = fetchJsonFor(fullyQualifiedName);
+        } catch (@SuppressWarnings("PMD.AvoidCatchingGenericException") final Exception e) {
+            if (strict) {
+                throw SyndesisServerException.launderThrowable(e);
+            }
+            return Collections.emptyList();
+        }
+
+        return getPathsFromJavaClassJson(prefix, json, visited);
+    }
+
+    @SuppressWarnings("PMD.CyclomaticComplexity")
+    protected final List<String> getPathsFromJavaClassJson(final String prefix, final String json, final List<String> visited) {
+        final List<String> paths = new ArrayList<>();
+        try {
+            final JsonNode node = Json.mapper().readTree(json);
+            if (node != null) {
+                final JsonNode javaClass = node.get(JAVA_CLASS);
+                if (javaClass != null) {
+                    final JsonNode fields = javaClass.get(JAVA_FIELDS);
+                    if (fields != null) {
+                        final JsonNode field = fields.get(JAVA_FIELD);
+                        if (field != null && field.isArray()) {
+                            for (final JsonNode f : field) {
+                                final String name = f.get(NAME).asText();
+                                final String fieldClassName = f.get(CLASSNAME).asText();
+                                final Boolean isPrimitive = f.get(PRIMITIVE).asBoolean();
+                                if (isPrimitive || isTerminal(fieldClassName)) {
+                                    paths.add(prependPrefix(prefix, name));
+                                    continue;
+                                }
+
+                                paths.addAll(getPathsForJavaClassName(prependPrefix(prefix, name), fieldClassName, visited));
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (final IOException e) {
+            throw SyndesisServerException.launderThrowable(e);
+        }
+        return paths;
+    }
+
+    private static String prependPrefix(final String prefix, final String name) {
+        return StringUtils.hasLength(prefix) ? prefix + DEFAULT_SEPARATOR + name : name;
+    }
+
+    /**
+     * Checks if the the specified class name is terminal (we can't further
+     * expand the path). Examples of terminals are primitive, or java.lang
+     * classes.
+     *
+     * @param fullyQualifiedName The specified class name.
+     * @return True if terminal, false otherwise.
+     */
+    protected static boolean isTerminal(final String fullyQualifiedName) {
+        return fullyQualifiedName == null || fullyQualifiedName.startsWith(JAVA_LANG) || fullyQualifiedName.startsWith(JAVA_UTIL);
+    }
+}
--- app/rest/inspector/src/main/java/io/syndesis/inspector/DataMapperClassInspector.java
+++ app/rest/inspector/src/main/java/io/syndesis/inspector/DataMapperClassInspector.java
@@ -15,157 +15,65 @@
  */
 package io.syndesis.inspector;
 
-
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import io.syndesis.core.cache.CacheManager;
-import io.syndesis.core.Json;
-import io.syndesis.core.SyndesisServerException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-public class DataMapperClassInspector implements Inspector {
-
-    private static final String INSPECTOR_URL_FORMAT = "http://%s/%s?%s=%s";
-
-    private static final String JAVA_CLASS = "JavaClass";
-    private static final String JAVA_FIELDS = "javaFields";
-    private static final String JAVA_FIELD = "javaField";
-
-    private static final String NAME = "name";
-    private static final String CLASSNAME = "className";
-    private static final String PRIMITIVE = "primitive";
-
-
-    private static final String DEFAULT_SEPARATOR = ".";
-
-    private static final String JAVA_LANG = "java.lang";
-    private static final String JAVA_UTIL = "java.util";
+public class DataMapperClassInspector extends DataMapperBaseInspector {
 
     private static final String CACHE_NAME = Inspector.class.getName();
 
+    private static final String INSPECTOR_URL_FORMAT = "http://%s/%s?%s=%s";
+
     private final CacheManager caches;
-    private final RestTemplate restTemplate;
     private final ClassInspectorConfigurationProperties config;
+    private final RestTemplate restTemplate;
 
-
-    protected DataMapperClassInspector(CacheManager caches, RestTemplate restTemplate, ClassInspectorConfigurationProperties config) {
+    protected DataMapperClassInspector(final CacheManager caches, final RestTemplate restTemplate,
+        final ClassInspectorConfigurationProperties config) {
+        super(config.isStrict());
         this.caches = caches;
         this.restTemplate = restTemplate;
         this.config = config;
     }
 
     @Override
-    public List<String> getPaths(String kind, String type, String specification, Optional<byte[]> exemplar) {
-        Map<String, List<String>> cache = caches.getCache(CACHE_NAME);
+    public List<String> getPaths(final String kind, final String type, final String specification, final Optional<byte[]> exemplar) {
+        final Map<String, List<String>> cache = caches.getCache(CACHE_NAME);
         if (cache.containsKey(type)) {
             return cache.get(type);
         }
-        List<String> paths = getPathsForJavaClassName("", type, new ArrayList<>());
+        final List<String> paths = getPathsForJavaClassName("", type, new ArrayList<>());
         cache.put(type, paths);
         return paths;
     }
 
     @Override
-    public boolean supports(String kind, String type, String specification, Optional<byte[]> exemplar) {
-        return "java".equals(kind) && !StringUtils.isEmpty(type);
+    protected String fetchJsonFor(final String fullyQualifiedName) throws Exception {
+        final ResponseEntity<String> response = restTemplate.getForEntity(getClassInspectionUrl(config, fullyQualifiedName), String.class);
+
+        return response.getBody();
     }
 
-    protected List<String> getPathsForJavaClassName(String prefix, String fullyQualifiedName, List<String> visited) {
-        if (visited.contains(fullyQualifiedName)) {
-            return Collections.emptyList();
-        }
-
-        visited.add(fullyQualifiedName);
-
-        ResponseEntity<String> response = null;
-        try {
-            response = restTemplate.getForEntity(getClassInspectionUrl(config, fullyQualifiedName), String.class);
-
-        } catch (@SuppressWarnings("PMD.AvoidCatchingGenericException") Exception e) {
-            if (config.isStrict()) {
-                throw SyndesisServerException.launderThrowable(e);
-            }
-            return Collections.emptyList();
-        }
-        String json = response.getBody();
-        return getPathsFromJavaClassJson(prefix, json, visited);
+    protected static String getClassInspectionUrl(final ClassInspectorConfigurationProperties config, final String className) {
+        return String.format(INSPECTOR_URL_FORMAT, config.getHost() + ":" + config.getPort(), config.getPath(),
+            config.getClassNameParameter(), className);
     }
 
-    @SuppressWarnings("PMD.CyclomaticComplexity")
-    protected List<String> getPathsFromJavaClassJson(String prefix, String json, List<String> visited) {
-        List<String> paths = new ArrayList<>();
-        try {
-            JsonNode node = Json.mapper().readTree(json);
-            if (node != null) {
-                JsonNode javaClass = node.get(JAVA_CLASS);
-                if (javaClass != null) {
-                    JsonNode fields = javaClass.get(JAVA_FIELDS);
-                    if (fields != null) {
-                        JsonNode field = fields.get(JAVA_FIELD);
-                        if (field != null && field.isArray()) {
-                            for (JsonNode f : field) {
-                                String name = f.get(NAME).asText();
-                                String fieldClassName = f.get(CLASSNAME).asText();
-                                Boolean isPrimitive = f.get(PRIMITIVE).asBoolean();
-                                if (isPrimitive || isTerminal(fieldClassName)) {
-                                    paths.add(prependPrefix(prefix,name));
-                                    continue;
-                                }
-
-                                paths.addAll(
-                                    getPathsForJavaClassName(
-                                        prependPrefix(prefix,name), fieldClassName, visited));
-                            }
-                        }
-                    }
-                }
-            }
-
-        } catch (IOException e) {
-            throw SyndesisServerException.launderThrowable(e);
-        }
-        return paths;
-    }
-
-    private String prependPrefix(String prefix, String name) {
-        return StringUtils.hasLength(prefix) ? prefix + DEFAULT_SEPARATOR + name : name;
-    }
-
-    protected static String getClassName(String fullyQualifiedName) {
-        int index = fullyQualifiedName.lastIndexOf('.');
+    protected static String getClassName(final String fullyQualifiedName) {
+        final int index = fullyQualifiedName.lastIndexOf('.');
         if (index > 0) {
             return fullyQualifiedName.substring(index + 1);
         }
 
         return fullyQualifiedName;
-    }
-
-    /**
-     * Checks if the the specified class name is terminal (we can't further expand the path).
-     * Examples of terminals are primitive, or java.lang classes.
-     *
-     * @param fullyQualifiedName The specified class name.
-     * @return True if terminal, false otherwise.
-     */
-    protected static boolean isTerminal(String fullyQualifiedName) {
-        return fullyQualifiedName == null || fullyQualifiedName.startsWith(JAVA_LANG) || fullyQualifiedName.startsWith(JAVA_UTIL);
-    }
-
-    protected static String getClassInspectionUrl(ClassInspectorConfigurationProperties config, String className) {
-        return String.format(INSPECTOR_URL_FORMAT,
-            config.getHost() + ":" + config.getPort(),
-            config.getPath(),
-            config.getClassNameParameter(),
-            className);
     }
 }
