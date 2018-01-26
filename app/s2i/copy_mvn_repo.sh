9688fd50e94b79358a71edc053ab5ab7a5eb5b7b
Roland Huß <roland@ro14nd.de>
2018-01-26 10:42:15 +0100

    feature: Rework release command

--- app/s2i/copy_mvn_repo.sh
+++ app/s2i/copy_mvn_repo.sh
@@ -6,8 +6,10 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION="$1"
 TARGET_DIR="$2"
 
+local_repo=${3:-~/.m2/repository}
+
 mkdir -p "$TARGET_DIR/m2"
-cd ~/.m2/repository
+cd $local_repo
 find ./io/syndesis | grep -F -- "/${VERSION}/" | grep -v -- "-sources\." | grep -v -- "-tests\." | xargs tar -c | tar -vx -C "$TARGET_DIR/m2"
 
 cp "${PROJECT_DIR}/src/main/docker/Dockerfile" $TARGET_DIR
