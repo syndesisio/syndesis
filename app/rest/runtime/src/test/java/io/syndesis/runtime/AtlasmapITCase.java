bd8de2bd6e0031ee929326d7bb85420c6a1bffce
Hiram Chirino <hiram@hiramchirino.com>
2018-01-29 20:50:43 -0500

    feat: Eliminate the atlasmap pod by embedding it’s rest services in the syndesis-rest pod.  Fixes #1308

---
+++ app/rest/runtime/src/test/java/io/syndesis/runtime/AtlasmapITCase.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.syndesis.runtime;
+
+import io.syndesis.model.Violation;
+import io.syndesis.model.connection.Connection;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AtlasmapITCase extends BaseITCase {
+
+    private final static ParameterizedTypeReference<List<Violation>> RESPONSE_TYPE = new ParameterizedTypeReference<List<Violation>>() {
+        // defining type parameters
+    };
+
+    private final String id = UUID.randomUUID().toString();
+
+    @Test
+    public void getFieldActions() {
+        ResponseEntity<byte[]> responseEntity = get("/api/v1/atlas/fieldActions", byte[].class);
+        System.out.println(new String(responseEntity.getBody(), StandardCharsets.UTF_8));
+    }
+
+    @Test
+    public void testJsonInspect() throws IOException {
+        String resource = resource("atlasmap-json-inspection.json");
+        ResponseEntity<byte[]> responseEntity = post("/api/v1/atlas/json/inspect", resource.getBytes(StandardCharsets.UTF_8), byte[].class);
+        System.out.println(new String(responseEntity.getBody(), StandardCharsets.UTF_8));
+    }
+
+}
