2314cad75b9ed11c03cf4c5f83e34abc331d340d
Stan Lewis <gashcrumb@gmail.com>
2018-01-31 10:59:31 -0500

    (feat) add support for a duration form control

--- app/ui/src/app/core/providers/form-factory-provider.service.ts
+++ app/ui/src/app/core/providers/form-factory-provider.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FormFactoryService, ConfigurationProperty, ConfiguredConfigurationProperty } from '@syndesis/ui/platform';
+import { DurationInputModel } from '@syndesis/ui/common/ui-patternfly/duration-form-control.model';
 import {
   DynamicFormControlModel,
   DynamicCheckboxModel,
@@ -10,6 +11,7 @@ import {
 
 @Injectable()
 export class FormFactoryProviderService extends FormFactoryService {
+
   constructor() {
     super();
   }
@@ -23,12 +25,12 @@ export class FormFactoryProviderService extends FormFactoryService {
     values: any = {},
     controls: Array<string> = ['*']
   ): DynamicFormControlModel[] {
-    const answer = <DynamicFormControlModel[]>[];
+    const answer = [] as Array<DynamicFormControlModel>;
     for (const key in properties) {
       if (!properties.hasOwnProperty(key)) {
         continue;
       }
-      const field = <ConfiguredConfigurationProperty>properties[key];
+      const field = properties[key] as ConfiguredConfigurationProperty;
       const value = values[key];
       let formField: any;
       const validators = field.required ? { required: null } : {};
@@ -55,103 +57,21 @@ export class FormFactoryProviderService extends FormFactoryService {
           break;
       }
       // then use the appropriate ng2 dynamic forms constructor
-      if (type === 'checkbox') {
-        formField = new DynamicCheckboxModel(
-          {
-            id: key,
-            label: field.displayName || key,
-            hint: field.description,
-            value: value || field.value || field.defaultValue
-          },
-          {
-            element: {
-              control: 'element-control checkbox'
-            },
-            grid: {
-              control: 'col-sm-offset-3 col-sm-9',
-              label: ''
-            }
-          }
-        );
-      } else if (type === 'textarea') {
-        formField = new DynamicTextAreaModel(
-          {
-            id: key,
-            label: field.displayName || key,
-            value: value || field.value || field.defaultValue,
-            hint: field.description,
-            required: field.required,
-            rows: field.rows,
-            cols: field.cols,
-            validators: validators,
-            errorMessages: errorMessages
-          },
-          {
-            element: {
-              label: 'control-label'
-            },
-            grid: {
-              control: 'col-sm-9',
-              label: 'col-sm-3'
-            }
-          }
-        );
-      } else if (type === 'select') {
-        formField = new DynamicSelectModel(
-          {
-            id: key,
-            multiple: false,
-            label: field.displayName || key,
-            value: value || field.defaultValue || field.enum[0].value,
-            hint: field.description,
-            required: field.required,
-            validators: validators,
-            errorMessages: errorMessages,
-            options: field.enum
-          },
-          {
-            element: {
-              label: 'control-label'
-            },
-            grid: {
-              control: 'col-sm-9',
-              label: 'col-sm-3'
-            }
-          }
-        );
-      } else {
-        if (field.secret) {
-          type = 'password';
-        }
-        formField = new DynamicInputModel(
-          {
-            id: key,
-            label: type === 'hidden' ? null : field.displayName || key,
-            inputType: type,
-            value: value || field.value || field.defaultValue,
-            hint: field.description,
-            list: field.enum
-              ? (<Array<any>>field.enum).map(val => {
-                  if (typeof val === 'string') {
-                    return val;
-                  }
-                  return val['value'];
-                })
-              : undefined,
-            required: type === 'hidden' ? undefined : field.required,
-            validators: type === 'hidden' ? undefined : validators,
-            errorMessages: errorMessages
-          },
-          {
-            element: {
-              label: 'control-label'
-            },
-            grid: {
-              control: 'col-sm-9',
-              label: 'col-sm-3'
-            }
-          }
-        );
+      switch (type) {
+        case 'duration':
+          formField = this.createDuration(key, field, value);
+          break;
+        case 'checkbox':
+          formField = this.createCheckbox(key, field, value);
+          break;
+        case 'textarea':
+          formField = this.createTextArea(key, field, value, validators, errorMessages);
+          break;
+        case 'select':
+          formField = this.createSelect(key, field, value, validators, errorMessages);
+          break;
+        default:
+          formField = this.createInput(key, field, value, type,  validators, errorMessages);
       }
 
       if (formField) {
@@ -176,5 +96,136 @@ export class FormFactoryProviderService extends FormFactoryService {
       return aIndex - bIndex;
     });
     return answer;
+  }
+
+  private createDuration(key: string,
+                         field: ConfiguredConfigurationProperty,
+                         value: any): any {
+    return new DurationInputModel({
+      id: key,
+      label: field.displayName || key,
+      inputType: 'duration',
+      value: value || field.value || field.defaultValue,
+      hint: field.description,
+      required: field.required,
+    }, {
+        element: {
+          label: 'control-label'
+        },
+        grid: {
+          control: 'col-sm-9',
+          label: 'col-sm-3'
+        }
+      });
+  }
+
+  private createInput(
+                      key: string,
+                      field: ConfiguredConfigurationProperty,
+                      value: any,
+                      type: string,
+                      validators: {},
+                      errorMessages: {}) {
+    if (field.secret) {
+      type = 'password';
+    }
+    return new DynamicInputModel({
+      id: key,
+      label: type === 'hidden' ? null : field.displayName || key,
+      inputType: type,
+      value: value || field.value || field.defaultValue,
+      hint: field.description,
+      list: field.enum
+        ? (<Array<any>>field.enum).map(val => {
+          if (typeof val === 'string') {
+            return val;
+          }
+          return val['value'];
+        })
+        : undefined,
+      required: type === 'hidden' ? undefined : field.required,
+      validators: type === 'hidden' ? undefined : validators,
+      errorMessages: errorMessages
+    }, {
+        element: {
+          label: 'control-label'
+        },
+        grid: {
+          control: 'col-sm-9',
+          label: 'col-sm-3'
+        }
+      });
+  }
+
+  private createSelect(key: string,
+                       field: ConfiguredConfigurationProperty,
+                       value: any,
+                       validators: {},
+                       errorMessages: {}) {
+    return new DynamicSelectModel({
+      id: key,
+      multiple: false,
+      label: field.displayName || key,
+      value: value || field.defaultValue || field.enum[0].value,
+      hint: field.description,
+      required: field.required,
+      validators: validators,
+      errorMessages: errorMessages,
+      options: field.enum
+    }, {
+        element: {
+          label: 'control-label'
+        },
+        grid: {
+          control: 'col-sm-9',
+          label: 'col-sm-3'
+        }
+      });
+  }
+
+  private createTextArea(key: string,
+                         field: ConfiguredConfigurationProperty,
+                         value: any,
+                         validators: {},
+                         errorMessages: {}) {
+    return new DynamicTextAreaModel({
+      id: key,
+      label: field.displayName || key,
+      value: value || field.value || field.defaultValue,
+      hint: field.description,
+      required: field.required,
+      rows: field.rows,
+      cols: field.cols,
+      validators: validators,
+      errorMessages: errorMessages
+    }, {
+        element: {
+          label: 'control-label'
+        },
+        grid: {
+          control: 'col-sm-9',
+          label: 'col-sm-3'
+        }
+      });
+  }
+
+  private createCheckbox(key: string,
+                         field: ConfiguredConfigurationProperty,
+                         value: any
+  ) {
+    return new DynamicCheckboxModel({
+      id: key,
+      label: field.displayName || key,
+      hint: field.description,
+      value: value || field.value || field.defaultValue
+    }, {
+        element: {
+          control: 'element-control checkbox'
+        },
+        grid: {
+          control: 'col-sm-offset-3 col-sm-9',
+          label: ''
+        }
+      });
   }
 }
