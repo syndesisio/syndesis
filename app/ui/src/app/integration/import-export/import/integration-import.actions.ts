0ad4bc0c951656b715189bd1a97d1233232b8f7a
kahboom <rachelyordan@gmail.com>
2018-02-12 08:44:37 -0500

    Remove potential circular reference

--- app/ui/src/app/integration/import-export/import/integration-import.actions.ts
+++ app/ui/src/app/integration/import-export/import/integration-import.actions.ts
@@ -15,22 +15,12 @@ export const EDIT_INTEGRATION_FAIL = '[Integrations] Edit integration failed';
 
 export class IntegrationImportActions {}
 
-/**
- * Step 1: Integration Upload
- * User selects one or more integration files and uploads them.
- */
 export class IntegrationImportUpload implements Action {
   readonly type = UPLOAD_INTEGRATION;
 
   constructor(public payload: IntegrationImportUploadState) { }
 }
 
-/**
- * Step 2: Integration Edit
- * Once the integration has been uploaded, it is available for review.
- * Once the user has reviewed the payload, they can then press 'Done' to
- * proceed to the Edit Integration view, which will be populated.
- */
 export class IntegrationImportEdit implements Action {
   readonly type = EDIT_INTEGRATION;
 
