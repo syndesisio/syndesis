1c9141a1f04da573fae9be720e753cab5d590eb9
Pablo Deeleman <deeleman@gmail.com>
2018-02-14 12:32:04 +0100

    feature: observable uptime refresh and configurable metrics polling interval

--- app/ui/src/app/dashboard/dashboard_metrics/dashboard-metrics.component.ts
+++ app/ui/src/app/dashboard/dashboard_metrics/dashboard-metrics.component.ts
@@ -1,4 +1,7 @@
+import { ConfigService } from '@syndesis/ui/config.service';
 import { Component, Input, Output, OnInit, OnDestroy, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 
 import { moment } from '@syndesis/ui/vendor';
 import {
@@ -6,18 +9,21 @@ import {
   IntegrationState, Integrations, IntegrationMetrics
 } from '@syndesis/ui/platform';
 
+const DEFAULT_POLLING_INTERVAL = 5000;
+
 @Component({
   selector: 'syndesis-dashboard-metrics',
   templateUrl: './dashboard-metrics.component.html',
   styleUrls: ['./dashboard-metrics.component.scss']
 })
 export class DashboardMetricsComponent implements OnInit, OnDestroy {
-  @Input() connections: Connections; // TODO: Replace by connectionState when the ngrx store supports it
+  @Input() connections: Connections; // TODO: Replace by connectionState once the ngrx store supports it
   @Input() integrationState: IntegrationState;
   @Output() refresh = new EventEmitter<any>();
 
   integrations: Integrations;
   uptimeStart: string;
+  uptimeLegend$: Observable<string>;
 
   private metricsRefreshInterval: any;
   private startDate: moment.Moment;
@@ -28,22 +34,33 @@ export class DashboardMetricsComponent implements OnInit, OnDestroy {
       .length;
   }
 
-  ngOnInit() {
-    this.integrations = this.integrationState.collection;
-    this.startDate = moment(this.integrationState.metrics.summary.start);
-    this.uptimeStart = this.startDate.format('MMM Do hh:mm'); // eg January 12 8:53 pm
-
-    this.metricsRefreshInterval = setInterval(() => {
-      this.refresh.emit(null);
-    }, 5000);
-  }
-
   get integrationMetrics(): IntegrationMetrics {
     return this.integrationState.metrics.summary;
   }
 
-  get uptimeTimeSpan(): string {
-    return moment.duration(moment().diff(this.startDate)).humanize();
+  constructor(private configService: ConfigService) { }
+
+  ngOnInit() {
+    this.integrations = this.integrationState.collection;
+    this.startDate = moment(this.integrationState.metrics.summary.start);
+    this.uptimeStart = this.startDate.format('MMM Do HH:mm A'); // eg January 12nd 8:53 pm
+
+    const pollingInterval = this.configService.getSettings('metricsPollingInterval') || DEFAULT_POLLING_INTERVAL;
+    this.metricsRefreshInterval = setInterval(() => this.refresh.emit(null), pollingInterval);
+
+    const uptimeDuration = moment.duration(moment().diff(this.startDate));
+    this.uptimeLegend$ = Observable.of(uptimeDuration).pipe(
+      map(duration => ({
+        days: duration.days(),
+        hours: duration.hours(),
+        minutes: duration.minutes(),
+      })),
+      map(durationAsObject => Object
+        .keys(durationAsObject)
+        .reduce((timeSpan: string, key: string) => {
+          return durationAsObject[key] > 0 ? timeSpan + `${durationAsObject[key]} ${key} ` : timeSpan;
+        }, ''))
+    );
   }
 
   ngOnDestroy() {
