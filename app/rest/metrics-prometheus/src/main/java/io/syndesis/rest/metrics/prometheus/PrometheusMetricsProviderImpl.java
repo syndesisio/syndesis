346e3df45849398bc1eb05a87a5be5f6d39545b9
Dhiraj Bokde <dbokde@redhat.com>
2018-02-13 18:55:11 -0800

    Support IntegrationDeploymentMetrics in prometheus metrics provider

--- app/rest/metrics-prometheus/src/main/java/io/syndesis/rest/metrics/prometheus/PrometheusMetricsProviderImpl.java
+++ app/rest/metrics-prometheus/src/main/java/io/syndesis/rest/metrics/prometheus/PrometheusMetricsProviderImpl.java
@@ -15,15 +15,18 @@
  */
 package io.syndesis.rest.metrics.prometheus;
 
+import java.util.Comparator;
 import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+import io.syndesis.model.metrics.IntegrationDeploymentMetrics;
 import io.syndesis.model.metrics.IntegrationMetricsSummary;
 import io.syndesis.rest.metrics.MetricsProvider;
 
@@ -34,41 +37,68 @@ public class PrometheusMetricsProviderImpl implements MetricsProvider {
     private final String serviceName;
 
     private final HttpClient httpClient = new HttpClient();
+    private String integrationIdLabel;
+    private String deploymentVersionLabel;
+    private String metricsHistoryRange;
 
     protected PrometheusMetricsProviderImpl(PrometheusConfigurationProperties config) {
         this.serviceName = config.getService();
+        this.integrationIdLabel = config.getIntegrationIdLabel();
+        this.deploymentVersionLabel = config.getDeploymentVersionLabel();
+        this.metricsHistoryRange = config.getMetricsHistoryRange();
     }
 
     @Override
     public IntegrationMetricsSummary getIntegrationMetricsSummary(String integrationId) {
 
         // aggregate values across versions
-        final BinaryOperator<Long> sumLongs = (aLong, aLong2) -> aLong + aLong2;
-        final Optional<Map<String, Long>> totalMessagesMap = getMetricValues(integrationId,
-            "org_apache_camel_ExchangesTotal", "syndesis_io_deployment_id", Long.class, sumLongs);
-        final Optional<Map<String, Long>> failedMessagesMap = getMetricValues(integrationId,
-            "org_apache_camel_ExchangesFailed", "syndesis_io_deployment_id", Long.class, sumLongs);
+        final BinaryOperator<Long> sumLongs = (aLong, aLong2) -> (aLong != null ? aLong : 0L) + (aLong2 != null ? aLong2 : 0L);
+        final BinaryOperator<Date> maxDate = (date1, date2) -> (date1 == null ? date2 : (date2 == null ? date1 : (date1.compareTo(date2) > 0 ? date1 : date2)));
 
-        final BinaryOperator<Date> maxDate = (date, date2) -> date.compareTo(date2) < 0 ? date2 : date;
-        final Optional<Map<String, Date>> startTimeMap = getMetricValues(integrationId,
-            "io_syndesis_camel_StartTimestamp", "syndesis_io_deployment_id", Date.class, maxDate);
-        final Optional<Map<String, Date>> lastProcessingTimeMap = getMetricValues(integrationId,
-            "io_syndesis_camel_LastExchangeCompletedTimestamp", "syndesis_io_deployment_id", Date.class, maxDate);
+        final Map<String, Long> totalMessagesMap = getMetricValues(integrationId,
+            "org_apache_camel_ExchangesTotal", deploymentVersionLabel, Long.class, sumLongs);
+        final Map<String, Long> failedMessagesMap = getMetricValues(integrationId,
+            "org_apache_camel_ExchangesFailed", deploymentVersionLabel, Long.class, sumLongs);
 
-        Optional<Long> totalMessages = totalMessagesMap.map(Map::values).flatMap(
-            longs -> Optional.of(longs.stream().mapToLong(Long::longValue).sum()));
-        Optional<Long> failedMessages = failedMessagesMap.map(Map::values).flatMap(
-            longs -> Optional.of(longs.stream().mapToLong(Long::longValue).sum()));
-        Optional<Date> startTime = startTimeMap.map(Map::values).flatMap(
-            dates -> dates.stream().max(Date::compareTo));
-        Optional<Date> lastProcessingTime = lastProcessingTimeMap.map(Map::values).flatMap(
-            dates -> dates.stream().max(Date::compareTo));
+        final Map<String, Date> startTimeMap = getMetricValues(integrationId,
+            "io_syndesis_camel_StartTimestamp", deploymentVersionLabel, Date.class, maxDate);
+        final Map<String, Date> lastProcessingTimeMap = getMetricValues(integrationId,
+            "io_syndesis_camel_LastExchangeCompletedTimestamp", deploymentVersionLabel, Date.class, maxDate);
+
+        final long[] totalMessages = {0L};
+        final long[] totalErrors = {0L};
+        final Date[] startTime = { null };
+        final Date[] lastProcessedTime = { null };
+
+        final List<IntegrationDeploymentMetrics> deploymentMetricsStream = totalMessagesMap.entrySet().stream().map(entry -> {
+            final String version = entry.getKey();
+            final Long messages = entry.getValue();
+
+            final Long errors = failedMessagesMap.get(version);
+            final Date start = startTimeMap.get(version);
+            final Date lastProcessed = lastProcessingTimeMap.get(version);
+
+            // aggregate values while we are at it
+            totalMessages[0] = sumLongs.apply(totalMessages[0], messages);
+            totalErrors[0] = sumLongs.apply(totalErrors[0], errors);
+            startTime[0] = maxDate.apply(startTime[0], start);
+            lastProcessedTime[0] = maxDate.apply(lastProcessedTime[0], lastProcessed);
+
+            return new IntegrationDeploymentMetrics.Builder()
+                .version(version)
+                .messages(messages)
+                .errors(errors)
+                .start(Optional.ofNullable(start))
+                .lastProcessed(Optional.ofNullable(lastProcessed))
+                .build();
+        }).sorted(Comparator.comparing(IntegrationDeploymentMetrics::getVersion)).collect(Collectors.toList());
 
         return new IntegrationMetricsSummary.Builder()
-                .start(startTime)
-                .lastProcessed(lastProcessingTime)
-                .messages(totalMessages.orElse(0L))
-                .errors(failedMessages.orElse(0L))
+                .integrationDeploymentMetrics(deploymentMetricsStream)
+                .start(Optional.ofNullable(startTime[0]))
+                .lastProcessed(Optional.ofNullable(lastProcessedTime[0]))
+                .messages(totalMessages[0])
+                .errors(totalErrors[0])
                 .build();
     }
 
@@ -77,7 +107,7 @@ public class PrometheusMetricsProviderImpl implements MetricsProvider {
         throw new UnsupportedOperationException();
     }
 
-    private <T> Optional<Map<String, T>> getMetricValues(String integrationId, String metric, String label, Class<? extends T> clazz, BinaryOperator<T> mergeFunction) {
+    private <T> Map<String, T> getMetricValues(String integrationId, String metric, String label, Class<? extends T> clazz, BinaryOperator<T> mergeFunction) {
         HttpQuery queryTotalMessages = createHttpQuery(integrationId, metric);
         QueryResult response = httpClient.queryPrometheus(queryTotalMessages);
         validateResponse(response);
@@ -89,9 +119,9 @@ public class PrometheusMetricsProviderImpl implements MetricsProvider {
                 .host(serviceName)
                 .function("max_over_time")
                 .metric(metric)
-                .addLabelValues(HttpQuery.LabelValue.Builder.of(integrationId.toLowerCase(Locale.US), "syndesis_io_integration_id"))
+                .addLabelValues(HttpQuery.LabelValue.Builder.of(integrationId, this.integrationIdLabel))
                 .addLabelValues(HttpQuery.LabelValue.Builder.of("context", "type"))
-                .range("1d")
+                .range(metricsHistoryRange)
                 .build();
     }
 
