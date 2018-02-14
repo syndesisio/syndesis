7e852a344612c430d4ea6acd5692df548ac674cf
kahboom <rachelyordan@gmail.com>
2018-02-14 13:37:17 -0500

    Add cancel redirect and confirmation modal

--- app/ui/src/app/integration/import-export/integration-import-export.component.ts
+++ app/ui/src/app/integration/import-export/integration-import-export.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ModalService } from '@syndesis/ui/common';
 
 @Component({
   selector: 'syndesis-integration-import-export',
   templateUrl: 'integration-import-export.component.html'
 })
-export class IntegrationImportExportComponent {
-  constructor(private route: ActivatedRoute, private router: Router) {}
+export class IntegrationImportExportComponent implements OnInit {
+  @ViewChild('cancelModalTemplate') cancelModalTemplate: TemplateRef<any>;
+
+  private cancelModalId = 'create-cancellation-modal';
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private modalService: ModalService) {}
+
+  ngOnInit() {
+    this.modalService.registerModal(this.cancelModalId, this.cancelModalTemplate);
+  }
+
+  onCancel(doCancel: boolean): void {
+    this.modalService.hide(this.cancelModalId, doCancel);
+  }
+
+  showCancelModal(): void {
+    this.modalService.show(this.cancelModalId).then(modal => {
+      if (modal.result) {
+        this.redirectBack();
+      }
+    });
+  }
+
+  private redirectBack(): void {
+    this.router.navigate(['/integrations']);
+  }
+
+  ngOnDestroy() {
+    this.modalService.unregisterModal(this.cancelModalId);
+  }
 }
