1c812d193f4646431ba99cc668461fa2bbc691ad
Pablo Deeleman <deeleman@gmail.com>
2018-02-16 10:59:48 +0100

    fix: wrong property name replaced

--- app/ui/src/app/integration/integration_detail/index.ts
+++ app/ui/src/app/integration/integration_detail/index.ts
@@ -1,10 +1,10 @@
 import { Type } from '@angular/core';
 
-export * from './integration-detail.component';
 import { IntegrationDescriptionComponent } from './integration-description.component';
 import { IntegrationHistoryComponent } from './integration-history.component';
 import { IntegrationMetricsComponent } from './integration-metrics.component';
 
+export * from './integration-detail.component';
 export const INTEGRATION_DETAIL_DIRECTIVES: Type<any>[] = [
   IntegrationDescriptionComponent,
   IntegrationHistoryComponent,
