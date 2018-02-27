fe26ac7ca84eb8d6c22ad85ade7d70fb1d10357c
Pablo Deeleman <deeleman@gmail.com>
2018-02-27 19:02:38 +0100

    feature: basic scaffolding for activity component

--- app/ui/src/app/integration/integration_detail/index.ts
+++ app/ui/src/app/integration/integration_detail/index.ts
@@ -1,11 +1,13 @@
 import { Type } from '@angular/core';
 
+import { IntegrationActivityComponent } from './integration-activity.component';
 import { IntegrationDescriptionComponent } from './integration-description.component';
 import { IntegrationHistoryComponent } from './integration-history.component';
 import { IntegrationMetricsComponent } from './integration-metrics.component';
 
 export * from './integration-detail.component';
 export const INTEGRATION_DETAIL_DIRECTIVES: Type<any>[] = [
+  IntegrationActivityComponent,
   IntegrationDescriptionComponent,
   IntegrationHistoryComponent,
   IntegrationMetricsComponent
