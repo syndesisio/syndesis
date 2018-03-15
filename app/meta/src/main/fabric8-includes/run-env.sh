ec5acec244e86f2aa619b2371780aa7ffe0629fc
Roland Huß <roland@ro14nd.de>
2018-03-15 14:30:26 +0100

    fix(meta): set-env.sh works now also with -e
    and supports spaces in file names

--- app/meta/src/main/fabric8-includes/run-env.sh
+++ app/meta/src/main/fabric8-includes/run-env.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-if ls $LOADER_HOME | grep -e ".*\.jar$" > /dev/null 2>&1; then
-    EXTENSION_JARS=$(ls $LOADER_HOME | grep -e ".*\.jar$")
-    export LOADER_PATH=$(echo $EXTENSION_JARS | sed 's/ /,/g')
+if [ -n "${LOADER_HOME:-}" ] && [ $(ls ${LOADER_HOME}/*.jar 2>/dev/null | wc -l) -gt 0 ]; then
+  LOADER_PATH=""
+  for jar in ${LOADER_HOME}/*.jar; do
+    LOADER_PATH="$LOADER_PATH,$jar"
+  done
+  LOADER_PATH=$(echo $LOADER_PATH | cut -c 2-)
+  export LOADER_PATH
+  echo "Using the following extensions in the spring-boot loader path: $LOADER_PATH"
+else
+  echo "No extensions found"
 fi
 
-echo "Using the following extensions in the spring-boot loader path: $LOADER_PATH"
