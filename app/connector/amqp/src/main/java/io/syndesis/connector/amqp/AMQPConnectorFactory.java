6a05ff33d97ca3522e85c7092bf22476f6def084
Dhiraj Bokde <dbokde@redhat.com>
2018-03-29 05:46:46 -0700

    Fixes 2138, resolved AMQP connector issue for topic connections with client ids

--- app/connector/amqp/src/main/java/io/syndesis/connector/amqp/AMQPConnectorFactory.java
+++ app/connector/amqp/src/main/java/io/syndesis/connector/amqp/AMQPConnectorFactory.java
@@ -15,14 +15,8 @@
  */
 package io.syndesis.connector.amqp;
 
-import java.util.Map;
-
 import io.syndesis.integration.component.proxy.ComponentProxyComponent;
 import io.syndesis.integration.component.proxy.ComponentProxyFactory;
-import org.apache.camel.Endpoint;
-import org.apache.camel.component.amqp.AMQPComponent;
-import org.apache.camel.component.extension.ComponentExtension;
-import org.apache.qpid.jms.JmsConnectionFactory;
 
 /**
  * {@link ComponentProxyFactory} to create AMQP Connectors.
@@ -32,53 +26,7 @@ public class AMQPConnectorFactory implements ComponentProxyFactory {
 
     @Override
     public ComponentProxyComponent newInstance(String componentId, String componentScheme) {
-        return new AMQPProxyComponent(componentId, componentScheme);
+        return new AMQPConnector(componentId, componentScheme);
     }
 
-    private static class AMQPProxyComponent extends ComponentProxyComponent {
-
-        private String connectionUri;
-        private String username;
-        private String password;
-        private String brokerCertificate;
-        private String clientCertificate;
-        private boolean skipCertificateCheck;
-
-        public AMQPProxyComponent(String componentId, String componentScheme) {
-            super(componentId, componentScheme);
-            // support connection verification
-            registerExtension(this::getComponentVerifier);
-        }
-
-        private ComponentExtension getComponentVerifier() {
-            return new AMQPVerifierExtension(getComponentId(), getCamelContext());
-        }
-
-        @Override
-        public void setOptions(Map<String, Object> options) {
-            // connection parameters
-            connectionUri = (String) options.remove("connectionUri");
-            username = (String) options.remove("username");
-            password = (String) options.remove("password");
-            brokerCertificate = (String) options.remove("brokerCertificate");
-            clientCertificate = (String) options.remove("clientCertificate");
-            skipCertificateCheck = Boolean.TRUE.equals(options.remove("skipCertificateCheck"));
-
-            super.setOptions(options);
-        }
-
-        @Override
-        protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws
-                Exception {
-
-            // create and set ConnectionFactory on delegate
-            AMQPComponent delegate = (AMQPComponent) getCamelContext().getComponent(getComponentScheme());
-            final AMQPUtil.ConnectionParameters connectionParameters = new AMQPUtil.ConnectionParameters
-                    (connectionUri, username, password, brokerCertificate, clientCertificate, skipCertificateCheck);
-            final JmsConnectionFactory connectionFactory = AMQPUtil.createConnectionFactory(connectionParameters);
-            delegate.setConnectionFactory(connectionFactory);
-
-            return super.createEndpoint(uri, remaining, parameters);
-        }
-    }
 }
