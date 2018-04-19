b517bd056c2a6d8bfc63d4f25c89a6d5ba6f97f1
Roland Huß <roland@ro14nd.de>
2018-04-19 10:25:56 +0200

    feature(upgrade): Added "replace_resources" and "replace_template" step

---
+++ tools/upgrade/common.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+
+# ========================
+# Common utility functions
+# ========================
+
+
+# ===================================================================================================
+# Openshift funcs
+
+scale_deployments() {
+  local replicas=$1
+  shift
+  local dcs="$@"
+  for dc in $dcs; do
+    oc scale dc $dc --replicas=$replicas
+  done
+  wait_for_deployments $replicas $dcs
+}
+
+wait_for_deployments() {
+  local replicas_desired=$1
+  shift
+  local dcs="$@"
+
+  oc get pods -w &
+  watch_pid=$!
+  for dc in $dcs; do
+      echo "Waiting for $dc to be scaled to ${replicas_desired}"
+      local replicas=$(get_replicas $dc)
+      while [ "$replicas" -ne $replicas_desired ]; do
+          echo "Sleeping 10s ..."
+          sleep 10
+          replicas=$(get_replicas $dc)
+      done
+  done
+  kill $watch_pid
+}
+
+backup_resource() {
+    local backupdir=$1
+    local kind=$2
+
+    mkdir -p ${backupdir}/${kind}
+    for res in $(oc get ${kind} -l app=syndesis,type=infrastructure -o name | sed -e "s/^[^\/]*\///"); do
+        echo "        * $res"
+        oc get ${kind} $res -o json > "${backupdir}/${kind}/${res}.json"
+    done
+}
+
+restore_resources() {
+    local resource_dir=$1
+
+    for kind in $(ls $resource_dir); do
+        echo "      - $kind: "
+        for resource in $(ls $resource_dir/$kind); do
+            echo "        * $(basename $resource .json)"
+            local result
+            local rc
+            set +e
+            result=$(oc replace --force -f "$resource_dir/$kind/$resource" 2>&1)
+            rc=$?
+            set -e
+            if [ $rc -gt 0 ]; then
+                if [ -z "${result/*not found*/}" ]; then
+                    echo "          not existing --> trying to create"
+                    oc create -f "$resource_dir/$kind/$resource"
+                else
+                    echo "$result"
+                    exit 1
+                fi
+            fi
+        done
+    done
+}
+
+syndesis_deployments() {
+  oc get dc -l app=syndesis,type=infrastructure -o name | sed -e "s/^deploymentconfigs\///"
+}
+
+pod() {
+  local dc=${1}
+  local ret=$(oc get pod -o name | grep "$dc" | sed -e "s/^pods\///")
+  local nr_pods=$(echo $ret | wc -l | awk '$1=$1')
+  if [ $nr_pods != "1" ]; then
+      echo "ERROR: More than 1 pod found for $dc ($nr_pods found)"
+  fi
+  echo $ret
+}
+
+get_replicas() {
+  local dc=${1}
+  oc get dc $dc -o jsonpath="{.status.availableReplicas}"
+}
+# Read from global config
+read_global_config() {
+    local key=$1
+    oc get secret syndesis-global-config -o jsonpath={.data.${key}} | base64 --decode
+}
+
+# ===================================================================================================
+# Postgres funcs
+
+pg_backup() {
+    local backupdir=${1}
+    local db=${2}
+
+    local pod=$(pod "syndesis-db")
+    check_error $pod
+
+    if [ ! -d ${backupdir} ]; then
+      mkdir -p ${backupdir}
+    fi
+
+    # Create backup file remotely
+    oc rsh $pod bash -c "pg_dump -Fc -b $db | base64" > ${backupdir}/${db}
+}
+
+pg_restore() {
+    local backupdir=${1}
+    local db=${2}
+
+    local pod=$(pod "syndesis-db")
+    check_error $pod
+
+    # Create backup file remotely
+    if [ ! -f "${backupdir}/${db}" ]; then
+        echo "ERROR: No backup for ${db} found in ${backupdir}"
+        exit 1
+    fi
+
+    local cmd=$(cat <<EOT
+set -e;
+base64 -d -i > /var/lib/pgsql/data/${db}.dmp;
+createdb -T template0 ${db}_restore;
+pg_restore -v -d ${db}_restore /var/lib/pgsql/data/${db}.dmp;
+dropdb ${db};
+psql -c 'alter database ${db}_restore rename to $db';
+rm /var/lib/pgsql/data/${db}.dmp;
+EOT
+)
+    oc rsh -t $pod bash -c "$cmd" < "${backupdir}/${db}"
+}
+
+# ==================================================================================
+
+
+# Checks if a flag is present in the arguments.
+hasflag() {
+    filters="$@"
+    for var in "${ARGS[@]}"; do
+        for filter in $filters; do
+          if [ "$var" = "$filter" ]; then
+              echo 'true'
+              return
+          fi
+        done
+    done
+}
+
+# Read the value of an option.
+readopt() {
+    filters="$@"
+    next=false
+    for var in "${ARGS[@]}"; do
+        if $next; then
+            echo $var
+            break;
+        fi
+        for filter in $filters; do
+            if [[ "$var" = ${filter}* ]]; then
+                local value="${var//${filter}=/}"
+                if [ "$value" != "$var" ]; then
+                    echo $value
+                    return
+                fi
+                next=true
+            fi
+        done
+    done
+}
+
+
+check_error() {
+    local msg="$*"
+    if [ "${msg//ERROR/}" != "${msg}" ]; then
+        echo $msg
+        exit 1
+    fi
+}
--- tools/upgrade/upgrade.sh
+++ tools/upgrade/upgrade.sh
@@ -40,8 +40,8 @@ EOT
 
 run() {
     local basedir=$(basedir)
-
     local stepsdir="$basedir/steps"
+
     local tag=$(readopt --tag)
     if [ -z "${tag}" ]; then
       local template=$(readopt --template)
@@ -84,45 +84,37 @@ run() {
     fi
 
     local time=$(date +"%Y-%m-%d-%s")
-    local backupdir=${base_backupdir}/$time
+    local top_dir="${base_backupdir}/$time"
+    [ -d ${top_dir} ] || mkdir -p ${top_dir}
+
+    # Create backup dir
+    local backupdir="${top_dir}/backup"
     [ -d ${backupdir} ] || mkdir -p ${backupdir}
 
-    # Create a workdir
-    local workdir=${base_backupdir}/workdir
+    # Create target dir
+    local workdir="${top_dir}/target"
     [ -d $workdir ] || mkdir -p ${workdir}
 
     # Check migrationdir
-    local migrationdir=$(readopt --migration)
+    local migrationdir="$(readopt --migration)"
     echo $time > ${base_backupdir}/LATEST
 
     # Preflight check
     source $basedir/migration/preflight.sh
-    preflight_version_check "$(read_global_config syndesis)" "$tag"
+    local current_tag=$(read_global_config syndesis)
+    preflight_version_check "$current_tag" "$tag"
 
     echo "============================================="
     echo "=== STARTING UPGRADE TO SYNDESIS $tag "
     echo "============================================="
     echo
 
-    local actions_performed=""
-    # Prepare for an upgrade.
-    for step_def in $(ls $stepsdir/prep_* $stepsdir/upgrade_*); do
-        source $step_def
-        local step="$(extract_step $step_def)"
-        local label=$(eval "${step}::label")
-        echo "=== * $label ($(basename $step_def))"
-        set +e
-        (eval "set -e; ${step}::run $backupdir $workdir $tag $migrationdir")
-        if [ $? -ne 0 ]; then
-            set -e
-            echo "====> Error ==> Rollback "
-            rollback $backupdir $actions_performed
-            cleanup_backupdir $backupdir
-            exit 1
-        fi
-        actions_performed="$step $actions_performed"
-        set -e
-    done
+    perform_actions "$backupdir" \
+                    "$workdir" \
+                    "$current_tag" \
+                    "$tag" \
+                    "$migrationdir" \
+                    "$(ls $stepsdir/prep_* $stepsdir/upgrade_*)"
 
     local wait_secs=$(readopt --wait)
     if [ -n "${wait_secs}" ]; then
@@ -131,9 +123,40 @@ run() {
     fi
 }
 
+perform_actions() {
+    local backupdir=${1}
+    local workdir=${2}
+    local current_tag=${3}
+    local tag=${4}
+    local migrationdir=${5}
+    local steps=${6}
+
+    local actions_performed=""
+
+    # Prepare for an upgrade.
+    for step_def in $steps; do
+        source $step_def
+        local step="$(extract_step $step_def)"
+        local label=$(eval "${step}::label")
+        echo "=== * $label ($(basename $step_def))"
+        set +e
+        actions_performed="$step $actions_performed"
+        (eval "set -e; ${step}::run \"$backupdir\" \"$workdir\" \"$tag\" \"$migrationdir\"")
+        if [ $? -ne 0 ]; then
+            set -e
+            echo "====> Error ==> Rollback "
+            rollback $backupdir $current_tag $actions_performed
+            cleanup_backupdir $backupdir
+            exit 1
+        fi
+        set -e
+    done
+}
+
 rollback() {
     local backupdir=$1
-    shift 1
+    local current_tag=$2
+    shift 2
     echo
     echo "----- Rollback"
     local cleanup=$(hasflag --cleanup)
@@ -141,7 +164,7 @@ rollback() {
       local label=$(eval "${step}::label")
       echo "--- * Rolling back '$label'"
       set +e
-      (eval "${step}::rollback $backupdir $workdir $cleanup")
+      (eval "${step}::rollback \"$backupdir\" \"$workdir\" \"$cleanup\" \"$current_tag\"")
       if [ $? -ne 0 ]; then
         set -e
         echo "====> Rollback Error ==> Exit"
@@ -151,7 +174,6 @@ rollback() {
       set -e
     done
 }
-
 
 # Dir where this script is located
 basedir() {
@@ -175,50 +197,6 @@ basedir() {
     local dir=$(dirname "$script")
     local full_dir=$(cd "${dir}" && pwd)
     echo ${full_dir}
-}
-
-# Checks if a flag is present in the arguments.
-hasflag() {
-    filters="$@"
-    for var in "${ARGS[@]}"; do
-        for filter in $filters; do
-          if [ "$var" = "$filter" ]; then
-              echo 'true'
-              return
-          fi
-        done
-    done
-}
-
-# Read the value of an option.
-readopt() {
-    filters="$@"
-    next=false
-    for var in "${ARGS[@]}"; do
-        if $next; then
-            echo $var
-            break;
-        fi
-        for filter in $filters; do
-            if [[ "$var" = ${filter}* ]]; then
-                local value="${var//${filter}=/}"
-                if [ "$value" != "$var" ]; then
-                    echo $value
-                    return
-                fi
-                next=true
-            fi
-        done
-    done
-}
-
-
-check_error() {
-    local msg="$*"
-    if [ "${msg//ERROR/}" != "${msg}" ]; then
-        echo $msg
-        exit 1
-    fi
 }
 
 cleanup_backupdir() {
@@ -258,132 +236,12 @@ template_path() {
     fi
     echo $(basedir)/../../install/syndesis.yml
 }
-# ===================================================================================================
-# Postgres funcs
-
-pg_backup() {
-    local backupdir=${1}
-    local db=${2}
-
-    local pod=$(pod "syndesis-db")
-    check_error $pod
-
-    if [ ! -d ${backupdir} ]; then
-      mkdir -p ${backupdir}
-    fi
-
-    # Create backup file remotely
-    oc rsh $pod bash -c "pg_dump -Fc -b $db | base64" > ${backupdir}/${db}
-}
-
-pg_restore() {
-    local backupdir=${1}
-    local db=${2}
-
-    local pod=$(pod "syndesis-db")
-    check_error $pod
-
-    # Create backup file remotely
-    if [ ! -f "${backupdir}/${db}" ]; then
-        echo "ERROR: No backup for ${db} found in ${backupdir}"
-        exit 1
-    fi
-
-    local cmd=$(cat <<EOT
-set -e;
-base64 -d -i > /var/lib/pgsql/data/${db}.dmp;
-createdb -T template0 ${db}_restore;
-pg_restore -v -d ${db}_restore /var/lib/pgsql/data/${db}.dmp;
-dropdb ${db};
-psql -c 'alter database ${db}_restore rename to $db';
-rm /var/lib/pgsql/data/${db}.dmp;
-EOT
-)
-    oc rsh -t $pod bash -c "$cmd" < "${backupdir}/${db}"
-}
-
-# ===================================================================================================
-# OpenShift funcs
-
-scale_deployments() {
-  local replicas=$1
-  shift
-  local dcs="$@"
-  for dc in $dcs; do
-    oc scale dc $dc --replicas=$replicas
-  done
-  wait_for_deployments $replicas $dcs
-}
-
-# Read from global config
-read_global_config() {
-    local key=$1
-    oc get secret syndesis-global-config -o jsonpath={.data.${key}} | base64 --decode
-}
-
-wait_for_deployments() {
-  local replicas_desired=$1
-  shift
-  local dcs="$@"
-
-  oc get pods -w &
-  watch_pid=$!
-  for dc in $dcs; do
-      echo "Waiting for $dc to be scaled to ${replicas_desired}"
-      local replicas=$(get_replicas $dc)
-      while [ "$replicas" -ne $replicas_desired ]; do
-          echo "Sleeping 10s ..."
-          sleep 10
-          replicas=$(get_replicas $dc)
-      done
-  done
-  kill $watch_pid
-}
-
-get_replicas() {
-  local dc=${1}
-  oc get dc $dc -o jsonpath="{.status.availableReplicas}"
-}
-
-backup_resource() {
-    local backupdir=$1
-    local kind=$2
-
-    mkdir -p ${backupdir}/${kind}
-    for res in $(oc get ${kind} -l app=syndesis -o name | sed -e "s/^${kind}s\///"); do
-        echo "      - $res"
-        oc get ${kind} $res -o yaml > "${backupdir}/${kind}/${res}"
-    done
-}
-
-restore_resource() {
-    local backupdir=$1
-    local kind=$2
-
-    if [ -d "${backupdir}/${kind}" ]; then
-      for res in $(ls $backupdir/$kind/*); do
-        echo "      - $res"
-        oc create ${kind} $res -o yaml > "${backupdir}/${kind}/${res}"
-      done
-    fi
-}
-
-syndesis_deployments() {
-  oc get dc -l app=syndesis -o name | sed -e "s/^deploymentconfigs\///"
-}
-
-pod() {
-  local dc=${1}
-  local ret=$(oc get pod -o name | grep "$dc" | sed -e "s/^pods\///")
-  local nr_pods=$(echo $ret | wc -l | awk '$1=$1')
-  if [ $nr_pods != "1" ]; then
-      echo "ERROR: More than 1 pod found for $dc ($nr_pods found)"
-  fi
-  echo $ret
-}
-
 
 # =================================================================================
+# Startup
+
+# Include common utilities
+source $(basedir)/common.sh
 
 if [ $(hasflag --verbose) ]; then
     export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
