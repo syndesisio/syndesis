e40011671e7a077147509f7652db75479b8d9dcf
Zoran Regvart <zoran@regvart.com>
2018-07-05 22:03:18 +0200

    chore(ui): logout endpoint
    This adds a `/logout` endpoint backed by nginx and static logout.html
    file.
    
    When `/logout` endpoint is requested with the 'SYNDESIS_XSRF_TOKEN` set
    to `awesome` the response will expire the `_oauth_proxy` cookie. In any
    case it will serve the `logout.html`.
    
    The UI when performing the logout will replace the `window.document`
    with the response of the `/logout` endpoint and will set the URL to
    `/logout`.
    
    This design protects the denial of service if the `/logout` endpoint is
    invoked cross origin and allows for the served page to be reloaded
    making it deterministic and thus safer.
    
    The `logout.html` embeds the used CSS and just displays a simple _You
    have been logged out_ message offering to Login again by linking to `/`.
    
    Fixes #2561

--- app/ui/src/app/core/providers/user-provider.service.ts
+++ app/ui/src/app/core/providers/user-provider.service.ts
@@ -2,20 +2,19 @@ import { Injectable, NgZone } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { UserService, ApiHttpService, User } from '@syndesis/ui/platform';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpXsrfTokenExtractor } from '@angular/common/http';
+
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class UserProviderService extends UserService {
   private user$: Observable<User>;
 
-  /**
-   * UserService constructor
-   * @param {HttpClient} httpClient
-   */
   constructor(
     private httpClient: HttpClient,
     private apiHttpService: ApiHttpService,
-    private ngZone: NgZone
+    private ngZone: NgZone,
+    private tokenExtractor: HttpXsrfTokenExtractor
   ) {
     super();
   }
@@ -28,11 +27,25 @@ export class UserProviderService extends UserService {
   }
 
   /**
-   * Triggers the logout flow and effectively returns the user to the login page
+   * Triggers the logout flow and renders the HTML response
    */
   logout(): void {
+    this.user$ = null;
     this.ngZone.runOutsideAngular(() => {
-      window.location.href = '/oauth/sign_out';
+      const headers = {};
+      const token = this.tokenExtractor.getToken() || environment.xsrf.defaultTokenValue;
+
+      headers[environment.xsrf.headerName] = token;
+      this.httpClient.get('/logout', {
+        headers: headers,
+        responseType: 'arraybuffer'
+      }).subscribe(buffy => {
+        const html = String.fromCharCode.apply(null, new Uint8Array(buffy));
+        window.history.pushState(null, null, '/logout');
+        window.document.open();
+        window.document.write(html);
+        window.document.close();
+      });
     });
   }
 }
