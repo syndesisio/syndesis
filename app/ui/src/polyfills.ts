7d76b68e4194616fc2ad936f2d5349a58d1d59df
kahboom <rachelyordan@gmail.com>
2018-07-04 22:52:46 -0400

    Moving polyfills to polyfills file

--- app/ui/src/polyfills.ts
+++ app/ui/src/polyfills.ts
@@ -81,3 +81,6 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any).global = window;
+
