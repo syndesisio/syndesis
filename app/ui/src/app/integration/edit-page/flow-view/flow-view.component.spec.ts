9482db1d4ff17f451b7c6cf70fe593e560ab3be6
Stan Lewis <slewis@fusesource.com>
2018-07-06 08:43:17 -0400

    cleanup and fix test

--- app/ui/src/app/integration/edit-page/flow-view/flow-view.component.spec.ts
+++ app/ui/src/app/integration/edit-page/flow-view/flow-view.component.spec.ts
@@ -2,22 +2,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { TabsModule, CollapseModule, PopoverModule } from 'ngx-bootstrap';
-import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { ApiModule } from '@syndesis/ui/api';
 import { CoreModule } from '@syndesis/ui/core';
 import { IntegrationSupportModule } from '@syndesis/ui/integration/integration-support.module';
 import { FlowViewComponent } from './flow-view.component';
 import { FlowViewStepComponent } from './flow-view-step.component';
-import { EventsService, IntegrationStore, StepStore } from '@syndesis/ui/store';
+import { SyndesisStoreModule } from '@syndesis/ui/store';
 import { IntegrationService } from '../../../store/integration/integration.service';
 import { SyndesisCommonModule } from '../../../common/common.module';
 import { ConnectionsModule } from '../../../connections/connections.module';
+import { VendorModule } from '@syndesis/ui/vendor';
 import {
   CurrentFlowService,
   FlowPageService
 } from '@syndesis/ui/integration/edit-page';
+import { PlatformModule } from '@syndesis/ui/platform';
 
 describe('FlowViewComponent', () => {
   let component: FlowViewComponent;
@@ -32,22 +32,18 @@ describe('FlowViewComponent', () => {
         FormsModule,
         RouterTestingModule.withRoutes([]),
         ConnectionsModule,
-        ModalModule.forRoot(),
-        TabsModule.forRoot(),
-        PopoverModule.forRoot(),
-        CollapseModule.forRoot(),
         SyndesisCommonModule.forRoot(),
         IntegrationSupportModule,
-        CollapseModule
+        SyndesisStoreModule,
+        PlatformModule.forRoot(),
+        CoreModule.forRoot(),
+        VendorModule
       ],
       declarations: [FlowViewComponent, FlowViewStepComponent],
       providers: [
         CurrentFlowService,
         FlowPageService,
-        IntegrationStore,
-        IntegrationService,
-        EventsService,
-        StepStore
+        IntegrationService
       ]
     }).compileComponents();
   }));
