f9c2f3930475a709a859d70719f4af579081ea05
Zoran Regvart <zoran@regvart.com>
2018-08-24 15:55:40 +0200

    chore(upgrade): server config map patch robustn...
    ...esses
    
    Use python to make parsing, modifying and patching application.yml to
    make it more robust.

--- tools/upgrade/migration/resource/1.1/03_server_config_map_upgrade.sh
+++ tools/upgrade/migration/resource/1.1/03_server_config_map_upgrade.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 # Add the new parameter to server ConfigMap
 # fetch the application.yml and append the new parameter
-APP_YAML="$(oc get configmap syndesis-server-config -o jsonpath='{.data.application\.yml}')
-  integrationStateCheckInterval: 60
-monitoring:
-  kind: default
-features:
-  monitoring:
-    enabled: true
-"
-# change newlines to \n
-APP_YAML=${APP_YAML//$'\n'/\\n}
-# change quotes to \"
-APP_YAML=${APP_YAML//\"/\\\"}
-# generate the patch
+APP_YAML=$(python - <<EOF
+import yaml
+import os
+
+def quoted_presenter(dumper, data):
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+
+yaml.add_representer(str, quoted_presenter)
+
+with os.popen("oc get configmap syndesis-server-config -o jsonpath='{.data.application\.yml}'") as stream:
+  config = yaml.load(stream)
+  config["controllers"] = config.get("controllers", dict())
+  config["controllers"]["integrationStateCheckInterval"] = 60
+  config["monitoring"] = config.get("monitoring", dict())
+  config["monitoring"]["kind"] = "default"
+  config["features"] = config.get("features", dict())
+  config["features"]["monitoring"] = config["features"].get("monitoring", dict())
+  config["features"]["monitoring"]["enabled"] = True
+
+  updated = yaml.dump(config, default_flow_style=False)
+  print updated.replace('\n', "\\\n").replace('\"', '\\\"')
+EOF
+)
 APP_YAML_PATCH="{ \"data\": { \"application.yml\": \"$APP_YAML\" } }"
 oc patch configmap syndesis-server-config -p "$APP_YAML_PATCH"
