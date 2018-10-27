540a715afcbd693f52e7a70af8bd1616290cb142
Zoran Regvart <zoran@regvart.com>
2018-10-27 16:36:01 +0200

    chore: tool to update icons stored on the backend
    Simply run `yarn icons` and all backend descriptors that end up being
    loaded will be updated with the new and optimized SVG icons from this
    directory.

---
+++ ux/connections_icons/svg/update_icons.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+const path = require('path');
+const jsonfile = require('jsonfile');
+const jq = require('node-jq')
+const readDir = require('readdir');
+const SVGO = require('svgo');
+
+const DEPLOYMENT_JSON = '../../../app/server/dao/src/main/resources/io/syndesis/server/dao/deployment.json';
+const CONNECTORS_PATH = '../../../app/connector';
+const SCHEMA_JAVA = '../../../app/common/model/src/main/java/io/syndesis/common/model/Schema.java';
+
+const CONNECTORS = jsonfile.readFileSync('mapping.json');
+
+const svgo = new SVGO({
+  quiet: true
+});
+
+function optimize(file) {
+  const data = fs.readFileSync(file);
+  return svgo.optimize(data).then(optimized => ({
+    id: CONNECTORS.find(c => c.icon == file).connectorId,
+    icon: 'data:image/svg+xml;base64,' + Buffer.from(optimized.data).toString('base64')
+  }));
+}
+
+const svgs = readDir.readSync('.', ['*.svg']);
+
+const missingSvgs = CONNECTORS.map(c => c.icon).filter(svg => svgs.indexOf(svg) < 0);
+if (missingSvgs.length != 0) {
+  console.log("No SVG icon found for:");
+  missingSvgs.forEach(e => console.log(e));
+  return;
+}
+
+const missingMappings = svgs.filter(file => !(CONNECTORS.find(c => c.icon == file)));
+if (missingMappings.length != 0) {
+  console.log("No mapping.json entry found for:");
+  missingMappings.forEach(e => console.log(e));
+  return;
+}
+
+Promise.all(CONNECTORS.filter(c => !c.ignore).map(c => {
+  return optimize(c.icon);
+})).then(optimized => {
+  jsonfile.readFile(DEPLOYMENT_JSON)
+    .then(deployment => {
+      let madeUpdates = false;
+
+      deployment.forEach(obj => {
+        if (!(obj.data.connectorId)) {
+          return;
+        }
+        const update = optimized.find(o => o.id == obj.data.connectorId);
+        if (update && obj.data.icon !== update.icon) {
+          obj.data.icon = update.icon;
+          madeUpdates = true;
+        }
+      })
+
+      if (madeUpdates) {
+        jsonfile.writeFile(DEPLOYMENT_JSON, deployment).then(
+          () => jq.run('.', DEPLOYMENT_JSON, { sort: true }).then(out => fs.writeFileSync(DEPLOYMENT_JSON, out + '\n'))
+        )
+        let schemaJava = fs.readFileSync(SCHEMA_JAVA).toString();
+        schemaJava = schemaJava.replace(/VERSION = (\d+);/, (match, version) => 'VERSION = ' + (parseInt(version, 10) + 1) + ';');
+        fs.writeFileSync(SCHEMA_JAVA, schemaJava);
+      }
+    })
+    .catch(error => console.error(error))
+
+  readDir.readSync(CONNECTORS_PATH, ['**/*.json'])
+    .filter(f => f.indexOf('src/main/resources/META-INF/syndesis/connector') > 0)
+    .forEach(f => {
+      const descriptorFile = path.join(CONNECTORS_PATH, f);
+      jsonfile.readFile(descriptorFile)
+        .then(descriptor => {
+          const update = optimized.find(o => o.id == descriptor.id);
+          if (update) {
+            descriptor.icon = update.icon;
+          }
+          jsonfile.writeFile(descriptorFile, descriptor).then(
+            () => jq.run('.', descriptorFile, { sort: true }).then(out => fs.writeFileSync(descriptorFile, out + '\n'))
+          )
+        })
+    });
+});
