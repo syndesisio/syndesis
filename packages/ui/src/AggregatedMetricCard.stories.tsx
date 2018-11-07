6b7a59d1321a96024adec305e6b5211d41c25cef
Riccardo Forina <rforina@redhat.com>
2018-11-07 12:28:16 +0100

    Add storybook to @syndesis/ui

---
+++ packages/ui/src/AggregatedMetricCard.stories.tsx
@@ -0,0 +1,43 @@
+import { storiesOf } from "@storybook/react";
+import { shallow } from "enzyme";
+import expect from "expect";
+import * as React from "react";
+import { describe, it, specs } from "storybook-addon-specifications";
+import { StoryHelper } from "../.storybook/StoryHelper";
+import { AggregatedMetricCard } from "./AggregatedMetricCard";
+
+const stories = storiesOf("Components", module);
+
+stories
+  .addDecorator(story => <StoryHelper>{story()}</StoryHelper>)
+  .add(
+    "AggregatedMetricCard",
+    () => {
+      const story =
+        <AggregatedMetricCard
+          title={"A Title"}
+          ok={10}
+          error={5}
+        />;
+
+      specs(() => describe("It renders", function() {
+        it("Should have the A Title title", function() {
+          const wrapper = shallow(story);
+          const title = wrapper.find("[data-test-aggregate-title]");
+          expect(title.text()).toEqual("A Title");
+        });
+        it("Should have 5 errors", function() {
+          const wrapper = shallow(story);
+          const errorCount = wrapper.find("[data-test-aggregate-error-count]");
+          expect(errorCount.text()).toEqual("5");
+        });
+        it("Should have 10 ok", function() {
+          const wrapper = shallow(story);
+          const okCount = wrapper.find("[data-test-aggregate-ok-count]");
+          expect(okCount.text()).toEqual("10");
+        });
+      }));
+
+      return story;
+    }
+  );
--- packages/ui/src/AggregatedMetricCard.tsx
+++ packages/ui/src/AggregatedMetricCard.tsx
@@ -3,9 +3,9 @@ import {
   AggregateStatusNotification,
   AggregateStatusNotifications,
   Card,
-  Icon,
-} from 'patternfly-react';
-import * as React from 'react';
+  Icon
+} from "patternfly-react";
+import * as React from "react";
 
 export interface IAggregatedMetricProps {
   title: string;
@@ -13,24 +13,30 @@ export interface IAggregatedMetricProps {
   error: number;
 }
 
-export class AggregatedMetricCard extends React.PureComponent<
-  IAggregatedMetricProps
-> {
+export class AggregatedMetricCard extends React.PureComponent<IAggregatedMetricProps> {
   public render() {
     return (
       <Card accented={true} aggregated={true} matchHeight={true}>
         <Card.Title>
-          <AggregateStatusCount>{this.props.title}</AggregateStatusCount>
+          <AggregateStatusCount>
+            <span data-test-aggregate-title={true}>
+              {this.props.title}
+            </span>
+          </AggregateStatusCount>
         </Card.Title>
         <Card.Body>
           <AggregateStatusNotifications>
             <AggregateStatusNotification>
-              <Icon type="pf" name="ok" />
-              {this.props.ok}{' '}
+              <Icon type="pf" name="ok"/>
+              <span data-test-aggregate-ok-count={true}>
+                {this.props.ok}
+              </span>{" "}
             </AggregateStatusNotification>
             <AggregateStatusNotification>
-              <Icon type="pf" name="error-circle-o" />
-              {this.props.error}
+              <Icon type="pf" name="error-circle-o"/>
+              <span data-test-aggregate-error-count={true}>
+                {this.props.error}
+              </span>
             </AggregateStatusNotification>
           </AggregateStatusNotifications>
         </Card.Body>
