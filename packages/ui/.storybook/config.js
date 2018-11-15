2c5519746d5c8effc73fbfddb3c5a928e572f1ab
Riccardo Forina <rforina@redhat.com>
2018-11-15 12:01:35 +0100

    Add @storybook/addon-a11y

--- packages/ui/.storybook/config.js
+++ packages/ui/.storybook/config.js
@@ -1,6 +1,7 @@
 import { addDecorator, configure } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
 import { withInfo } from '@storybook/addon-info';
+import { checkA11y } from '@storybook/addon-a11y';
 import * as React from 'react';
 
 const req = require.context('../stories', true, /\.stories\.tsx$/);
@@ -16,4 +17,5 @@ addDecorator(
     maxPropsIntoLine: 1,
   })
 );
+addDecorator(checkA11y);
 configure(loadStories, module);
