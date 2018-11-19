bf0ebe327e757414235674723085c6dd62188201
Riccardo Forina <rforina@redhat.com>
2018-11-19 16:10:24 +0100

    Add an empty test to the main app, and fix the existin tests

--- packages/ui/src/ConnectionsListView.test.tsx
+++ packages/ui/src/ConnectionsListView.test.tsx
@@ -1,36 +1,39 @@
-import { shallow } from 'enzyme';
+import { render } from 'react-testing-library';
 import * as React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { ConnectionsListView } from './ConnectionsListView';
 
 it('renders the heading', () => {
   const noop = () => false;
-  const result = shallow(
-    <ConnectionsListView
-      linkToConnectionCreate={'/create'}
-      activeFilters={[]}
-      filterTypes={[]}
-      currentFilterType={{
-        filterType: 'text',
-        id: 'foo',
-        placeholder: 'foo placeholder',
-        title: 'Foo',
-      }}
-      currentSortType={'sort'}
-      currentValue={''}
-      isSortAscending={true}
-      resultsCount={0}
-      sortTypes={[]}
-      onUpdateCurrentValue={noop}
-      onValueKeyPress={noop}
-      onFilterAdded={noop}
-      onSelectFilterType={noop}
-      onFilterValueSelected={noop}
-      onRemoveFilter={noop}
-      onClearFilters={noop}
-      onToggleCurrentSortDirection={noop}
-      onUpdateCurrentSortType={noop}
-    />
+  const { container } = render(
+    <Router>
+      <ConnectionsListView
+        linkToConnectionCreate={'/create'}
+        activeFilters={[]}
+        filterTypes={[]}
+        currentFilterType={{
+          filterType: 'text',
+          id: 'foo',
+          placeholder: 'foo placeholder',
+          title: 'Foo',
+        }}
+        currentSortType={'sort'}
+        currentValue={''}
+        isSortAscending={true}
+        resultsCount={0}
+        sortTypes={[]}
+        onUpdateCurrentValue={noop}
+        onValueKeyPress={noop}
+        onFilterAdded={noop}
+        onSelectFilterType={noop}
+        onFilterValueSelected={noop}
+        onRemoveFilter={noop}
+        onClearFilters={noop}
+        onToggleCurrentSortDirection={noop}
+        onUpdateCurrentSortType={noop}
+      />
+    </Router>
   );
-  expect(result).toBeTruthy();
+  expect(container).toBeTruthy();
 });
