54428e31b25d9c58f99f0d2830ab7f83147b3603
Riccardo Forina <rforina@redhat.com>
2018-11-19 16:03:46 +0100

    Also move the LogViewer to the @syndesis/ui package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -15,6 +15,7 @@ export * from './IntegrationStatusDetail';
 export * from './ListViewToolbar';
 export * from './ListViewToolbarAbstractComponent';
 export * from './Loader';
+export * from './LogViewer';
 export * from './PfNavLink';
 export * from './PfVerticalNavItem';
 export * from './UnrecoverableError';
