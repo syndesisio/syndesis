b1a81634699fc83d6847da26c34372e4d78baca8
Hiram Chirino <hiram@hiramchirino.com>
2018-11-29 16:38:01 +0000

    feat: Have the syndesis build -m operator use build the operator on docker without needing multi-stage builds.

--- install/operator/build.sh
+++ install/operator/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
 
 echo ===========================================
 echo Running go build via a docker build
