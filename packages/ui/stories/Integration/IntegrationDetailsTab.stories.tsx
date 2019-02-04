d9472db16481e50bb3c3b01c63fcf7a0311fe303
kahboom <rachelyordan@gmail.com>
2019-02-04 16:06:02 -0500

    Basic imports

---
+++ packages/ui/stories/Integration/IntegrationDetailsTab.stories.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { StoryHelper } from '../../.storybook/StoryHelper';
+import { action } from '@storybook/addon-actions';
