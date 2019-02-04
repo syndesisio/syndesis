32ae9ef82a8a2b5f420269924a8a75a3b5259765
kahboom <rachelyordan@gmail.com>
2019-02-04 16:32:30 -0500

    Add int detail history story

--- packages/ui/stories/Integration/IntegrationDetailsTab.stories.tsx
+++ packages/ui/stories/Integration/IntegrationDetailsTab.stories.tsx
@@ -1,4 +1,6 @@
+/****
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { StoryHelper } from '../../.storybook/StoryHelper';
 import { action } from '@storybook/addon-actions';
+**/
