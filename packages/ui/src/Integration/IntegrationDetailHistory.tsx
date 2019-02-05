e87c8bc79ff2cf5d3c3683857da4b3eb1332dc0f
kahboom <rachelyordan@gmail.com>
2019-02-04 19:43:10 -0500

    Update history table props

--- packages/ui/src/Integration/IntegrationDetailHistory.tsx
+++ packages/ui/src/Integration/IntegrationDetailHistory.tsx
@@ -1,13 +1,17 @@
+import { ListView } from 'patternfly-react';
 import * as React from 'react';
 
 export interface IIntegrationDetailHistoryProps {
-  i18nTitle: string;
+  i18nHistory: string;
+  i18nVersion: string;
+  i18nLastPublished: string;
 }
 
 export class IntegrationDetailHistory extends React.Component<
   IIntegrationDetailHistoryProps
 > {
   public render() {
-    return <div>Integration Detail History Component</div>;
+    // return <div>Integration Detail History Component</div>;
+    return <ListView>{this.props.children}</ListView>;
   }
 }
