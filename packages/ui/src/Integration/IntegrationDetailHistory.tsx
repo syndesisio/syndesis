34135d5f0dd07fc35e1e599dc8c28ab74aa79fa6
kahboom <rachelyordan@gmail.com>
2019-02-06 20:20:19 -0500

    Consolidate tab stories into one

--- packages/ui/src/Integration/IntegrationDetailHistory.tsx
+++ packages/ui/src/Integration/IntegrationDetailHistory.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 
 export interface IIntegrationDetailHistoryProps {
   i18nHistory: string;
+  i18nTitle: string; // temporary
   i18nVersion: string;
   i18nLastPublished: string;
 }
