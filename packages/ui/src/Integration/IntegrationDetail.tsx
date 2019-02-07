8ec9945bc833eb8777511024e250a709b7598053
kahboom <rachelyordan@gmail.com>
2019-02-07 12:12:32 -0500

    Update, add new i18n strings

--- packages/ui/src/Integration/IntegrationDetail.tsx
+++ packages/ui/src/Integration/IntegrationDetail.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 
 export interface IIntegrationDetailProps {
+  i18nBtnEdit: string;
+  i18nBtnPublish: string;
+  i18nDraft: string;
   i18nHistory: string;
-  i18nTitle: string; // temporary
-  i18nVersion: string;
   i18nLastPublished: string;
+  i18nNoDescription: string;
+  i18nTabActivity: string;
+  i18nTabDetails: string;
+  i18nTableMetrics: string;
+  i18nTitle: string;
+  i18nVersion: string;
 }
 
 export class IntegrationDetail extends React.Component<
--- packages/ui/stories/Integration/IntegrationDetail.stories.tsx
+++ packages/ui/stories/Integration/IntegrationDetail.stories.tsx
@@ -5,14 +5,20 @@ import * as React from 'react';
 import { StoryHelper } from '../../.storybook/StoryHelper';
 import { IntegrationDetail } from '../../src';
 
-export const integrationDetailStory = 'example usage';
-
 const stories = storiesOf('Integration/IntegrationDetail', module);
 
+const integrationDetailStory = 'sample integration detail page';
 const storyNotes = 'Integration Detail';
 
+const textBtnEdit = 'Edit';
+const textBtnPublish = 'Publish';
+const textDraft = 'Draft';
 const textHistory = 'History';
 const textLastPublished = 'Last published';
+const textNoDescription = 'No description set...';
+const textTabActivity = 'Activity';
+const textTabDetails = 'Details';
+const textTabMetrics = 'Metrics';
 const textTitle = 'Integration Detail';
 const textVersion = 'Version';
 
@@ -22,8 +28,15 @@ stories
     integrationDetailStory,
     withNotes(storyNotes)(() => (
       <IntegrationDetail
+        i18nBtnEdit={textBtnEdit}
+        i18nBtnPublish={textBtnPublish}
+        i18nDraft={textDraft}
         i18nHistory={textHistory}
         i18nLastPublished={textLastPublished}
+        i18nNoDescription={textNoDescription}
+        i18nTabActivity={textTabActivity}
+        i18nTabDetails={textTabDetails}
+        i18nTableMetrics={textTabMetrics}
         i18nTitle={textTitle}
         i18nVersion={textVersion}
       />
