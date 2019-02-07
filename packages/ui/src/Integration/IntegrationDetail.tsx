0ce20b73285c2bd18e9da3cfcfb4fde76eb48dea
kahboom <rachelyordan@gmail.com>
2019-02-07 09:53:52 -0500

    Add history property back for i18n

--- packages/ui/src/Integration/IntegrationDetail.tsx
+++ packages/ui/src/Integration/IntegrationDetail.tsx
@@ -1,18 +1,19 @@
 import { ListView } from 'patternfly-react';
 import * as React from 'react';
 
-export interface IIntegrationDetailHistoryProps {
+export interface IIntegrationDetailProps {
+  i18n: string;
   i18nHistory: string;
   i18nTitle: string; // temporary
   i18nVersion: string;
   i18nLastPublished: string;
 }
 
-export class IntegrationDetailHistory extends React.Component<
-  IIntegrationDetailHistoryProps
+export class IntegrationDetail extends React.Component<
+  IIntegrationDetailProps
 > {
   public render() {
-    // return <div>Integration Detail History Component</div>;
+    // return <div>Integration Detail Component</div>;
     return <ListView>{this.props.children}</ListView>;
   }
 }
