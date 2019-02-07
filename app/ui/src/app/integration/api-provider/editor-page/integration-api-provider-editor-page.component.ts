16b7f9039447825750b655884814df05146807fa
Riccardo Forina <rforina@redhat.com>
2019-02-07 12:16:33 +0100

    Add support for a panes based layout in the api provider editor

--- app/ui/src/app/integration/api-provider/editor-page/integration-api-provider-editor-page.component.ts
+++ app/ui/src/app/integration/api-provider/editor-page/integration-api-provider-editor-page.component.ts
@@ -3,6 +3,7 @@ import { IntegrationEditPage } from '../../edit-page/edit-page.component';
 
 @Component({
   selector: 'syndesis-integration-api-provider-operation-editor-page',
-  templateUrl: './integration-api-provider-editor-page.component.html'
+  templateUrl: './integration-api-provider-editor-page.component.html',
+  styleUrls: ['integration-api-provider-editor-page.component.scss']
 })
 export class ApiProviderOperationsEditorComponent extends IntegrationEditPage {}
