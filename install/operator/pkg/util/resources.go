b176d1ac5f39604a16cf2dd0804ef367e23142a4
Roland Huß <roland@ro14nd.de>
2019-02-08 20:20:15 +0100

    chore: Rebased to latest operator updates

--- install/operator/pkg/util/resources.go
+++ install/operator/pkg/util/resources.go
@@ -2,14 +2,36 @@ package util
 
 import (
 	"fmt"
-	"github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	apps "github.com/openshift/api/apps/v1"
 	"io/ioutil"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"strings"
 )
+
+
+var (
+	// scheme tracks the type registry for the sdk
+	// This scheme is used to decode json data into the correct Go type based on the object's GVK
+	// All types that the operator watches must be added to this scheme
+	scheme = runtime.NewScheme()
+	codecs = serializer.NewCodecFactory(scheme)
+)
+
+func init() {
+	apps.AddToScheme(scheme)
+}
+
+func NewObjectKey(name string, namespace string) (client.ObjectKey) {
+	return client.ObjectKey{
+		Name: name,
+		Namespace: namespace,
+	}
+}
 
 func LoadKubernetesResourceFromFile(path string) (runtime.Object, error) {
 	data, err := ioutil.ReadFile(path)
@@ -34,6 +56,23 @@ func LoadKubernetesResource(jsonData []byte) (runtime.Object, error) {
 
 	obj := runtimeObjectFromUnstructured(&u)
 	return obj, nil
+}
+
+// RuntimeObjectFromUnstructured converts an unstructured to a runtime object
+func runtimeObjectFromUnstructured(u *unstructured.Unstructured) runtime.Object {
+	gvk := u.GroupVersionKind()
+	decoder := codecs.UniversalDecoder(gvk.GroupVersion());
+
+	b, err := u.MarshalJSON()
+	if err != nil {
+		panic(err)
+	}
+	ro, _, err := decoder.Decode(b, &gvk, nil)
+	if err != nil {
+		err = fmt.Errorf("failed to decode json data with gvk(%v): %v", gvk.String(), err)
+		panic(err)
+	}
+	return ro
 }
 
 func jsonIfYaml(source []byte, filename string) ([]byte, error) {
