8baa1c022b0d7f8cceba01c322d237444be9c431
kahboom <rachelyordan@gmail.com>
2019-02-25 10:53:18 -0500

    Add grid markup to listview

--- packages/ui/src/Integration/IntegrationDetailHistoryListView.tsx
+++ packages/ui/src/Integration/IntegrationDetailHistoryListView.tsx
@@ -1,3 +1,4 @@
+import { Grid } from 'patternfly-react';
 import * as React from 'react';
 
 export interface IIntegrationDetailHistoryListViewProps {
@@ -9,6 +10,28 @@ export class IntegrationDetailHistoryListView extends React.Component<
   IIntegrationDetailHistoryListViewProps
 > {
   public render() {
-    return <></>;
+    return (
+      <>
+        <Grid fluid={true} key={1}>
+          <Grid.Row className="show-grid">
+            <Grid.Col xs={2} md={2}>
+              {<span>{this.props.i18nTextDraft}:</span>}
+            </Grid.Col>
+            <Grid.Col xs={10} md={10}>
+              <p>blah</p>
+            </Grid.Col>
+          </Grid.Row>
+
+          <Grid.Row className="show-grid">
+            <Grid.Col xs={2} md={2}>
+              {<span>{this.props.i18nTextHistory}:</span>}
+            </Grid.Col>
+            <Grid.Col xs={10} md={10}>
+              <p>blah</p>
+            </Grid.Col>
+          </Grid.Row>
+        </Grid>
+      </>
+    );
   }
 }
