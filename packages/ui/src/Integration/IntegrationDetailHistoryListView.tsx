e2c7ece71ea88cf79f8ecf75094644c0c7e719cb
kahboom <rachelyordan@gmail.com>
2019-02-25 10:16:50 -0500

    Set up component for IntegrationDetailHistoryListView

---
+++ packages/ui/src/Integration/IntegrationDetailHistoryListView.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IIntegrationDetailHistoryListViewProps {
+  i18nTextDraft?: string;
+  i18nTextHistory?: string;
+}
+
+export class IntegrationDetailHistoryListView extends React.Component<
+  IIntegrationDetailHistoryListViewProps
+> {
+  public render() {
+    return <></>;
+  }
+}
