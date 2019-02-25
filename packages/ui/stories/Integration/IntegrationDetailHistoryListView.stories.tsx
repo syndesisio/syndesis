ee1f77f46ca44b2a8123b8f782757a25a12de566
kahboom <rachelyordan@gmail.com>
2019-02-25 10:22:15 -0500

    Set up story

---
+++ packages/ui/stories/Integration/IntegrationDetailHistoryListView.stories.tsx
@@ -0,0 +1,24 @@
+import { text } from '@storybook/addon-knobs';
+import { withNotes } from '@storybook/addon-notes';
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+import { IntegrationDetailHistoryListView } from '../../src';
+
+const stories = storiesOf(
+  'Integration/IntegrationDetailHistoryListView',
+  module
+);
+
+const i18nTextDraft = 'Draft';
+const i18nTextHistory = 'History';
+
+stories.add(
+  'has history',
+  withNotes('Verify there is a Publish button')(() => (
+    <IntegrationDetailHistoryListView
+      i18nTextDraft={text('i18nTextDraft', i18nTextDraft)}
+      i18nTextHistory={text('i18nTextHistory', i18nTextHistory)}
+    />
+  ))
+);
