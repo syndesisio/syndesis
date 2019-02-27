b82ba0513027a8d877361b59ed220a31c03f0293
kahboom <rachelyordan@gmail.com>
2019-02-27 13:52:46 -0500

    Prep story for stopped integration

--- packages/ui/stories/Integration/IntegrationStepsHorizontalView.stories.tsx
+++ packages/ui/stories/Integration/IntegrationStepsHorizontalView.stories.tsx
@@ -4,7 +4,13 @@ import * as React from 'react';
 
 const stories = storiesOf('Integration/IntegrationStepsHorizontalView', module);
 
-stories.add(
-  'running',
-  withNotes('Verify the integration is running')(() => <p>Placeholder</p>)
-);
+stories
+  .add(
+    'running',
+    withNotes('Verify the integration is running')(() => <p>Placeholder</p>)
+  )
+
+  .add(
+    'stopped',
+    withNotes('Verify the integration is stopped')(() => <p>Placeholder</p>)
+  );
