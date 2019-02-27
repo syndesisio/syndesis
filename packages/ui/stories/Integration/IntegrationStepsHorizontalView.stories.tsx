235cf1e82ca31f45593055859518cb56d5b6e5c8
kahboom <rachelyordan@gmail.com>
2019-02-27 14:36:17 -0500

    Flatten data structure, display properties in story

--- packages/ui/stories/Integration/IntegrationStepsHorizontalView.stories.tsx
+++ packages/ui/stories/Integration/IntegrationStepsHorizontalView.stories.tsx
@@ -4,13 +4,27 @@ import * as React from 'react';
 
 const stories = storiesOf('Integration/IntegrationStepsHorizontalView', module);
 
+const integrationRunning = {
+  steps: [
+    {
+      name: 'Timer',
+    },
+    {
+      name: 'Log',
+    },
+  ],
+};
+
+// const integrationStopped = {};
+
 stories
   .add(
     'running',
-    withNotes('Verify the integration is running')(() => <p>Placeholder</p>)
+    withNotes('Verify the integration is running')(() => (
+      <>
+        <p>{integrationRunning.steps[0].name}</p>
+      </>
+    ))
   )
 
-  .add(
-    'stopped',
-    withNotes('Verify the integration is stopped')(() => <p>Placeholder</p>)
-  );
+  .add('stopped', withNotes('Verify the integration is stopped')(() => <></>));
