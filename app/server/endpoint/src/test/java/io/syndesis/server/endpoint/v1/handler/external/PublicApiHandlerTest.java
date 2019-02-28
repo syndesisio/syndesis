8930e7a1cf7e26d1262265bc673231c9d38f382a
Dhiraj Bokde <dbokde@redhat.com>
2019-02-28 11:18:38 -0800

    Fixes #4704, add support for deleting tags across all integrations

--- app/server/endpoint/src/test/java/io/syndesis/server/endpoint/v1/handler/external/PublicApiHandlerTest.java
+++ app/server/endpoint/src/test/java/io/syndesis/server/endpoint/v1/handler/external/PublicApiHandlerTest.java
@@ -114,7 +114,7 @@ public class PublicApiHandlerTest {
     }
 
     @Test
-    public void tagForRelease() throws Exception {
+    public void testTagForRelease() throws Exception {
         final Date now = new Date();
         // delay to avoid false positives in Date::after
         Thread.sleep(1000);
@@ -131,7 +131,7 @@ public class PublicApiHandlerTest {
     }
 
     @Test
-    public void tagForReleaseByName() throws Exception {
+    public void testTagForReleaseByName() throws Exception {
         final Date now = new Date();
         // delay to avoid false positives in Date::after
         Thread.sleep(1000);
@@ -168,6 +168,18 @@ public class PublicApiHandlerTest {
         assertThat(releaseTags.isEmpty(), is(true));
 
         verify(dataManager, times(2)).fetch(Integration.class, INTEGRATION_ID);
+        verify(dataManager).update(any(Integration.class));
+    }
+
+    @Test
+    public void testDeleteEnvironment() {
+        handler.deleteEnvironment(ENVIRONMENT);
+
+        final List<String> environments = handler.getReleaseEnvironments();
+
+        assertThat(environments, notNullValue());
+        assertThat(environments.isEmpty(), is(true));
+
         verify(dataManager).update(any(Integration.class));
     }
 
