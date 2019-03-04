5b5ef7a6551e9b1e9c8c825d54d2ac052d6a8af5
kahboom <rachelyordan@gmail.com>
2019-03-04 09:58:37 -0500

    Rm icon prop, as that is step-specific; update storybook data
    Fix up markup
    
    Set up grid system again

--- packages/ui/src/Integration/IntegrationStepsHorizontalView.tsx
+++ packages/ui/src/Integration/IntegrationStepsHorizontalView.tsx
@@ -2,7 +2,6 @@ import { Grid, Icon } from 'patternfly-react';
 import * as React from 'react';
 
 export interface IIntegrationStepsHorizontalViewProps {
-  // icon?: any;
   steps?: any;
 }
 
@@ -11,33 +10,42 @@ export class IntegrationStepsHorizontalView extends React.Component<
 > {
   public render() {
     return (
-      <Grid fluid={true} key={1}>
-        {this.props.steps &&
-          this.props.steps.map((opt: any) => (
-            <p key={opt.id}>blah, {opt.name}</p>
-          ))}
-        {this.props.steps ? (
+      <>
+        <Grid fluid={true}>
           <Grid.Row className="show-grid">
-            <Grid.Col xs={2} md={2}>
-              <Icon name={'external-link'} />
+            <Grid.Col xs={6} md={4}>
+              {this.props.steps && this.props.steps[0] ? (
+                <Icon name={'external-link'} />
+              ) : null}
             </Grid.Col>
-            <Grid.Col xs={10} md={10}>
-              <span>Hello:</span>
-            </Grid.Col>
+            {this.props.steps &&
+              this.props.steps.map((opt: any) => (
+                <>
+                  <Grid.Col xs={6} md={4}>
+                    <Icon name={'angle-right'} />
+                  </Grid.Col>
+                  <Grid.Col xsHidden={true} md={4}>
+                    <span>
+                      <p key={opt.id}>{opt.name}</p>
+                    </span>
+                  </Grid.Col>
+                </>
+              ))}
           </Grid.Row>
-        ) : null}
-
-        {this.props.steps ? (
-          <Grid.Row className="show-grid">
-            <Grid.Col xs={2} md={2}>
-              {<span>Step:</span>}
-            </Grid.Col>
-            <Grid.Col xs={10} md={10}>
-              {this.props.steps ? <span>Goodbye</span> : null}
-            </Grid.Col>
-          </Grid.Row>
-        ) : null}
-      </Grid>
+        </Grid>
+        <Grid fluid={true}>
+          {this.props.steps ? (
+            <Grid.Row className="show-grid">
+              <Grid.Col xs={2} md={2}>
+                {<span>Step:</span>}
+              </Grid.Col>
+              <Grid.Col xs={10} md={10}>
+                {this.props.steps ? <span>Goodbye</span> : null}
+              </Grid.Col>
+            </Grid.Row>
+          ) : null}
+        </Grid>
+      </>
     );
   }
 }
