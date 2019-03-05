0499f5f67eb2ca80b0fc26a58c452f36767a41ea
kahboom <rachelyordan@gmail.com>
2019-03-05 13:50:35 -0500

    Display icons for connectors
    Rm stopped int

--- packages/ui/src/Integration/IntegrationStepsHorizontalView.tsx
+++ packages/ui/src/Integration/IntegrationStepsHorizontalView.tsx
@@ -15,9 +15,12 @@ export class IntegrationStepsHorizontalView extends React.Component<
           <Grid.Row className="show-grid">
             <Grid.Col xs={6} md={4}>
               {this.props.steps && this.props.steps[0] ? (
-                <span>
-                  <p>{this.props.steps[0].name}</p>
-                </span>
+                <>
+                  <img src={this.props.steps[0].icon} />
+                  <span>
+                    <p>{this.props.steps[0].name}</p>
+                  </span>
+                </>
               ) : null}
             </Grid.Col>
             {this.props.steps &&
@@ -28,6 +31,7 @@ export class IntegrationStepsHorizontalView extends React.Component<
                   </Grid.Col>
                   <Grid.Col xsHidden={true} md={4}>
                     <span>
+                      <img src={opt.icon} />
                       <p key={index}>{opt.name}</p>
                     </span>
                   </Grid.Col>
