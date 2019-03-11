16fce7f37c4fea59dd9e303b1ebc0a6543564da0
Riccardo Forina <rforina@redhat.com>
2019-03-11 12:07:11 +0100

    Record and replay a remote backend

---
+++ syndesis/src/setupProxy.js
@@ -0,0 +1,43 @@
+/* tslint:disable */
+const talkback = require('talkback');
+const proxy = require('http-proxy-middleware');
+const filenamify = require('filenamify');
+const config = require('../public/config.json');
+
+// Disable self-signed certificate check
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const backendName = filenamify(process.env.BACKEND);
+
+const server = talkback({
+  host: process.env.BACKEND,
+  port: 556,
+  path: __dirname + '/../tapes/' + backendName,
+  record: process.env.MOCKS_RECORD,
+  ignoreHeaders: [
+    'content-length',
+    'host',
+    'cookie',
+    'cache-control',
+    'pragma',
+  ],
+  summary: false,
+  debug: true,
+});
+server.start(() => console.log('Talkback Started'));
+
+module.exports = function(app) {
+  app.use(
+    proxy('/api', {
+      target: 'http://localhost:556',
+      secure: false,
+      changeOrigin: true,
+      ws: false,
+      headers: {
+        'X-Forwarded-Origin': 'for=127.0.0.1;host=localhost:3000;proto=https',
+        'X-Forwarded-Access-Token': 'supersecret',
+        Cookie: process.env.BACKEND_COOKIE || '',
+      },
+    })
+  );
+};
