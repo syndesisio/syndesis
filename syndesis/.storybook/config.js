6a42e688c49157b1b8c35f0a5ae8d8783eaf5cbd
Riccardo Forina <rforina@redhat.com>
2019-03-19 15:32:55 +0100

    syndesis stories working

--- syndesis/.storybook/config.js
+++ syndesis/.storybook/config.js
@@ -1,9 +1,41 @@
 import '../../packages/.storybook/config';
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import { ApiContext } from '@syndesis/api';
+import { AppContext } from '../src/app';
+import { MemoryRouter as Router } from 'react-router-dom';
+import * as React from 'react';
+import config from '../config.storybook.json';
 
 /* load all stories in the story folder */
 const req = require.context('../stories', true, /\.stories\.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+const AppContextStorybook = storyFn => (
+  <Router>
+    <AppContext.Provider
+      value={{
+        config: config,
+        getPodLogUrl: () => '',
+        hideNavigation: () => void 0,
+        logout: () => void 0,
+        showNavigation: () => void 0,
+      }}
+    >
+      <ApiContext.Provider
+        value={{
+          apiUri: `${config.apiBase}${config.apiEndpoint}`,
+          dvApiUri: `${config.apiBase}${config.datavirt.dvUrl}`,
+          headers: { 'SYNDESIS-XSRF-TOKEN': 'awesome' },
+        }}
+      >
+        {storyFn()}
+      </ApiContext.Provider>
+    </AppContext.Provider>
+  </Router>
+);
+
+addDecorator(AppContextStorybook);
+
 configure(loadStories, module);
