9bdd8e109f00b7ad0ad84e8ae631e8b767c57b4d
kahboom <rachelyordan@gmail.com>
2019-03-19 23:20:35 -0400

    Set up Integration Detail tab as page

--- syndesis/src/modules/integrations/pages/integration/detail/main/DetailsPage.tsx
+++ syndesis/src/modules/integrations/pages/integration/detail/main/DetailsPage.tsx
@@ -1,3 +1,25 @@
+import { Integration } from '@syndesis/models';
+// import { IntegrationDetailTab } from '@syndesis/ui';
+import { WithRouteData } from '@syndesis/utils';
 import * as React from 'react';
+// import { PageTitle } from '../../../../../../containers/PageTitle';
 
-export class DetailsPage extends React.Component {}
+export interface IViewIntegrationRouteState {
+  /**
+   * the integration object being viewed
+   */
+  integration: Integration;
+}
+
+export class DetailsPage extends React.Component {
+  public render() {
+    return (
+      <WithRouteData<null, IViewIntegrationRouteState>>
+        {(_, { integration }) => (
+          // <IntegrationDetailTab content={}/>
+          <></>
+        )}
+      </WithRouteData>
+    );
+  }
+}
