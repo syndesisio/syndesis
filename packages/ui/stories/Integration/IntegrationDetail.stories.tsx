161f22c8c391bf26cb80269939f12a46868f73a7
kahboom <rachelyordan@gmail.com>
2019-03-21 09:43:13 -0400

    Add Router to IntegrationDetail stories

--- packages/ui/stories/Integration/IntegrationDetail.stories.tsx
+++ packages/ui/stories/Integration/IntegrationDetail.stories.tsx
@@ -1,6 +1,7 @@
 // import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import {
   Container,
   IntegrationDetailHistoryListView,
@@ -9,20 +10,22 @@ import {
 } from '../../src';
 
 storiesOf('Integration/Detail', module).add('integration published', () => (
-  <>
-    <Container
-      style={{
-        background: '#fff',
-      }}
-    >
-      <TabBar>
-        <TabBarItem
-          label={'integrationDetailTitle'}
-          to={'#integration-detail'}
-        />
-        <TabBarItem label={'integrationDetailTitle'} to={'#detail'} />
-      </TabBar>
-    </Container>
-    <IntegrationDetailHistoryListView integrationIsDraft={false} />
-  </>
+  <Router>
+    <>
+      <Container
+        style={{
+          background: '#fff',
+        }}
+      >
+        <TabBar>
+          <TabBarItem
+            label={'integrationDetailTitle'}
+            to={'#integration-detail'}
+          />
+          <TabBarItem label={'integrationDetailTitle'} to={'#detail'} />
+        </TabBar>
+      </Container>
+      <IntegrationDetailHistoryListView integrationIsDraft={false} />
+    </>
+  </Router>
 ));
