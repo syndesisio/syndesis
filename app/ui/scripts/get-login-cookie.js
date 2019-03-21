84eaa8e8e1c6ba3ab6f315f0354b31983aa23f58
Stan Lewis <slewis@fusesource.com>
2019-03-21 14:28:20 -0400

    feat: add a 'ui' command to 'syndesis'; fixes #5006

---
+++ app/ui/scripts/get-login-cookie.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const [nodeBinary, script, url] = process.argv;
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    ignoreHTTPSErrors: true,
+  });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  //code to wait for user to enter username and password, and click `login`
+
+  await browser.waitForTarget(target => target.url() === `${url}/`);
+
+  await page.waitForNavigation({
+    waitUntil: 'load',
+  });
+  const cookies = await page.cookies();
+  // Don't print anything else out of this file except the cookie value :-)
+  console.log(cookies.map(c => `${c.name}=${c.value}`).join(';'));
+  await browser.close();
+})();
