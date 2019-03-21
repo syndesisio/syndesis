e0a6800ac10f51a882dde43ebc2944315f5d7ec4
Christoph Deppisch <cdeppisch@redhat.com>
2019-03-21 13:26:28 +0000

    chore: add more unit tests for SQL connector

--- app/connector/sql/src/test/java/io/syndesis/connector/sql/SqlConnectorTest.java
+++ app/connector/sql/src/test/java/io/syndesis/connector/sql/SqlConnectorTest.java
@@ -16,40 +16,49 @@
 package io.syndesis.connector.sql;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
+import io.syndesis.common.model.integration.Step;
 import io.syndesis.connector.sql.common.JSONBeanUtil;
 import io.syndesis.connector.sql.util.SqlConnectorTestSupport;
-import io.syndesis.common.model.integration.Step;
-import org.assertj.core.api.Assertions;
-import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 @SuppressWarnings({"PMD.SignatureDeclareThrowsException", "PMD.JUnitTestsShouldIncludeAssert"})
+@RunWith(Parameterized.class)
 public class SqlConnectorTest extends SqlConnectorTestSupport {
 
-    // **************************
-    // Set up
-    // **************************
+    private final String sqlQuery;
+    private final List<Map<String, String[]>> expectedResults;
+    private final Map<String, Object> parameters;
 
-    @Override
-    protected void doPreSetup() throws Exception {
-        try (Statement stmt = db.connection.createStatement()) {
-            //stmt.executeUpdate("DROP TABLE ADDRESS");
-            stmt.executeUpdate("CREATE TABLE ADDRESS (street VARCHAR(255), number INTEGER)");
-        }
+    public SqlConnectorTest(String sqlQuery, List<Map<String, String[]>> expectedResults, Map<String, Object> parameters) {
+        this.sqlQuery = sqlQuery;
+        this.expectedResults = expectedResults;
+        this.parameters = parameters;
     }
 
-    @After
-    public void after() throws SQLException {
-        try (Statement stmt = db.connection.createStatement()) {
-            stmt.executeUpdate("DROP TABLE ADDRESS");
-        }
+    @Override
+    protected List<String> setupStatements() {
+        return Collections.singletonList("CREATE TABLE ADDRESS (street VARCHAR(255), number INTEGER)");
+    }
+
+    @Override
+    protected List<String> cleanupStatements() {
+        return Collections.singletonList("DROP TABLE ADDRESS");
     }
 
     @Override
@@ -60,11 +69,29 @@ public class SqlConnectorTest extends SqlConnectorTestSupport {
                 builder -> builder.putConfiguredProperty("name", "start")),
             newSqlEndpointStep(
                 "sql-connector",
-                builder -> builder.putConfiguredProperty("query", "INSERT INTO ADDRESS VALUES (:#street, :#number)")),
+                builder -> builder.putConfiguredProperty("query", sqlQuery)),
             newSimpleEndpointStep(
                 "log",
                 builder -> builder.putConfiguredProperty("loggerName", "test"))
         );
+    }
+
+    // **************************
+    // Parameters
+    // **************************
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("number", 14);
+        parameters.put("street", "LaborInVain");
+
+        return Arrays.asList(new Object[][] {
+                { "INSERT INTO ADDRESS VALUES ('East Davie Street', 100)", Arrays.asList(Collections.singletonMap("NUMBER", new String[] { "100" }),
+                        Collections.singletonMap("STREET", new String[] { "East Davie Street" })), Collections.emptyMap()},
+                { "INSERT INTO ADDRESS VALUES (:#street, :#number)", Arrays.asList(Collections.singletonMap("NUMBER", new String[] { "14" }),
+                        Collections.singletonMap("STREET", new String[] { "LaborInVain" })), parameters}
+        });
     }
 
     // **************************
@@ -73,20 +100,52 @@ public class SqlConnectorTest extends SqlConnectorTestSupport {
 
     @Test
     public void sqlConnectorTest() throws Exception {
+        String body;
+        if (parameters.isEmpty()) {
+            body = null;
+        } else {
+            body = JSONBeanUtil.toJSONBean(parameters);
+        }
 
-        Map<String,Object> values = new HashMap<>();
-        values.put("street", "LaborInVain");
-        values.put("number", 14);
-        String body = JSONBeanUtil.toJSONBean(values);
-
-        String result = template.requestBody("direct:start", body, String.class);
+        template.requestBody("direct:start", body, List.class);
 
         try (Statement stmt = db.connection.createStatement()) {
             stmt.execute("SELECT * FROM ADDRESS");
-            ResultSet resultSet = stmt.getResultSet();
-            resultSet.next();
-            Assertions.assertThat(resultSet.getString(1)).isEqualTo("LaborInVain");
-            Assertions.assertThat(resultSet.getInt(2)).isEqualTo(14);
+            List<Properties> jsonBeans = resultSetToList(stmt.getResultSet())
+                                                    .stream()
+                                                    .map(raw -> {
+                                                        Properties properties = new Properties();
+                                                        properties.putAll(raw);
+                                                        return properties;
+                                                    })
+                                                    .collect(Collectors.toList());
+
+            Assert.assertEquals(expectedResults.isEmpty(), jsonBeans.isEmpty());
+
+            for (Map<String, String[]> result : expectedResults) {
+                for (Map.Entry<String, String[]> resultEntry : result.entrySet()) {
+                    validateProperty(jsonBeans, resultEntry.getKey(), resultEntry.getValue());
+                }
+            }
         }
+    }
+
+    // **************************
+    // Helpers
+    // **************************
+
+    private List<Map<String, Object>> resultSetToList(ResultSet rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int columns = md.getColumnCount();
+        List<Map<String, Object>> list = new ArrayList<>();
+        while (rs.next()){
+            Map<String, Object> row = new HashMap<>(columns);
+            for(int i = 1; i <= columns; ++i){
+                row.put(md.getColumnName(i).toUpperCase(), rs.getObject(i));
+            }
+            list.add(row);
+        }
+
+        return list;
     }
 }
--- app/connector/sql/src/test/java/io/syndesis/connector/sql/SqlStartConnectorTest.java
+++ app/connector/sql/src/test/java/io/syndesis/connector/sql/SqlStartConnectorTest.java
@@ -15,9 +15,11 @@
  */
 package io.syndesis.connector.sql;
 
-import java.sql.Statement;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
@@ -29,21 +31,31 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 @SuppressWarnings({"PMD.SignatureDeclareThrowsException"})
+@RunWith(Parameterized.class)
 public class SqlStartConnectorTest extends SqlConnectorTestSupport {
 
-    // **************************
-    // Set up
-    // **************************
+    private final String sqlQuery;
+    private final List<Map<String, String[]>> expectedResults;
+
+    public SqlStartConnectorTest(String sqlQuery, List<Map<String, String[]>> expectedResults) {
+        this.sqlQuery = sqlQuery;
+        this.expectedResults = expectedResults;
+    }
 
     @Override
-    protected void doPreSetup() throws Exception {
-        try (Statement stmt = db.connection.createStatement()) {
-            stmt.executeUpdate("CREATE TABLE NAME (id INTEGER PRIMARY KEY, firstName VARCHAR(255), lastName VARCHAR(255))");
-            stmt.executeUpdate("INSERT INTO NAME VALUES (1, 'Joe', 'Jackson')");
-            stmt.executeUpdate("INSERT INTO NAME VALUES (2, 'Roger', 'Waters')");
-        }
+    protected List<String> cleanupStatements() {
+        return Collections.singletonList("DROP TABLE NAME");
+    }
+
+    @Override
+    protected List<String> setupStatements() {
+        return Arrays.asList("CREATE TABLE NAME (id INTEGER PRIMARY KEY, firstName VARCHAR(255), lastName VARCHAR(255))",
+                "INSERT INTO NAME VALUES (1, 'Joe', 'Jackson')",
+                "INSERT INTO NAME VALUES (2, 'Roger', 'Waters')");
     }
 
     @Override
@@ -54,7 +66,7 @@ public class SqlStartConnectorTest extends SqlConnectorTestSupport {
                 builder -> builder.putConfiguredProperty("name", "start")),
             newSqlEndpointStep(
                 "sql-start-connector",
-                builder -> builder.putConfiguredProperty("query", "SELECT * FROM NAME ORDER BY id")),
+                builder -> builder.putConfiguredProperty("query", sqlQuery)),
             newSimpleEndpointStep(
                 "log",
                 builder -> builder.putConfiguredProperty("loggerName", "test")),
@@ -62,6 +74,23 @@ public class SqlStartConnectorTest extends SqlConnectorTestSupport {
                 "mock",
                 builder -> builder.putConfiguredProperty("name", "result"))
         );
+    }
+
+    // **************************
+    // Parameters
+    // **************************
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { "SELECT * FROM NAME ORDER BY id", Arrays.asList(Collections.singletonMap("ID", new String[] { "1", "2" }),
+                        Collections.singletonMap("FIRSTNAME", new String[] { "Joe", "Roger" }),
+                        Collections.singletonMap("LASTNAME", new String[] { "Jackson", "Waters" }))},
+                { "SELECT * FROM NAME WHERE id = 2", Arrays.asList(Collections.singletonMap("ID", new String[] { "2" }),
+                        Collections.singletonMap("FIRSTNAME", new String[] { "Roger" }),
+                        Collections.singletonMap("LASTNAME", new String[] { "Waters" }))},
+                { "SELECT * FROM NAME WHERE id = 99", Collections.emptyList()}
+        });
     }
 
     // **************************
@@ -85,18 +114,12 @@ public class SqlStartConnectorTest extends SqlConnectorTestSupport {
                 .map(JSONBeanUtil::parsePropertiesFromJSONBean)
                 .collect(Collectors.toList());
 
-        validateProperty(jsonBeans, "ID", "1", "2");
-        validateProperty(jsonBeans, "FIRSTNAME", "Joe", "Roger");
-        validateProperty(jsonBeans, "LASTNAME", "Jackson", "Waters");
-    }
+        Assert.assertEquals(expectedResults.isEmpty(), jsonBeans.isEmpty());
 
-    // **************************
-    // Helpers
-    // **************************
-
-    private void validateProperty(List<Properties> jsonBeans, String propertyName, String ... expectedValues) {
-        for (int i = 0; i < expectedValues.length; i++) {
-            Assert.assertEquals(expectedValues[i], jsonBeans.get(i).getProperty(propertyName));
+        for (Map<String, String[]> result : expectedResults) {
+            for (Map.Entry<String, String[]> resultEntry : result.entrySet()) {
+                validateProperty(jsonBeans, resultEntry.getKey(), resultEntry.getValue());
+            }
         }
     }
 }
