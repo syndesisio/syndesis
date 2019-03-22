dad310105c4e054d5cdba5d741497190148f0499
kahboom <rachelyordan@gmail.com>
2019-03-22 16:48:10 -0400

    Rm unused import

--- packages/ui/stories/Integration/IntegrationDetail.stories.tsx
+++ packages/ui/stories/Integration/IntegrationDetail.stories.tsx
@@ -1,4 +1,3 @@
-// import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
