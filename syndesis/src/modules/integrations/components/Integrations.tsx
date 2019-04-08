0bc8f6737fdf25af41f51baa7f68ba6015a01b92
Riccardo Forina <rforina@redhat.com>
2019-04-08 20:41:47 +0200

    Make the integration list more resilient to errors

--- syndesis/src/modules/integrations/components/Integrations.tsx
+++ syndesis/src/modules/integrations/components/Integrations.tsx
@@ -5,6 +5,7 @@ import {
   IntegrationsList,
   IntegrationsListItem,
   IntegrationsListItemActions,
+  IntegrationsListItemUnreadable,
   IntegrationsListSkeleton,
 } from '@syndesis/ui';
 import { WithLoader } from '@syndesis/utils';
@@ -37,104 +38,122 @@ export class Integrations extends React.Component<IIntegrationsProps> {
                     >
                       {() =>
                         this.props.integrations.map(
-                          (mi: IntegrationWithMonitoring, index) => {
-                            const editAction: IIntegrationAction = {
-                              href: resolvers.integration.edit.index({
-                                integration: mi.integration,
-                              }),
-                              label: 'Edit',
-                            };
-                            const startAction: IIntegrationAction = {
-                              label: 'Start',
-                              onClick: () => alert('todo: start'),
-                            };
-                            const stopAction: IIntegrationAction = {
-                              label: 'Stop',
-                              onClick: () => alert('todo: stop'),
-                            };
-                            const deleteAction: IIntegrationAction = {
-                              label: 'Delete',
-                              onClick: () => alert('todo: delete'),
-                            };
-                            const exportAction: IIntegrationAction = {
-                              label: 'Export',
-                              onClick: () => alert('todo: export'),
-                            };
-                            const actions: IIntegrationAction[] = [];
-                            if (canEdit(mi.integration)) {
-                              actions.push(editAction);
+                          (mi: IntegrationWithMonitoring) => {
+                            try {
+                              const editAction: IIntegrationAction = {
+                                href: resolvers.integration.edit.index({
+                                  integration: mi.integration,
+                                }),
+                                label: 'Edit',
+                              };
+                              const startAction: IIntegrationAction = {
+                                label: 'Start',
+                                onClick: () => alert('todo: start'),
+                              };
+                              const stopAction: IIntegrationAction = {
+                                label: 'Stop',
+                                onClick: () => alert('todo: stop'),
+                              };
+                              const deleteAction: IIntegrationAction = {
+                                label: 'Delete',
+                                onClick: () => alert('todo: delete'),
+                              };
+                              const exportAction: IIntegrationAction = {
+                                label: 'Export',
+                                onClick: () => alert('todo: export'),
+                              };
+                              const actions: IIntegrationAction[] = [];
+                              if (canEdit(mi.integration)) {
+                                actions.push(editAction);
+                              }
+                              if (canActivate(mi.integration)) {
+                                actions.push(startAction);
+                              }
+                              if (canDeactivate(mi.integration)) {
+                                actions.push(stopAction);
+                              }
+                              actions.push(deleteAction);
+                              actions.push(exportAction);
+                              return (
+                                <IntegrationsListItem
+                                  key={mi.integration.id}
+                                  integrationName={mi.integration.name}
+                                  currentState={mi.integration!.currentState!}
+                                  targetState={mi.integration!.targetState!}
+                                  isConfigurationRequired={
+                                    !!(
+                                      mi.integration!.board!.warnings ||
+                                      mi.integration!.board!.errors ||
+                                      mi.integration!.board!.notices
+                                    )
+                                  }
+                                  monitoringValue={
+                                    mi.monitoring &&
+                                    t(
+                                      'integrations:' +
+                                        mi.monitoring.detailedState.value
+                                    )
+                                  }
+                                  monitoringCurrentStep={
+                                    mi.monitoring &&
+                                    mi.monitoring.detailedState.currentStep
+                                  }
+                                  monitoringTotalSteps={
+                                    mi.monitoring &&
+                                    mi.monitoring.detailedState.totalSteps
+                                  }
+                                  monitoringLogUrl={getPodLogUrl(
+                                    config,
+                                    mi.monitoring
+                                  )}
+                                  startConnectionIcon={
+                                    mi.integration.flows![0].steps![0]
+                                      .connection!.icon!
+                                  }
+                                  finishConnectionIcon={
+                                    mi.integration.flows![0].steps![
+                                      mi.integration.flows![0].steps!.length - 1
+                                    ].connection!.icon!
+                                  }
+                                  actions={
+                                    <IntegrationsListItemActions
+                                      integrationId={mi.integration!.id!}
+                                      actions={actions}
+                                    />
+                                  }
+                                  i18nConfigurationRequired={t(
+                                    'integrations:ConfigurationRequired'
+                                  )}
+                                  i18nError={t('shared:Error')}
+                                  i18nPublished={t('shared:Published')}
+                                  i18nUnpublished={t('shared:Unpublished')}
+                                  i18nProgressPending={t('shared:Pending')}
+                                  i18nProgressStarting={t(
+                                    'integrations:progressStarting'
+                                  )}
+                                  i18nProgressStopping={t(
+                                    'integrations:progressStopping'
+                                  )}
+                                  i18nLogUrlText={t('shared:viewLogs')}
+                                />
+                              );
+                            } catch (e) {
+                              return (
+                                <IntegrationsListItemUnreadable
+                                  key={mi.integration.id}
+                                  integrationName={
+                                    (mi &&
+                                      mi.integration &&
+                                      mi.integration.name) ||
+                                    'An integration'
+                                  }
+                                  i18nDescription={
+                                    "Sorry, we can't display more information about this integration right now."
+                                  }
+                                  rawObject={JSON.stringify(mi.integration)}
+                                />
+                              );
                             }
-                            if (canActivate(mi.integration)) {
-                              actions.push(startAction);
-                            }
-                            if (canDeactivate(mi.integration)) {
-                              actions.push(stopAction);
-                            }
-                            actions.push(deleteAction);
-                            actions.push(exportAction);
-                            return (
-                              <IntegrationsListItem
-                                key={index}
-                                integrationName={mi.integration.name}
-                                currentState={mi.integration!.currentState!}
-                                targetState={mi.integration!.targetState!}
-                                isConfigurationRequired={
-                                  !!(
-                                    mi.integration!.board!.warnings ||
-                                    mi.integration!.board!.errors ||
-                                    mi.integration!.board!.notices
-                                  )
-                                }
-                                monitoringValue={
-                                  mi.monitoring &&
-                                  t(
-                                    'integrations:' +
-                                      mi.monitoring.detailedState.value
-                                  )
-                                }
-                                monitoringCurrentStep={
-                                  mi.monitoring &&
-                                  mi.monitoring.detailedState.currentStep
-                                }
-                                monitoringTotalSteps={
-                                  mi.monitoring &&
-                                  mi.monitoring.detailedState.totalSteps
-                                }
-                                monitoringLogUrl={getPodLogUrl(
-                                  config,
-                                  mi.monitoring
-                                )}
-                                startConnectionIcon={
-                                  mi.integration.flows![0].steps![0].connection!
-                                    .icon!
-                                }
-                                finishConnectionIcon={
-                                  mi.integration.flows![0].steps![
-                                    mi.integration.flows![0].steps!.length - 1
-                                  ].connection!.icon!
-                                }
-                                actions={
-                                  <IntegrationsListItemActions
-                                    integrationId={mi.integration!.id!}
-                                    actions={actions}
-                                  />
-                                }
-                                i18nConfigurationRequired={t(
-                                  'integrations:ConfigurationRequired'
-                                )}
-                                i18nError={t('shared:Error')}
-                                i18nPublished={t('shared:Published')}
-                                i18nUnpublished={t('shared:Unpublished')}
-                                i18nProgressPending={t('shared:Pending')}
-                                i18nProgressStarting={t(
-                                  'integrations:progressStarting'
-                                )}
-                                i18nProgressStopping={t(
-                                  'integrations:progressStopping'
-                                )}
-                                i18nLogUrlText={t('shared:viewLogs')}
-                              />
-                            );
                           }
                         )
                       }
