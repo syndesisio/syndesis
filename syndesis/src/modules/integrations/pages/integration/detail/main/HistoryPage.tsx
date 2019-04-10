4f12801a96d603bcca0b08e4eaeb2f3182672d3c
kahboom <rachelyordan@gmail.com>
2019-04-10 11:10:46 +0100

    Add WithRouteData, addl setup

--- syndesis/src/modules/integrations/pages/integration/detail/main/HistoryPage.tsx
+++ syndesis/src/modules/integrations/pages/integration/detail/main/HistoryPage.tsx
@@ -1,9 +1,23 @@
+// import { WithIntegrations } from '@syndesis/api';
+import { Integration } from '@syndesis/models';
+// import { IntegrationDetailHistoryListView, IntegrationDetailHistoryListViewItem } from '@syndesis/ui';
+// import { Loader } from '@syndesis/ui';
 import { WithRouteData } from '@syndesis/utils';
 import * as React from 'react';
 
-interface IHistoryPageParams {
+/**
+ * @integrationId - the ID of the integration for which details are being displayed.
+ */
+export interface IHistoryPageParams {
+  integration: Integration;
   integrationId: string;
 }
+
+export interface IHistoryPageState {
+  integration: Integration;
+}
+
+// const integrationHistoryItems: IntegrationDetailHistoryListViewItem[];
 
 /**
  * This page shows the first, and default, tab of the Integration Detail page.
@@ -14,9 +28,13 @@ interface IHistoryPageParams {
 export class HistoryPage extends React.Component {
   public render() {
     return (
-      <WithRouteData<IHistoryPageParams, null>>
-        {({ integrationId }, {}) => {
-          //
+      <WithRouteData<IHistoryPageParams, IHistoryPageState>>
+        {({ integrationId }, { integration }, { history }) => {
+          return (
+            <div>
+              <p>This is the Integration Detail History page.</p>
+            </div>
+          );
         }}
       </WithRouteData>
     );
