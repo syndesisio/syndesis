113908ac64ab0e4ed7627fb51078037f41a2baf6
kahboom <rachelyordan@gmail.com>
2019-04-16 09:56:52 +0100

    Add PF markup for expandable list view

--- app/ui-react/packages/ui/src/Integration/IntegrationDetailActivity.tsx
+++ app/ui-react/packages/ui/src/Integration/IntegrationDetailActivity.tsx
@@ -1,3 +1,11 @@
+import {
+  Col,
+  ListView,
+  ListViewIcon,
+  ListViewInfoItem,
+  ListViewItem,
+  Row,
+} from 'patternfly-react';
 import * as React from 'react';
 
 export interface IIntegrationDetailActivityProps {}
@@ -6,6 +14,28 @@ export class IntegrationDetailActivity extends React.Component<
   IIntegrationDetailActivityProps
 > {
   public render() {
-    return <></>;
+    return (
+      <ListView>
+        <ListViewItem
+          actions={<div />}
+          checkboxInput={<input />}
+          leftContent={<ListViewIcon />}
+          additionalInfo={[
+            <ListViewInfoItem />,
+            <ListViewInfoItem />,
+            <ListViewInfoItem />,
+          ]}
+          heading="Item 1"
+          description="This is Item 1 description"
+        >
+          <Row>
+            <Col sm={11}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry
+            </Col>
+          </Row>
+        </ListViewItem>
+      </ListView>
+    );
   }
 }
