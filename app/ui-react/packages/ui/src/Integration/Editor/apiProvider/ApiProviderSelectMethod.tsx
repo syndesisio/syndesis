e577981853b8213c40810636ba406eb20d9943d4
kahboom <rachelyordan@gmail.com>
2019-05-24 10:55:19 +0100

    Switch to form for Select Method

--- app/ui-react/packages/ui/src/Integration/Editor/apiProvider/ApiProviderSelectMethod.tsx
+++ app/ui-react/packages/ui/src/Integration/Editor/apiProvider/ApiProviderSelectMethod.tsx
@@ -1,5 +1,13 @@
-import { Text } from '@patternfly/react-core';
-import { FormControl, ListView } from 'patternfly-react';
+import {
+  Col,
+  ControlLabel,
+  Form,
+  FormControl,
+  FormGroup,
+  Grid,
+  Radio,
+  Row,
+} from 'patternfly-react';
 import * as React from 'react';
 
 import { Container } from '../../../Layout';
@@ -13,7 +21,6 @@ export interface IApiProviderSelectMethodProps {
   /**
    * Localized strings to be displayed.
    */
-  i18nDescription?: string;
   i18nHelpMessage?: string;
   i18nInstructions: string;
   i18nMethodFromFile: string;
@@ -26,10 +33,12 @@ export interface IApiProviderSelectMethodProps {
   i18nUploadSuccessMessage?: string;
   i18nUploadSuccessMessages?: string[];
   i18nUrlNote: string;
+
   /**
    * Callback for when one or more file uploads have been accepted.
    */
   onUploadAccepted(file: File[]): void;
+
   /**
    * Obtains the localized text (may include HTML tags) that appears when the file upload was rejected. This
    * will occur when a DnD of a file with the wrong extension is dropped. This message is presented
@@ -43,57 +52,61 @@ export class ApiProviderSelectMethod extends React.Component<
 > {
   public render() {
     return (
-      <>
-        <Text>{this.props.i18nDescription}</Text>
-        <ListView
-          id={'api-provider-select-method__list'}
-          className={'api-provider-select-method__list'}
-        >
-          <ListView.Item
-            key={'1'}
-            checkboxInput={<input type="radio" />}
-            heading={this.props.i18nMethodFromFile}
-            stacked={false}
-          >
-            <Container style={{ margin: '50px' }}>
-              <DndFileChooser
-                allowMultiple={this.props.allowMultiple}
-                disableDropzone={this.props.disableDropzone}
-                fileExtensions={this.props.fileExtensions}
-                i18nHelpMessage={this.props.i18nHelpMessage}
-                i18nInstructions={this.props.i18nInstructions}
-                i18nNoFileSelectedMessage={this.props.i18nNoFileSelectedMessage}
-                i18nSelectedFileLabel={this.props.i18nSelectedFileLabel}
-                i18nUploadFailedMessage={this.props.i18nUploadFailedMessage}
-                i18nUploadFailedMessages={this.props.i18nUploadFailedMessages}
-                i18nUploadSuccessMessage={this.props.i18nUploadSuccessMessage}
-                i18nUploadSuccessMessages={this.props.i18nUploadSuccessMessages}
-                onUploadAccepted={this.props.onUploadAccepted}
-                onUploadRejected={this.props.onUploadRejected}
-              />
-            </Container>
-          </ListView.Item>
-          <ListView.Item
-            key={'2'}
-            checkboxInput={<input type="radio" />}
-            heading={this.props.i18nMethodFromUrl}
-            stacked={false}
-          >
-            <div>
-              <FormControl type={'text'} />
-            </div>
-            <div>
-              <span className={'url-note'}>{this.props.i18nUrlNote}</span>
-            </div>
-          </ListView.Item>
-          <ListView.Item
-            key={'3'}
-            checkboxInput={<input type="radio" />}
-            heading={this.props.i18nMethodFromScratch}
-            stacked={false}
-          />
-        </ListView>
-      </>
+      <Grid>
+        <Form>
+          <Row>
+            <Col>
+              <FormGroup controlId={'method'} disabled={false}>
+                <div>
+                  <Radio name={'method'}>
+                    <ControlLabel>{this.props.i18nMethodFromFile}</ControlLabel>
+                    <div>
+                      <Container style={{ margin: '50px' }}>
+                        <DndFileChooser
+                          allowMultiple={this.props.allowMultiple}
+                          disableDropzone={this.props.disableDropzone}
+                          fileExtensions={this.props.fileExtensions}
+                          i18nHelpMessage={this.props.i18nHelpMessage}
+                          i18nInstructions={this.props.i18nInstructions}
+                          i18nNoFileSelectedMessage={
+                            this.props.i18nNoFileSelectedMessage
+                          }
+                          i18nSelectedFileLabel={
+                            this.props.i18nSelectedFileLabel
+                          }
+                          i18nUploadFailedMessage={
+                            this.props.i18nUploadFailedMessage
+                          }
+                          i18nUploadFailedMessages={
+                            this.props.i18nUploadFailedMessages
+                          }
+                          i18nUploadSuccessMessage={
+                            this.props.i18nUploadSuccessMessage
+                          }
+                          i18nUploadSuccessMessages={
+                            this.props.i18nUploadSuccessMessages
+                          }
+                          onUploadAccepted={this.props.onUploadAccepted}
+                          onUploadRejected={this.props.onUploadRejected}
+                        />
+                      </Container>
+                    </div>
+                  </Radio>
+                  <Radio name={'method'}>
+                    <ControlLabel>{this.props.i18nMethodFromUrl}</ControlLabel>
+                    <FormControl type="text" disabled={false} />
+                  </Radio>
+                  <Radio name={'method'}>
+                    <ControlLabel>
+                      {this.props.i18nMethodFromScratch}
+                    </ControlLabel>
+                  </Radio>
+                </div>
+              </FormGroup>
+            </Col>
+          </Row>
+        </Form>
+      </Grid>
     );
   }
 }
