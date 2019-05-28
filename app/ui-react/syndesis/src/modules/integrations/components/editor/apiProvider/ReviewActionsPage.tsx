8d5c81aa69c15e1ec1128397fc0c73cf8a0a5f1f
Riccardo Forina <rforina@redhat.com>
2019-05-28 23:25:38 +0200

    delete the integration id for the object returned by the api provider api, to allow for saving later on

--- app/ui-react/syndesis/src/modules/integrations/components/editor/apiProvider/ReviewActionsPage.tsx
+++ app/ui-react/syndesis/src/modules/integrations/components/editor/apiProvider/ReviewActionsPage.tsx
@@ -60,6 +60,7 @@ export const ReviewActionsPage: React.FunctionComponent<
       const integration = await getIntegration(
         apiSummary!.configuredProperties!.specification
       );
+      delete integration.id;
       history.push(nextHref(integration, params, state));
     } catch (e) {
       // todo show the error?
