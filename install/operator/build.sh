19a8acc1313aa7c1babed45104004b6dca7a9b66
Hiram Chirino <hiram@hiramchirino.com>
2019-05-29 12:21:29 -0400

    Updating the syndesis operator to conform more to the operator-sdk v0.8.0 file layout and build strategy.

--- install/operator/build.sh
+++ install/operator/build.sh
@@ -1,15 +1,74 @@
 #!/bin/bash
-set -e 
+#
+# Builds the operator, using operator-sdk for developers that have it
+# installed locally, or using docker if they don't have it installed.
+#
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-cd $DIR
+OPERATOR_IMAGE_NAME="syndesis/syndesis-operator"
+BUILDER_IMAGE_NAME="syndesis-operator-builder"
 
-echo ===========================================
-echo Running go build via a docker build
-echo ===========================================
-docker build -t syndesis-operator-builder . -f Dockerfile-builder
+set -e
 
-echo ===========================================
-echo Extracting go binary from the builder image
-echo ===========================================
-docker run syndesis-operator-builder cat /syndesis-operator > syndesis-operator
-chmod a+x syndesis-operator
+if [[ "$(which docker)" == "" ]] ; then
+    echo 'docker command not found.  Required to build this operator'
+    exit 1
+fi
+
+USE_DOCKER_BUILD=false
+
+if [[ "$(which operator-sdk)" == "" ]] ; then
+    echo 'operator-sdk command not found, will build with docker.'
+    USE_DOCKER_BUILD=true
+elif [[ "$(which go)" == "" ]] ; then
+    echo 'go command not found, will build with docker.'
+    USE_DOCKER_BUILD=true
+elif [[ "$DIR" != "$GOPATH/src/github.com/syndesisio/syndesis/install/operator" ]] ; then
+    echo 'Project not checked out into the $GOPATH, will build with docker.'
+    echo ''
+    echo 'Next time try checking out like this instead:'
+    echo '    mkdir -p $GOPATH/src/github.com/syndesisio'
+    echo '    git clone https://github.com/syndesisio/syndesis.git'
+    echo '    syndesis/install/operator/build.sh'
+    USE_DOCKER_BUILD=true
+fi
+
+#
+# TODO Could we avoid copying these files by moving them under the build directory
+#
+cp "$DIR/../syndesis.yml" "$DIR/build/conf/syndesis-template.yml"
+cp -R "$DIR/../addons/" "$DIR/build/conf/addons/"
+
+if [[ "$USE_DOCKER_BUILD" == "true" ]] ; then
+
+    echo ======================================================
+    echo operator-sdk not found. Running build in Docker
+    echo ======================================================
+    rm -rf build/_output
+    docker build -t "${BUILDER_IMAGE_NAME}" .
+
+    echo ======================================================
+    echo Downloading built operator binary from Docker build
+    echo ======================================================
+    mkdir -p ./build/_output/bin
+    docker run "${BUILDER_IMAGE_NAME}" cat /operator > ./build/_output/bin/operator
+    chmod a+x ./build/_output/bin/operator
+
+    echo ======================================================
+    echo Building Operator Image
+    echo ======================================================
+    docker build -f "build/Dockerfile" -t "${OPERATOR_IMAGE_NAME}" .
+
+else
+
+    export GO111MODULE=on
+    go mod vendor
+    operator-sdk generate k8s
+    # operator-sdk generate openapi
+    operator-sdk build "${OPERATOR_IMAGE_NAME}"
+
+fi
+
+echo ======================================================
+echo "Operator Image Built: ${OPERATOR_IMAGE_NAME}"
+echo ======================================================
+
--- install/operator/cmd/manager/main.go
+++ install/operator/cmd/manager/main.go
@@ -2,35 +2,42 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
-	"github.com/syndesisio/syndesis/install/operator/pkg/openshift"
-	"github.com/syndesisio/syndesis/install/operator/pkg/syndesis/configuration"
 	"os"
 	"runtime"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	"github.com/syndesisio/syndesis/install/operator/pkg/syndesis/configuration"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"github.com/syndesisio/syndesis/install/operator/pkg/apis"
+	"github.com/syndesisio/syndesis/install/operator/pkg/controller"
 
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
 	"github.com/operator-framework/operator-sdk/pkg/log/zap"
+	"github.com/operator-framework/operator-sdk/pkg/metrics"
+	"github.com/operator-framework/operator-sdk/pkg/restmapper"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
-
 	"github.com/spf13/pflag"
-	"github.com/syndesisio/syndesis/install/operator/pkg/apis"
-	"github.com/syndesisio/syndesis/install/operator/pkg/controller"
-
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
 )
 
+// Change below variables to serve metrics on different host or port.
+var (
+	metricsHost       = "0.0.0.0"
+	metricsPort int32 = 8383
+)
 var log = logf.Log.WithName("cmd")
 
 func printVersion() {
-	log.Info(fmt.Sprintf("Go Version: " + runtime.Version()))
-	log.Info(fmt.Sprintf("Go OS/Arch: " + runtime.GOOS + "/" + runtime.GOARCH))
-	log.Info(fmt.Sprintf("Version of operator-sdk: " + sdkVersion.Version))
+	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
+	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
+	log.Info(fmt.Sprintf("Version of operator-sdk: %v", sdkVersion.Version))
 }
 
 func main() {
@@ -38,17 +45,26 @@ func main() {
 	configuration.AddonsDirLocation = pflag.StringP("addons", "a", "", "Path to the addons directory used for installation")
 	configuration.Registry = pflag.StringP("registry", "r", "docker.io", "Registry to use for loading images like the upgrade pod")
 
+	// Add the zap logger flag set to the CLI. The flag set must
+	// be added before calling pflag.Parse().
+	pflag.CommandLine.AddFlagSet(zap.FlagSet())
+
+	// Add flags registered by imported packages (e.g. glog and
+	// controller-runtime)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+
+	pflag.Parse()
+
+	// Use a zap logr.Logger implementation. If none of the zap
+	// flags are configured (or if the zap flag set is not being
+	// used), this defaults to a production zap logger.
+	//
 	// The logger instantiated here can be changed to any logger
 	// implementing the logr.Logger interface. This logger will
 	// be propagated through the whole operator, generating
 	// uniform and structured logs.
-	pflag.CommandLine.AddFlagSet(zap.FlagSet())
 	logf.SetLogger(zap.Logger())
-	logf.SetLogger(logf.ZapLogger(false))
 
-	pflag.Parse()
-
-	log.Info("Using template", "template", *configuration.TemplateLocation)
 	printVersion()
 
 	namespace, err := k8sutil.GetWatchNamespace()
@@ -64,15 +80,21 @@ func main() {
 		os.Exit(1)
 	}
 
+	ctx := context.TODO()
+
 	// Become the leader before proceeding
-	err = leader.Become(context.TODO(), "syndesis-operator-lock")
+	err = leader.Become(ctx, "syndesis-operator-lock")
 	if err != nil {
 		log.Error(err, "")
 		os.Exit(1)
 	}
 
 	// Create a new Cmd to provide shared dependencies and start components
-	mgr, err := manager.New(cfg, manager.Options{Namespace: namespace})
+	mgr, err := manager.New(cfg, manager.Options{
+		Namespace:          namespace,
+		MapperProvider:     restmapper.NewDynamicRESTMapper,
+		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
+	})
 	if err != nil {
 		log.Error(err, "")
 		os.Exit(1)
@@ -80,19 +102,22 @@ func main() {
 
 	log.Info("Registering Components.")
 
-	// Setup Scheme for all own CR resources
+	// Setup Scheme for all resources
 	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
 		log.Error(err, "")
 		os.Exit(1)
 	}
 
-	// Add OpenShift schema
-	openshift.AddToScheme(mgr.GetScheme())
-
 	// Setup all Controllers
 	if err := controller.AddToManager(mgr); err != nil {
 		log.Error(err, "")
 		os.Exit(1)
+	}
+
+	// Create Service object to expose the metrics port.
+	_, err = metrics.ExposeMetricsPort(ctx, metricsPort)
+	if err != nil {
+		log.Info(err.Error())
 	}
 
 	log.Info("Starting the Cmd.")
