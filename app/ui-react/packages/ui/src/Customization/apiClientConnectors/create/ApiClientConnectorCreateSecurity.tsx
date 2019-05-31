d660ee052610c5fee2caa2d5cc9676ee305b10e7
kahboom <rachelyordan@gmail.com>
2019-05-31 11:25:23 +0100

    Add props for security UI component

--- app/ui-react/packages/ui/src/Customization/apiClientConnectors/create/ApiClientConnectorCreateSecurity.tsx
+++ app/ui-react/packages/ui/src/Customization/apiClientConnectors/create/ApiClientConnectorCreateSecurity.tsx
@@ -1,8 +1,18 @@
-import { Card } from 'patternfly-react';
+import { Card, FormGroup } from 'patternfly-react';
+import { Radio } from '@patternfly/react-core';
 import * as React from 'react';
 
 export interface IApiClientConnectorCreateSecurityProps {
-  i18nTitle?: string;
+  /**
+   * Required in OpenAPI 2.0 for the security scheme object.
+   * The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
+   */
+  authenticationType?: 'basic' | 'apiKey' | 'oauth2';
+  /**
+   * Locale string for when no security is specified
+   */
+  i18nNoSecurity: string;
+  i18nTitle: string;
 }
 
 export class ApiClientConnectorCreateSecurity extends React.Component<
@@ -10,11 +20,22 @@ export class ApiClientConnectorCreateSecurity extends React.Component<
 > {
   public render() {
     return (
-      <Card>
+      <Card style={{ maxWidth: '600px' }}>
         <Card.Heading>
           <Card.Title>{this.props.i18nTitle}</Card.Title>
         </Card.Heading>
-        <Card.Body />
+        <Card.Body>
+          <FormGroup controlId={'authenticationType'} disabled={false}>
+            <Radio
+              id={'authenticationType'}
+              aria-label={'Authentication Type'}
+              label={this.props.authenticationType || this.props.i18nNoSecurity}
+              checked={true}
+              name={'authenticationType'}
+              readOnly={true}
+            />
+          </FormGroup>
+        </Card.Body>
       </Card>
     );
   }
