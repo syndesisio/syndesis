71c3362232889a5f7500c803e95c8d36b94c0fc5
kahboom <rachelyordan@gmail.com>
2019-05-31 16:28:55 +0100

    Add OAuth vals to state, dummy connector for now

--- app/ui-react/packages/ui/src/Customization/apiClientConnectors/create/ApiClientConnectorCreateSecurity.tsx
+++ app/ui-react/packages/ui/src/Customization/apiClientConnectors/create/ApiClientConnectorCreateSecurity.tsx
@@ -1,23 +1,101 @@
-import { Radio } from '@patternfly/react-core';
-import { Card, FormGroup } from 'patternfly-react';
+import {
+  Card,
+  ControlLabel,
+  FormControl,
+  FormGroup,
+  Radio,
+} from 'patternfly-react';
 import * as React from 'react';
+import { ButtonLink } from '../../../Layout';
+
+export interface IAuthenticationType {
+  label: string;
+  value: 'basic' | 'apiKey' | 'oauth2';
+}
 
 export interface IApiClientConnectorCreateSecurityProps {
   /**
-   * Required in OpenAPI 2.0 for the security scheme object.
-   * The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
+   * Access token, required for OAuth 2.0.
    */
-  authenticationType?: 'basic' | 'apiKey' | 'oauth2';
+  accessToken?: string;
+  /**
+   * The type of authentication.
+   * Optional, though required in OpenAPI 2.0 for the security scheme object.
+   * Valid values are "basic", "apiKey" or "oauth2".
+   */
+  authenticationType?: IAuthenticationType[];
+  /**
+   * Authorization URL, required for OAuth 2.0.
+   */
+  authorizationUrl?: string;
+  i18nAccessTokenUrl: string;
+  i18nAuthorizationUrl: string;
+  i18nBtnNext: string;
   /**
    * Locale string for when no security is specified
    */
   i18nNoSecurity: string;
   i18nTitle: string;
+
+  /**
+   * The action fired when the user presses the Next button
+   */
+  onNext(
+    accessToken?: string,
+    authenticationType?: IAuthenticationType[],
+    authorizationUrl?: string
+  ): void;
+}
+
+export interface IApiClientConnectorCreateSecurityState {
+  accessTokenUrl?: string;
+  authorizationUrl?: string;
+  selectedType?: IAuthenticationType;
+  valid: boolean;
 }
 
 export class ApiClientConnectorCreateSecurity extends React.Component<
-  IApiClientConnectorCreateSecurityProps
+  IApiClientConnectorCreateSecurityProps,
+  IApiClientConnectorCreateSecurityState
 > {
+  constructor(props: any) {
+    super(props);
+    /**
+     * TODO: Improve this, crappy
+     */
+    this.state = {
+      selectedType: {
+        label: 'HTTP Basic Authentication',
+        value: 'basic',
+      },
+      valid: false,
+    };
+
+    this.onSelectType = this.onSelectType.bind(this);
+    this.setAccessTokenUrl = this.setAccessTokenUrl.bind(this);
+    this.setAuthorizationUrl = this.setAuthorizationUrl.bind(this);
+  }
+
+  /**
+   * The action fired when the user selects the authentication
+   * type they want to use for the client connector.
+   * @param newType
+   */
+  public onSelectType(newType: IAuthenticationType) {
+    this.setState({
+      selectedType: newType,
+      valid: newType.value === 'basic',
+    });
+  }
+
+  public setAccessTokenUrl(e: React.FormEvent<HTMLInputElement>) {
+    this.setState({ accessTokenUrl: e.currentTarget.value });
+  }
+
+  public setAuthorizationUrl(e: React.FormEvent<HTMLInputElement>) {
+    this.setState({ authorizationUrl: e.currentTarget.value });
+  }
+
   public render() {
     return (
       <Card style={{ maxWidth: '600px' }}>
@@ -26,15 +104,58 @@ export class ApiClientConnectorCreateSecurity extends React.Component<
         </Card.Heading>
         <Card.Body>
           <FormGroup controlId={'authenticationType'} disabled={false}>
-            <Radio
-              id={'authenticationType'}
-              aria-label={'Authentication Type'}
-              label={this.props.authenticationType || this.props.i18nNoSecurity}
-              checked={true}
-              name={'authenticationType'}
-              readOnly={true}
-            />
+            {this.props.authenticationType!.map(
+              (authType: IAuthenticationType, idx) => {
+                return (
+                  <div key={authType.value}>
+                    <Radio
+                      id={'authenticationType'}
+                      aria-label={'Authentication Type'}
+                      checked={
+                        this.state.selectedType!.value === authType.value
+                      }
+                      name={'authenticationType'}
+                      onClick={() => this.onSelectType(authType)}
+                      readOnly={true}
+                    >
+                      {authType.label || this.props.i18nNoSecurity}
+                    </Radio>
+                  </div>
+                );
+              }
+            )}
+            {this.state.selectedType &&
+              this.state.selectedType.value === 'oauth2' && (
+                <>
+                  <FormGroup controlId={'authorizationUrl'} disabled={false}>
+                    <ControlLabel>
+                      {this.props.i18nAuthorizationUrl}
+                    </ControlLabel>
+                    <FormControl
+                      type={'text'}
+                      value={
+                        this.state.authorizationUrl ||
+                        this.props.authorizationUrl
+                      }
+                      onChange={this.setAuthorizationUrl}
+                    />
+                  </FormGroup>
+                  <FormGroup controlId={'accessTokenUrl'} disabled={false}>
+                    <ControlLabel>{this.props.i18nAccessTokenUrl}</ControlLabel>
+                    <FormControl
+                      type={'text'}
+                      value={
+                        this.state.accessTokenUrl || this.props.accessToken
+                      }
+                      onChange={this.setAccessTokenUrl}
+                    />
+                  </FormGroup>
+                </>
+              )}
           </FormGroup>
+          <ButtonLink onClick={this.props.onNext} as={'primary'}>
+            {this.props.i18nBtnNext}
+          </ButtonLink>
         </Card.Body>
       </Card>
     );
--- app/ui-react/syndesis/src/modules/apiClientConnectors/pages/create/SecurityPage.tsx
+++ app/ui-react/syndesis/src/modules/apiClientConnectors/pages/create/SecurityPage.tsx
@@ -1,9 +1,10 @@
 import * as H from '@syndesis/history';
-import { APISummary } from '@syndesis/models/src';
+import { APISummary } from '@syndesis/models';
 import {
   ApiClientConnectorCreateSecurity,
   ApiConnectorCreatorLayout,
   ButtonLink,
+  IAuthenticationType,
   PageSection,
 } from '@syndesis/ui';
 import { useRouteData } from '@syndesis/utils';
@@ -25,8 +26,42 @@ export interface ISecurityPageRouteState {
 export const SecurityPage: React.FunctionComponent = () => {
   const { state, history } = useRouteData<null, ISecurityPageRouteState>();
 
-  const onNext = () => {
+  const authenticationType: IAuthenticationType[] = [
+    {
+      label: 'HTTP Basic Authentication',
+      value: 'basic',
+    },
+    {
+      label: 'OAuth 2.0',
+      value: 'oauth2',
+    },
+  ];
+
+  const connectorExample = {
+    properties: {
+      authenticationType: {
+        description: 'Type of authentication used to connect to the API',
+        enum: authenticationType,
+      },
+      authorizationEndpoint: {
+        defaultValue: '/',
+        description: 'URL for the start of the OAuth flow',
+        displayName: 'OAuth Authorization Endpoint URL',
+      },
+      tokenEndpoint: {
+        defaultValue: '/',
+      },
+    },
+  };
+
+  const onNext = (
+    accessToken?: string,
+    authType?: IAuthenticationType[],
+    authUrl?: string
+  ) => {
     history.push(resolvers.create.save(state));
+    // Leaving the following just so lint doesn't complain
+    return { accessToken, authType, authUrl };
   };
 
   return (
@@ -54,11 +89,28 @@ export const SecurityPage: React.FunctionComponent = () => {
                 content={
                   <PageSection>
                     <ApiClientConnectorCreateSecurity
-                      authenticationType={undefined}
+                      accessToken={
+                        connectorExample.properties.tokenEndpoint.defaultValue
+                      }
+                      authenticationType={
+                        connectorExample.properties.authenticationType.enum
+                      }
+                      authorizationUrl={
+                        connectorExample.properties.authorizationEndpoint
+                          .defaultValue
+                      }
+                      i18nAccessTokenUrl={t(
+                        'apiClientConnectors:create:security:accessTokenUrl'
+                      )}
+                      i18nAuthorizationUrl={t(
+                        'apiClientConnectors:create:security:authorizationUrl'
+                      )}
+                      i18nBtnNext={t('Next')}
                       i18nNoSecurity={t(
                         'apiClientConnectors:create:security:noSecurity'
                       )}
                       i18nTitle={t('apiClientConnectors:create:security:title')}
+                      onNext={onNext}
                     />
                     <div>
                       <ButtonLink
