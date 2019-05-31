f93d8fc0834479a79f86dad8e973afe3bd6f0c69
Riccardo Forina <rforina@redhat.com>
2019-05-31 16:16:26 +0200

    now with connector saving

--- app/ui-react/syndesis/src/modules/apiClientConnectors/pages/create/DetailsPage.tsx
+++ app/ui-react/syndesis/src/modules/apiClientConnectors/pages/create/DetailsPage.tsx
@@ -1,3 +1,4 @@
+import { useApiConnectorCreator } from '@syndesis/api';
 import * as H from '@syndesis/history';
 import { APISummary } from '@syndesis/models';
 import {
@@ -8,7 +9,8 @@ import {
 } from '@syndesis/ui';
 import { useRouteData } from '@syndesis/utils';
 import * as React from 'react';
-import { Translation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+import { UIContext } from '../../../../app';
 import { PageTitle } from '../../../../shared';
 import { WithLeaveConfirmation } from '../../../../shared/WithLeaveConfirmation';
 import {
@@ -25,82 +27,92 @@ export interface IDetailsPageRouteState {
 }
 
 export const DetailsPage: React.FunctionComponent = () => {
-  const { state } = useRouteData<null, IDetailsPageRouteState>();
-
-  const handleSubmit = async (values: IConnectorValues, actions: any) => {
-    actions.setSubmitting(true);
-    try {
-      // tslint:disable-next-line
-      console.log(values);
-      actions.setSubmitting(false);
-      return true;
-    } catch (e) {
-      actions.setSubmitting(false);
-      return false;
-    }
-  };
+  const { t } = useTranslation(['apiClientConnectors', 'shared']);
+  const { pushNotification } = React.useContext(UIContext);
+  const { state, history } = useRouteData<null, IDetailsPageRouteState>();
+  const createApiConnector = useApiConnectorCreator();
 
   return (
-    <Translation ns={['apiClientConnectors', 'shared']}>
-      {t => (
-        <WithLeaveConfirmation
-          i18nTitle={t('unsavedChangesTitle')}
-          i18nConfirmationMessage={t('unsavedChangesMessage')}
-          shouldDisplayDialog={(location: H.LocationDescriptor) => {
-            const url =
-              typeof location === 'string' ? location : location.pathname!;
-            return !url.startsWith(routes.create.root);
-          }}
-        >
-          {() => (
-            <>
-              <PageTitle
-                title={t('apiClientConnectors:create:details:title')}
-              />
-              <ApiConnectorCreatorBreadcrumb cancelHref={resolvers.list()} />
-              <ApiConnectorCreatorLayout
-                header={<ApiConnectorCreatorWizardSteps step={4} />}
-                content={
-                  <PageSection>
-                    <ApiConnectorInfoForm
-                      isEditing={true}
-                      handleSubmit={handleSubmit}
-                    >
-                      {({ submitForm, isSubmitting, isUploadingImage }) => (
-                        <>
-                          <ButtonLink
-                            data-testid={
-                              'api-connector-details-form-cancel-button'
-                            }
-                            className="api-connector-details-form__editButton"
-                            href={resolvers.create.security(state)}
-                          >
-                            {t('shared:Cancel')}
-                          </ButtonLink>
-                          <ButtonLink
-                            data-testid={
-                              'api-connector-details-form-save-button'
-                            }
-                            as="primary"
-                            className="api-connector-details-form__editButton"
-                            disabled={isSubmitting || isUploadingImage}
-                            onClick={submitForm}
-                          >
-                            {(isSubmitting || isUploadingImage) && (
-                              <Loader size={'sm'} inline={true} />
-                            )}
-                            {t('shared:Save')}
-                          </ButtonLink>
-                        </>
-                      )}
-                    </ApiConnectorInfoForm>
-                  </PageSection>
-                }
-              />
-            </>
-          )}
-        </WithLeaveConfirmation>
-      )}
-    </Translation>
+    <WithLeaveConfirmation
+      i18nTitle={t('unsavedChangesTitle')}
+      i18nConfirmationMessage={t('unsavedChangesMessage')}
+      shouldDisplayDialog={(location: H.LocationDescriptor) => {
+        const url =
+          typeof location === 'string' ? location : location.pathname!;
+        return !url.startsWith(routes.create.root);
+      }}
+    >
+      {({ allowNavigation }) => {
+        const handleSubmit = async (values: IConnectorValues, actions: any) => {
+          actions.setSubmitting(true);
+          try {
+            // tslint:disable-next-line
+            await createApiConnector({
+              ...values,
+              specification: state.specification.configuredProperties!
+                .specification,
+            });
+            actions.setSubmitting(false);
+            allowNavigation();
+            history.push(resolvers.list());
+            pushNotification(
+              t('apiClientConnectors:create:details:successNotification'),
+              'success'
+            );
+            return true;
+          } catch (e) {
+            actions.setSubmitting(false);
+            pushNotification(e.message, 'error');
+            return false;
+          }
+        };
+
+        return (
+          <>
+            <PageTitle title={t('apiClientConnectors:create:details:title')} />
+            <ApiConnectorCreatorBreadcrumb cancelHref={resolvers.list()} />
+            <ApiConnectorCreatorLayout
+              header={<ApiConnectorCreatorWizardSteps step={4} />}
+              content={
+                <PageSection>
+                  <ApiConnectorInfoForm
+                    name={state.specification.name}
+                    description={state.specification.description}
+                    isEditing={true}
+                    handleSubmit={handleSubmit}
+                  >
+                    {({ submitForm, isSubmitting, isUploadingImage }) => (
+                      <>
+                        <ButtonLink
+                          data-testid={
+                            'api-connector-details-form-cancel-button'
+                          }
+                          className="api-connector-details-form__editButton"
+                          href={resolvers.create.security(state)}
+                        >
+                          {t('shared:Back')}
+                        </ButtonLink>
+                        <ButtonLink
+                          data-testid={'api-connector-details-form-save-button'}
+                          as="primary"
+                          className="api-connector-details-form__editButton"
+                          disabled={isSubmitting || isUploadingImage}
+                          onClick={submitForm}
+                        >
+                          {(isSubmitting || isUploadingImage) && (
+                            <Loader size={'sm'} inline={true} />
+                          )}
+                          {t('shared:Save')}
+                        </ButtonLink>
+                      </>
+                    )}
+                  </ApiConnectorInfoForm>
+                </PageSection>
+              }
+            />
+          </>
+        );
+      }}
+    </WithLeaveConfirmation>
   );
 };
