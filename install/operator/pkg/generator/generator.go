e70709a7e7bdd8e15aab2ac19d54a5502de5f8a5
Hiram Chirino <hiram@hiramchirino.com>
2019-06-26 10:35:49 -0400

    chore: Support using go lang templates in addition to mustache templates to generate k8s resources in the operator.

--- install/operator/pkg/generator/generator.go
+++ install/operator/pkg/generator/generator.go
@@ -3,16 +3,21 @@
 package generator
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"github.com/hoisie/mustache"
+	"github.com/syndesisio/syndesis/install/operator/pkg/apis/syndesis/v1alpha1"
 	"github.com/syndesisio/syndesis/install/operator/pkg/util"
 	"io/ioutil"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"net/http"
+	"reflect"
 	"regexp"
+	"sigs.k8s.io/yaml"
 	"sort"
 	"strings"
+	"text/template"
 )
 
 type supportImages struct {
@@ -64,6 +69,7 @@ type Context struct {
 	Debug            bool
 	PrometheusRules  string
 	Env              map[string]string
+	Syndesis         *v1alpha1.Syndesis
 }
 
 // TODO: Could be added from a local configuration file
@@ -159,6 +165,19 @@ func RenderDir(directory string, context interface{}) ([]unstructured.Unstructur
 	return RenderFSDir(GetAssetsFS(), directory, context)
 }
 
+var templateFunctions = template.FuncMap{
+	"mapHasKey": func(item reflect.Value, key reflect.Value) (bool, error) {
+		if item.Kind() != reflect.Map {
+			return false, fmt.Errorf("mapHasKey requires a map type")
+		}
+		if x := item.MapIndex(key); x.IsValid() {
+			return true, nil
+		} else {
+			return false, nil
+		}
+	},
+}
+
 func RenderFSDir(assets http.FileSystem, directory string, context interface{}) ([]unstructured.Unstructured, error) {
 	f, err := assets.Open(directory)
 	if err != nil {
@@ -177,44 +196,106 @@ func RenderFSDir(assets http.FileSystem, directory string, context interface{}) 
 	response := []unstructured.Unstructured{}
 	for _, f := range files {
 		filePath := directory + f.Name()
-		if strings.HasSuffix(f.Name(), ".yml.mustache") {
-			template, err := AssetAsBytes(filePath)
+		var obj interface{} = nil
+
+		// We can process go lang templates.
+		if strings.HasSuffix(f.Name(), ".yml.tmpl") {
+			fileData, err := AssetAsBytes(filePath)
 			if err != nil {
 				return nil, err
 			}
 
-			rawYaml := mustache.Render(string(template), context)
-			var obj interface{} = nil
+			tmpl, err := template.New(filePath).Funcs(templateFunctions).Parse(string(fileData))
+			if err != nil {
+				return nil, err
+			}
+
+			buffer := &bytes.Buffer{}
+			err = tmpl.Execute(buffer, context)
+			if err != nil {
+				return nil, err
+			}
+			rawYaml := buffer.Bytes()
+
+			err = util.UnmarshalYaml(rawYaml, &obj)
+			if err != nil {
+				return nil, fmt.Errorf("%s:\n%s\n", err, string(rawYaml))
+			}
+		}
+
+		// We can process mustache templates.
+		if strings.HasSuffix(f.Name(), ".yml.mustache") {
+			fileData, err := AssetAsBytes(filePath)
+			if err != nil {
+				return nil, err
+			}
+
+			rawYaml := mustache.Render(string(fileData), context)
 			err = util.UnmarshalYaml([]byte(rawYaml), &obj)
 			if err != nil {
 				return nil, fmt.Errorf("%s:\n%s\n", err, rawYaml)
 			}
 
-			switch v := obj.(type) {
-			case []interface{}:
-				for _, value := range v {
-					if x, ok := value.(map[string]interface{}); ok {
-						u := unstructured.Unstructured{x}
-						//annotatedForDebugging(u, name, rawYaml)
-						response = append(response, u)
-					} else {
-						return nil, errors.New("list did not contain objects")
-					}
+			// This is only here to assist in migrating from mustache to go lang templates.
+			fileDataTmpl, err := AssetAsBytes(filePath + ".tmpl")
+			if err == nil {
+				t, err := template.New(filePath + ".tmpl").Parse(string(fileDataTmpl))
+				if err != nil {
+					return nil, err
 				}
-			case map[string]interface{}:
-				u := unstructured.Unstructured{v}
-				//annotatedForDebugging(u, name, rawYaml)
-				response = append(response, u)
-			case nil:
-				// It's ok if a template chooses not to generate any resources..
 
-			default:
-				return nil, fmt.Errorf("Unexptected yaml unmarshal type: %v", obj)
+				x := &bytes.Buffer{}
+				err = t.Execute(x, context)
+				if err != nil {
+					return nil, err
+				}
+
+				var objTmpl interface{} = nil
+				err = util.UnmarshalYaml([]byte(x.String()), &objTmpl)
+				if err != nil {
+					return nil, fmt.Errorf("%s:\n%s\n", err, rawYaml)
+				}
+
+				must1 := string(Must(yaml.Marshal(obj)))
+				must2 := string(Must(yaml.Marshal(objTmpl)))
+				if must1 != must2 {
+					return nil, fmt.Errorf("template did not render the same way")
+				}
 			}
+
 		}
+
+		switch v := obj.(type) {
+		case []interface{}:
+			for _, value := range v {
+				if x, ok := value.(map[string]interface{}); ok {
+					u := unstructured.Unstructured{x}
+					//annotatedForDebugging(u, name, rawYaml)
+					response = append(response, u)
+				} else {
+					return nil, errors.New("list did not contain objects")
+				}
+			}
+		case map[string]interface{}:
+			u := unstructured.Unstructured{v}
+			//annotatedForDebugging(u, name, rawYaml)
+			response = append(response, u)
+		case nil:
+			// It's ok if a template chooses not to generate any resources..
+
+		default:
+			return nil, fmt.Errorf("Unexptected yaml unmarshal type: %v", obj)
+		}
+
 	}
 
 	return response, nil
+}
+func Must(data []byte, err error) []byte {
+	if err != nil {
+		panic(err)
+	}
+	return data
 }
 
 func annotatedForDebugging(u unstructured.Unstructured, location string, rawYaml string) {
--- install/operator/pkg/generator/generator_test.go
+++ install/operator/pkg/generator/generator_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/syndesisio/syndesis/install/operator/pkg/apis/syndesis/v1alpha1"
 	"github.com/syndesisio/syndesis/install/operator/pkg/build"
 	"github.com/syndesisio/syndesis/install/operator/pkg/generator"
 	"github.com/syndesisio/syndesis/install/operator/pkg/util"
@@ -20,6 +21,14 @@ func TestGenerator(t *testing.T) {
 	gen := &generator.Context{}
 	err = json.Unmarshal(templateConfig, gen)
 	require.NoError(t, err)
+
+	gen.Syndesis = &v1alpha1.Syndesis{
+		Spec: v1alpha1.SyndesisSpec{
+			Addons: map[string]v1alpha1.Parameters{
+				"todo": []v1alpha1.Parameter{},
+			},
+		},
+	}
 
 	resources, err := generator.RenderFSDir(generator.GetAssetsFS(), "./install/", gen)
 	require.NoError(t, err)
