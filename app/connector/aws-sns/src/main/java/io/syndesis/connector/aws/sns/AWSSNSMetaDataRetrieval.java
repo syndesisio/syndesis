a2df42456870eb00fd7bc45e62b8165e0074d990
Andrea Cosentino <ancosen@gmail.com>
2019-07-16 10:56:42 +0200

    feat(aws-sqs) Support metadata suggestions in AWS SNS

---
+++ app/connector/aws-sns/src/main/java/io/syndesis/connector/aws/sns/AWSSNSMetaDataRetrieval.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.syndesis.connector.aws.sns;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.extension.MetaDataExtension;
+import org.apache.camel.component.extension.MetaDataExtension.MetaData;
+
+import io.syndesis.connector.support.verifier.api.ComponentMetadataRetrieval;
+import io.syndesis.connector.support.verifier.api.PropertyPair;
+import io.syndesis.connector.support.verifier.api.SyndesisMetadata;
+
+public class AWSSNSMetaDataRetrieval extends ComponentMetadataRetrieval {
+    /**
+     * TODO: use local extension, remove when switching to camel 2.22.x
+     */
+    @Override
+    protected MetaDataExtension resolveMetaDataExtension(CamelContext context, Class<? extends MetaDataExtension> metaDataExtensionClass, String componentId, String actionId) {
+        return new AWSSNSMetaDataExtension(context);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected SyndesisMetadata adapt(CamelContext context, String componentId, String actionId, Map<String, Object> properties, MetaData metadata) {
+        try {
+            Set<String> topics = (Set<String>)metadata.getPayload();
+
+            List<PropertyPair> channelsResult = new ArrayList<>();
+            topics.stream().forEach(t -> channelsResult.add(new PropertyPair(t, t)));
+
+            return SyndesisMetadata.of(Collections.singletonMap("topicNameOrArn", channelsResult));
+        } catch (Exception e) {
+            return SyndesisMetadata.EMPTY;
+        }
+    }
+
+}
