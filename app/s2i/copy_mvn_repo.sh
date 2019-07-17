68fc0fe12a7a38c8aa10df8f6e1d1696757ac8b0
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2019-07-17 15:27:33 +0200

    #6100 - Adding base project libraries to m2.cache

--- app/s2i/copy_mvn_repo.sh
+++ app/s2i/copy_mvn_repo.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
-set -x
+#set -x
 
 project_dir="$1"
 version="$2"
 target_dir="$3"
 local_repo="$4"
+syndesis_dependencies="/tmp/syndesis-dependencies"
 
 if [ -z "$local_repo" ]; then
   local_repo=~/.m2/repository/
 fi
 
 mkdir -p "${target_dir}/m2"
+mkdir -p "${target_dir}/m2.cache"
 cd ${local_repo}
 
-if [[ ! -d "${target_dir}/m2.cache" ]]; then
-    mkdir -p "${target_dir}/m2.cache"
-fi
+# Store a file with the list of Syndesis local dependencies
+find ./io/syndesis | grep -F -- "/${version}/" | grep -v -- "-sources\." | grep -v -- "-tests\." > $syndesis_dependencies
+echo "Found following list of local Syndesis ${version} dependencies"
+cat /tmp/syndesis-dependencies | grep .pom | grep -v .lastUpdated | sed -e 's/\/[^\/]*$//g' -e 's/\.\///g'
 
 if [[ ! "$(ls -A ${target_dir}/m2.cache)" ]]; then
-     echo "Going to cache locally Syndesis ${version} dependencies. Clean target directory to refresh the cache."
-     find ./io/syndesis | grep -F -- "/${version}/" | grep -v -- "-sources\." | grep -v -- "-tests\." | xargs tar -c | tar -vx -C "${target_dir}/m2.cache"
+    echo "Initializing local maven cache ${target_dir}/m2.cache"
+    echo "Installing all base project dependencies..."
+    mvn -f $target_dir/m2/project/pom.xml -s $target_dir/settings.xml dependency:copy-dependencies \
+      -DoutputDirectory=${target_dir}/m2.cache -Dmdep.copyPom=true -Dmdep.useRepositoryLayout=true
+
+    echo "Going to cache locally Syndesis ${version} dependencies."
+    xargs -a $syndesis_dependencies tar -c | tar -vx -C "${target_dir}/m2.cache" > /dev/null
 else
-    echo "Found cached Syndesis dependencies, using them to skip remote dependencies download."
+    echo "Using cached base project dependencies. Clean target directory if you need to refresh the cache though it will \
+      take longer to build your S2I Docker image."
 fi
 
-echo "Installing Syndesis ${version} dependencies to target directory."
-find ./io/syndesis | grep -F -- "/${version}/" | grep -v -- "-sources\." | grep -v -- "-tests\." | xargs tar -c | tar -vx -C "${target_dir}/m2"
+echo "Installing Syndesis ${version} dependencies to target directory..."
+xargs -a $syndesis_dependencies tar -c | tar -vx -C "${target_dir}/m2" > /dev/null
 
 if [ -z "$CAMEL_SNAPSHOT_VERSION" ]; then
    echo "This installation won't use a camel snapshot version"
