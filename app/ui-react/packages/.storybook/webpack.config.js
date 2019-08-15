8fba25b750a8effd8f86eefd984de669b9094482
Michael Spaxman <seanforyou23@gmail.com>
2019-08-15 16:33:08 -0400

    feat(typescript): use a better loader for typescript, tweak webpack config

--- app/ui-react/packages/.storybook/webpack.config.js
+++ app/ui-react/packages/.storybook/webpack.config.js
@@ -1,13 +1,26 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = ({ config, mode }) => {
   config.module.rules.push({
     test: /\.tsx?$/,
     exclude: /node_modules/,
     use: [
-      'awesome-typescript-loader?configFileName=tsconfig.storybook.json',
+      {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+          experimentalWatchApi: true,
+        }
+      },
       'react-docgen-typescript-loader',
-    ],
+    ]
   });
   config.resolve.extensions = ['.ts', '.tsx', '.js', '.jsx'];
+  config.resolve.plugins = [
+    new TsconfigPathsPlugin({
+      configFile: 'tsconfig.storybook.json'
+    })
+  ];
   config.node = {
     fs: 'empty',
   };
