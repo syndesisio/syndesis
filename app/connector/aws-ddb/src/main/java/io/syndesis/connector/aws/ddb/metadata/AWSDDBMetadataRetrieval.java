6162de5ec599907d90cbdd3da66fb1451e87a3ec
Maria Arias de Reyna <ariasdereyna@redhat.com>
2019-09-10 16:13:58 +0200

    Adding schema for input and output data

---
+++ app/connector/aws-ddb/src/main/java/io/syndesis/connector/aws/ddb/metadata/AWSDDBMetadataRetrieval.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.syndesis.connector.aws.ddb.metadata;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.module.jsonSchema.factories.JsonSchemaFactory;
+import com.fasterxml.jackson.module.jsonSchema.types.ObjectSchema;
+import com.google.common.base.Splitter;
+import io.syndesis.common.model.DataShape;
+import io.syndesis.common.model.DataShapeKinds;
+import io.syndesis.common.model.DataShapeMetaData;
+import io.syndesis.common.util.Json;
+import io.syndesis.connector.aws.ddb.util.Util;
+import io.syndesis.connector.support.util.ConnectorOptions;
+import io.syndesis.connector.support.verifier.api.ComponentMetadataRetrieval;
+import io.syndesis.connector.support.verifier.api.PropertyPair;
+import io.syndesis.connector.support.verifier.api.SyndesisMetadata;
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.extension.MetaDataExtension;
+import org.apache.camel.component.extension.MetaDataExtension.MetaData;
+
+/**
+ * Extract all automatic info (column names) to enrich the properties.
+ */
+public final class AWSDDBMetadataRetrieval extends ComponentMetadataRetrieval {
+
+    @Override
+    protected MetaDataExtension resolveMetaDataExtension(CamelContext context,
+                                                         Class<? extends MetaDataExtension> metaDataExtensionClass,
+                                                         String componentId,
+                                                         String actionId) {
+        return new AWSDDBConnectorMetaDataExtension(context);
+    }
+
+
+    @Override
+    protected SyndesisMetadata adapt(CamelContext context, String componentId, String actionId,
+                                     Map<String, Object> properties, MetaData metadata) {
+
+        return adaptForDDB(properties);
+    }
+
+    /**
+     * Extract columns of query and result to create a data shape.
+     *
+     * @param properties
+     * @return
+     */
+    private SyndesisMetadata adaptForDDB(final Map<String, Object> properties) {
+
+        final Map<String, List<PropertyPair>> enrichedProperties = new HashMap<>();
+
+
+        Map<String, AttributeValue> element = Util.getAttributeValueMap("element", properties);
+
+        List<String> attributes = new ArrayList<String>();
+        String optionAttributes = ConnectorOptions.extractOption(properties, "attributes", "");
+        if (!optionAttributes.isEmpty()) {
+            Splitter splitter = Splitter.on(',');
+            splitter = splitter.trimResults();
+            splitter = splitter.omitEmptyStrings();
+            attributes = splitter.splitToList(optionAttributes);
+        }
+
+
+        //fallback to use the list of attributes on the filter
+        //this is used always on put-item
+        if (attributes.isEmpty()) {
+            attributes.addAll(element.keySet());
+        }
+
+
+        // build the input and output schemas
+        final JsonSchemaFactory factory = new JsonSchemaFactory();
+        final ObjectSchema builderIn = new ObjectSchema();
+        builderIn.setTitle("Parameters");
+
+        Set<Map.Entry<String, AttributeValue>> elements = element.entrySet();
+        elements.removeIf(e -> !e.getValue().getS().startsWith(":"));
+
+        for (Map.Entry<String, AttributeValue> inParam : elements) {
+            builderIn.putOptionalProperty(inParam.getKey(), factory.stringSchema());
+        }
+
+        final ObjectSchema builderOut = new ObjectSchema();
+        builderOut.setTitle("Result");
+        for (String outParam : attributes) {
+            builderOut.putOptionalProperty(outParam, factory.stringSchema());
+        }
+
+        try {
+            DataShape.Builder inDataShapeBuilder = new DataShape.Builder().type(builderIn.getTitle());
+            if (builderIn.getProperties().isEmpty()) {
+                inDataShapeBuilder.kind(DataShapeKinds.NONE);
+            } else {
+                inDataShapeBuilder.kind(DataShapeKinds.JSON_SCHEMA)
+                .name("Parameters")
+                .description(String.format("Query parameters."))
+                .specification(Json.writer().writeValueAsString(builderIn));
+
+                inDataShapeBuilder.putMetadata(DataShapeMetaData.VARIANT, DataShapeMetaData.VARIANT_ELEMENT);
+
+            }
+            DataShape.Builder outDataShapeBuilder = new DataShape.Builder().type(builderOut.getTitle());
+            if (builderOut.getProperties().isEmpty()) {
+                outDataShapeBuilder.kind(DataShapeKinds.NONE);
+            } else {
+                outDataShapeBuilder.kind(DataShapeKinds.JSON_SCHEMA)
+                .name("Result")
+                .description(String.format("Attributes on the result."))
+                .putMetadata(DataShapeMetaData.VARIANT, DataShapeMetaData.VARIANT_COLLECTION)
+                .specification(Json.writer().writeValueAsString(builderOut));
+            }
+
+            return new SyndesisMetadata(enrichedProperties,
+            inDataShapeBuilder.build(), outDataShapeBuilder.build());
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
---
+++ app/connector/aws-ddb/src/main/java/io/syndesis/connector/aws/ddb/util/Util.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.syndesis.connector.aws.ddb.util;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.syndesis.connector.support.util.ConnectorOptions;
+import org.apache.camel.CamelContext;
+import org.apache.camel.TypeConverter;
+import org.apache.camel.util.EndpointHelper;
+import org.apache.camel.util.IntrospectionSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility class for common procedures.
+ */
+public class Util {
+    public static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Logger LOG = LoggerFactory.getLogger(Util.class);
+
+    /**
+     * Extract a map from a JSON in a header as AttributeValue. Useful for the headers.
+     *
+     * @param parameterName
+     * @param options
+     * @return
+     */
+    public static Map<String, AttributeValue> getAttributeValueMap(
+    final String parameterName, Map<String, Object> options) {
+        final Map<String, AttributeValue> attributeMap = new HashMap<String, AttributeValue>();
+
+
+        String element = ConnectorOptions.extractOption(options, parameterName, "");
+
+        if (!element.isEmpty()) {
+            try {
+
+                @SuppressWarnings("unchecked")
+                Map<String, Object> attributes = MAPPER.readValue(element, Map.class);
+
+                if (attributes.isEmpty() && LOG.isWarnEnabled()) {
+                    LOG.warn("The parameter {} is an empty map.", parameterName);
+                }
+
+                for (Map.Entry<String, Object> attribute : attributes.entrySet()) {
+                    attributeMap.put(attribute.getKey(),
+                    new AttributeValue(attribute.getValue().toString()));
+                    LOG.trace("Parameter adding '" + attribute.getKey() + "'->'" + attribute.getValue() + "'");
+
+                }
+            } catch (IOException e) {
+                LOG.warn("Error trying to parse the json: {}", element, e);
+            }
+        } else {
+            LOG.warn("The parameter {} is empty.", parameterName);
+        }
+
+        return attributeMap;
+    }
+
+
+
+    public static <T> T setProperties(T instance, Map<String, Object> properties,
+                                  CamelContext camelContext) {
+        if (camelContext == null) {
+            throw new IllegalStateException("Camel context is null");
+        }
+
+        try {
+            if (!properties.isEmpty()) {
+                final TypeConverter converter = camelContext.getTypeConverter();
+
+                IntrospectionSupport.setProperties(converter, instance, properties);
+
+                for (Map.Entry<String, Object> entry : properties.entrySet()) {
+                    if (entry.getValue() instanceof String) {
+                        String value = (String) entry.getValue();
+                        if (EndpointHelper.isReferenceParameter(value)) {
+                            IntrospectionSupport.setProperty(camelContext, converter, instance, entry.getKey(), null, value, true);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(
+            "Get information about tables failed.", e);
+        }
+
+        return instance;
+    }
+}
