f53c1755593a79e18511849058d96d484d8f56e9
nferraro <ni.ferraro@gmail.com>
2019-09-17 11:46:23 +0200

    Fixes ENTESB-11264: Add ability to specify custom maven repositories

--- install/operator/pkg/apis/syndesis/v1alpha1/syndesis_types.go
+++ install/operator/pkg/apis/syndesis/v1alpha1/syndesis_types.go
@@ -27,6 +27,7 @@ type SyndesisSpec struct {
 	// if true, then the image streams are changed to used local development builds & JAVA_DEBUG is enabled
 	DevSupport bool `json:"devSupport,omitempty"`
 
+	MavenRepositories map[string]string `json:"mavenRepositories,omitempty"`
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
