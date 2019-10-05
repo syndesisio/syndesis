7d4152fe9809941673053870927b3fc1dff37f25
Kurt Stam <kstam@redhat.com>
2019-10-05 11:57:31 -0400

    fix for ENTESB-11633 - preventing timeout by setting limit on select

--- app/connector/sql/src/main/java/io/syndesis/connector/sql/common/DbMetaDataHelper.java
+++ app/connector/sql/src/main/java/io/syndesis/connector/sql/common/DbMetaDataHelper.java
@@ -22,7 +22,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
