79a2ed4d1474d3fac8fdb664df8d5a39a9287bff
vhalbert <vhalbert@redhat.com>
2019-11-21 10:38:27 -0500

    Update license headers to set year to 2016

--- app/dv/src/main/java/io/syndesis/dv/metadata/internal/DDLDBMetadataRepository.java
+++ app/dv/src/main/java/io/syndesis/dv/metadata/internal/DDLDBMetadataRepository.java
@@ -3,7 +3,7 @@ package io.syndesis.dv.metadata.internal;
 import java.io.StringReader;
 
 /*
- * Copyright (C) 2013 Red Hat, Inc.
+ * Copyright (C) 2016 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
