2e039b2ff3bc695d6be87fd1afaa32ab6feb4d7d
Zoran Regvart <zoran@regvart.com>
2019-11-27 13:50:39 +0100

    chore(it): remove stub www.googleapis.com server
    This removes the test and the stub server configuration from the Google
    Calendar connector integration test. As noted previously it's really
    difficult to get this to work.
    
    The test still has value as the issue the test tries to test manifested
    itself on the integration startup and the test currently tests that the
    integration successfully starts.

--- app/test/integration-test/src/test/java/io/syndesis/test/itest/googlecalendar/WebhookToGoogleCalendar_IT.java
+++ app/test/integration-test/src/test/java/io/syndesis/test/itest/googlecalendar/WebhookToGoogleCalendar_IT.java
@@ -15,45 +15,19 @@
  */
 package io.syndesis.test.itest.googlecalendar;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import javax.servlet.Filter;
-
 import io.syndesis.test.SyndesisTestEnvironment;
 import io.syndesis.test.container.integration.SyndesisIntegrationRuntimeContainer;
 import io.syndesis.test.itest.SyndesisIntegrationTestSupport;
-import io.syndesis.test.itest.sheets.util.GzipServletFilter;
 
-import org.eclipse.jetty.server.HttpConfiguration;
-import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.SecureRequestCustomizer;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.SslConnectionFactory;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.util.SocketUtils;
-import org.testcontainers.Testcontainers;
 
 import com.consol.citrus.annotations.CitrusResource;
 import com.consol.citrus.annotations.CitrusTest;
-import com.consol.citrus.dsl.endpoint.CitrusEndpoints;
 import com.consol.citrus.dsl.runner.TestRunner;
-import com.consol.citrus.http.client.HttpClient;
-import com.consol.citrus.http.server.HttpServer;
-import com.consol.citrus.http.servlet.RequestCachingServletFilter;
 
-@ContextConfiguration(classes = WebhookToGoogleCalendar_IT.EndpointConfig.class)
 public class WebhookToGoogleCalendar_IT extends SyndesisIntegrationTestSupport {
 
     /**
@@ -67,80 +41,6 @@ public class WebhookToGoogleCalendar_IT extends SyndesisIntegrationTestSupport {
         .withExposedPorts(SyndesisTestEnvironment.getServerPort(),
             SyndesisTestEnvironment.getManagementPort())
         .withExtraHost("www.googleapis.com", "127.0.0.1");
-
-    @Autowired
-    private HttpClient webHookClient;
-
-    @Autowired
-    private HttpServer googleApiServer;
-
-    @Configuration
-    public static class EndpointConfig {
-
-        static final int GOOGLE_API_SERVER_PORT = SocketUtils.findAvailableTcpPort();
-
-        static {
-            Testcontainers.exposeHostPorts(Collections.singletonMap(GOOGLE_API_SERVER_PORT, 443));
-        }
-
-        @Bean
-        public HttpServer googleApiServer() {
-            final Map<String, Filter> filterMap = new LinkedHashMap<>();
-            filterMap.put("request-caching-filter", new RequestCachingServletFilter());
-            filterMap.put("gzip-filter", new GzipServletFilter());
-
-            final HttpConfiguration https = new HttpConfiguration();
-            https.addCustomizer(new SecureRequestCustomizer());
-
-            final SslContextFactory ssl = new SslContextFactory();
-            ssl.setKeyStoreResource(Resource.newClassPathResource("www.googleapis.com.p12"));
-            ssl.setKeyStorePassword("password");
-
-            final ServerConnector sslConnector = new ServerConnector(new Server(),
-                new SslConnectionFactory(ssl, "http/1.1"),
-                new HttpConnectionFactory(https));
-            sslConnector.setPort(GOOGLE_API_SERVER_PORT);
-
-            return CitrusEndpoints.http()
-                .server()
-                .connector(sslConnector)
-                .port(GOOGLE_API_SERVER_PORT)
-                .autoStart(true)
-                .timeout(60000L)
-                .filters(filterMap)
-                .build();
-        }
-
-        @Bean
-        public HttpClient webHookClient() {
-            return CitrusEndpoints.http().client()
-                .requestUrl(String.format("http://localhost:%s/webhook/token", integrationContainer.getServerPort()))
-                .build();
-        }
-    }
-
-    @Test
-    @CitrusTest
-    public void shouldCreateGoogleCalendarEvent(@CitrusResource final TestRunner runner) {
-        runner.http(builder -> builder.client(webHookClient)
-            .send()
-            .post()
-            .payload("{\"name\":\"Event name\",\"date\":\"1997-08-29\",\"time\":\"02:14:00\"}"));
-
-        runner.http(builder -> builder.client(webHookClient)
-            .receive()
-            .response(HttpStatus.NO_CONTENT));
-
-        runner.http(builder -> builder.server(googleApiServer)
-            .receive()
-            .post("/calendar/v3/calendars/calendar-id/events")
-            .payload(
-                "{\"attendees\":[],\"end\":{\"date\":\"1997-08-29T02:14:00.000Z\"},\"start\":{\"date\":\"1997-08-29T02:14:00.000Z\"},\"summary\":\"Event name\"}"));
-
-        runner.http(builder -> builder.server(googleApiServer)
-            .send()
-            .response(HttpStatus.OK));
-    }
 
     @Test
     @CitrusTest
