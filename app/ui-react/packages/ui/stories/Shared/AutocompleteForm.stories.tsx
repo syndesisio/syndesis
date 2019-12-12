91705f7d73b8e0537387d07b7ae1568e9858bb95
kahboom <rachelyordan@gmail.com>
2019-12-12 16:18:30 +0000

    setup story

---
+++ app/ui-react/packages/ui/stories/Shared/AutocompleteForm.stories.tsx
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+const stories = storiesOf('Shared/AutocompleteForm', module);
+
+stories.add('autocomplete form', () => (
+  <p>Testing.</p>
+));
