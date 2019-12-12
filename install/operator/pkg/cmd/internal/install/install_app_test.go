2b5ef5a5fe460642967bf867734483e0e47f8d65
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2019-12-12 11:56:34 +0000

    ENTESB-12330: Allow a custom resource to be specified to the operator
    * install.go
     * Moves the custom-resource switch to the `install app` command
    
    * install_app.go
     * Handles a custom resource being specified.
     * Loads it, checks its a syndesis schema, applies it
    
    * Provides tests for providing a custom resource

---
+++ install/operator/pkg/cmd/internal/install/install_app_test.go
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package install
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/syndesisio/syndesis/install/operator/pkg/cmd/internal"
+	"github.com/syndesisio/syndesis/install/operator/pkg/util"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+const (
+	appNS      = "namespace"
+	appTag     = "1.9.0"
+	appSucceed = "\u2713"
+	appFailed  = "\u2717"
+)
+
+func TestInstallAppNoCustomResource(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping app install tests in short mode")
+	}
+
+	ctx := context.TODO()
+	i := &Install{Options: &internal.Options{Namespace: appNS, Context: ctx}, tag: appTag, devSupport: false}
+
+	//
+	// Want to compare app resource being installed rather than actually installing
+	//
+	i.eject = "yaml"
+	i.ejectedResources = []unstructured.Unstructured{}
+
+	if err := i.installApplication(); err != nil {
+		t.Fatalf("\t%s\t got an error when running the command: [%v]", appFailed, err)
+	}
+
+	assert.True(t, len(i.ejectedResources) == 1)
+
+	//
+	// No custom resource specified ensures default resource is used
+	//
+	resource, err := util.LoadUnstructuredObjectFromFile("default_app_test.yml")
+	if err != nil {
+		t.Fatalf("\t%s\t got an error when loading test app.yml file: [%v]", appFailed, err)
+	}
+
+	assert.Equal(t, i.ejectedResources[0], *resource)
+}
+
+func TestInstallAppCustomResource(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping app install tests in short mode")
+	}
+
+	ctx := context.TODO()
+	i := &Install{Options: &internal.Options{Namespace: appNS, Context: ctx}, tag: appTag, devSupport: false}
+
+	//
+	// Want to compare app resource being installed rather than actually installing
+	//
+	i.eject = "yaml"
+	i.ejectedResources = []unstructured.Unstructured{}
+
+	i.customResource = "custom_app_test.yml"
+
+	if err := i.installApplication(); err != nil {
+		t.Fatalf("\t%s\t got an error when running the command: [%v]", appFailed, err)
+	}
+
+	assert.True(t, len(i.ejectedResources) == 1)
+
+	//
+	// Custom resource should be rendered and used for install
+	//
+	resource, err := util.LoadUnstructuredObjectFromFile("custom_app_test.yml")
+	if err != nil {
+		t.Fatalf("\t%s\t got an error when loading test app.yml file: [%v]", appFailed, err)
+	}
+
+	assert.Equal(t, i.ejectedResources[0], *resource)
+}
