4c445c7a35989ed9da5f892b82103d5794c51b0d
Ramesh Reddy <rareddy@jboss.org>
2019-12-12 09:56:12 -0600

    TEIIDTOOLS-883, TEIIDTOOLS-865, TEIIDTOOLS-752: Fixing the resource leak for http connection leak and upgrading the openshift client libraries to 4.x

--- app/dv/src/main/java/io/syndesis/dv/openshift/HttpClientUtils.java
+++ app/dv/src/main/java/io/syndesis/dv/openshift/HttpClientUtils.java
@@ -15,22 +15,23 @@
  */
 package io.syndesis.dv.openshift;
 
-import static io.fabric8.kubernetes.client.utils.Utils.isNotNullOrEmpty;
 import static okhttp3.ConnectionSpec.CLEARTEXT;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.Proxy;
 import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
@@ -40,35 +41,50 @@ import org.slf4j.LoggerFactory;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.kubernetes.client.internal.SSLUtils;
-import okhttp3.Authenticator;
+import io.fabric8.kubernetes.client.utils.BackwardsCompatibilityInterceptor;
+import io.fabric8.kubernetes.client.utils.ImpersonatorInterceptor;
+import io.fabric8.kubernetes.client.utils.IpAddressMatcher;
+import io.fabric8.kubernetes.client.utils.Utils;
 import okhttp3.ConnectionSpec;
 import okhttp3.Credentials;
 import okhttp3.Dispatcher;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
 import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.Route;
 import okhttp3.logging.HttpLoggingInterceptor;
 
-// this copy copied from io.fabric8.kubernetes client library to pass the custom builder
+// This class is copied from the Kubernetes client to send a custom httpClientBuilder in constructor
 public class HttpClientUtils {
 
-    private HttpClientUtils() {}
+  private static Pattern VALID_IPV4_PATTERN = null;
+  public static final String ipv4Pattern = "(http:\\/\\/|https:\\/\\/)?(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])(\\/[0-9]\\d|1[0-9]\\d|2[0-9]\\d|3[0-2]\\d)?";
+  private static final Logger logger = LoggerFactory.getLogger(HttpClientUtils.class);
 
-    public static OkHttpClient createHttpClient(final Config config, OkHttpClient.Builder httpClientBuilder) {
+  static {
+    try {
+      VALID_IPV4_PATTERN = Pattern.compile(ipv4Pattern, Pattern.CASE_INSENSITIVE);
+    } catch (PatternSyntaxException e) {
+      throw KubernetesClientException.launderThrowable("Unable to compile ipv4address pattern.", e);
+    }
+  }
+
+  public static OkHttpClient createHttpClient(final Config config, OkHttpClient.Builder httpClientBuilder) {
+      return createHttpClient(config, (b) -> {}, httpClientBuilder);
+  }
+
+  public static OkHttpClient createHttpClientForMockServer(final Config config) {
+      OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+      return createHttpClient(config, b -> b.protocols(Collections.singletonList(Protocol.HTTP_1_1)), httpClientBuilder);
+  }
+
+  private static OkHttpClient createHttpClient(final Config config, final Consumer<OkHttpClient.Builder> additionalConfig, OkHttpClient.Builder httpClientBuilder) {
         try {
             // Follow any redirects
             httpClientBuilder.followRedirects(true);
             httpClientBuilder.followSslRedirects(true);
 
-            if (config.isTrustCerts()) {
-                httpClientBuilder.hostnameVerifier(new HostnameVerifier() {
-                    @Override
-                    public boolean verify(String s, SSLSession sslSession) {
-                        return true;
-                    }
-                });
+            if (config.isTrustCerts() || config.isDisableHostnameVerification()) {
+                httpClientBuilder.hostnameVerifier((s, sslSession) -> true);
             }
 
             TrustManager[] trustManagers = SSLUtils.trustManagers(config);
@@ -81,7 +97,7 @@ public class HttpClientUtils {
                 }
 
                 try {
-                    SSLContext sslContext = SSLUtils.sslContext(keyManagers, trustManagers, config.isTrustCerts());
+                    SSLContext sslContext = SSLUtils.sslContext(keyManagers, trustManagers);
                     httpClientBuilder.sslSocketFactory(sslContext.getSocketFactory(), trustManager);
                 } catch (GeneralSecurityException e) {
                     throw new AssertionError(); // The system has no TLS. Just give up.
@@ -92,20 +108,18 @@ public class HttpClientUtils {
               httpClientBuilder.sslSocketFactory(context.getSocketFactory(), (X509TrustManager) trustManagers[0]);
             }
 
-          httpClientBuilder.addInterceptor(new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-              Request request = chain.request();
-              if (isNotNullOrEmpty(config.getUsername()) && isNotNullOrEmpty(config.getPassword())) {
-                Request authReq = chain.request().newBuilder().addHeader("Authorization", Credentials.basic(config.getUsername(), config.getPassword())).build();
-                return chain.proceed(authReq);
-              } else if (isNotNullOrEmpty(config.getOauthToken())) {
-                Request authReq = chain.request().newBuilder().addHeader("Authorization", "Bearer " + config.getOauthToken()).build();
-                return chain.proceed(authReq);
-              }
-              return chain.proceed(request);
+          httpClientBuilder.addInterceptor(chain -> {
+            Request request = chain.request();
+            if (Utils.isNotNullOrEmpty(config.getUsername()) && Utils.isNotNullOrEmpty(config.getPassword())) {
+              Request authReq = chain.request().newBuilder().addHeader("Authorization", Credentials.basic(config.getUsername(), config.getPassword())).build();
+              return chain.proceed(authReq);
+            } else if (Utils.isNotNullOrEmpty(config.getOauthToken())) {
+              Request authReq = chain.request().newBuilder().addHeader("Authorization", "Bearer " + config.getOauthToken()).build();
+              return chain.proceed(authReq);
             }
-          });
+            return chain.proceed(request);
+          }).addInterceptor(new ImpersonatorInterceptor(config))
+            .addInterceptor(new BackwardsCompatibilityInterceptor());
 
             Logger reqLogger = LoggerFactory.getLogger(HttpLoggingInterceptor.class);
             if (reqLogger.isTraceEnabled()) {
@@ -141,13 +155,10 @@ public class HttpClientUtils {
                         httpClientBuilder.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyUrl.getHost(), proxyUrl.getPort())));
 
                         if (config.getProxyUsername() != null) {
-                          httpClientBuilder.proxyAuthenticator(new Authenticator() {
-                            @Override
-                            public Request authenticate(Route route, Response response) throws IOException {
+                          httpClientBuilder.proxyAuthenticator((route, response) -> {
 
-                              String credential = Credentials.basic(config.getProxyUsername(), config.getProxyPassword());
-                              return response.request().newBuilder().header("Proxy-Authorization", credential).build();
-                            }
+                            String credential = Credentials.basic(config.getProxyUsername(), config.getProxyPassword());
+                            return response.request().newBuilder().header("Proxy-Authorization", credential).build();
                           });
                         }
                     }
@@ -158,12 +169,9 @@ public class HttpClientUtils {
             }
 
             if (config.getUserAgent() != null && !config.getUserAgent().isEmpty()) {
-                httpClientBuilder.addNetworkInterceptor(new Interceptor() {
-                    @Override
-                    public Response intercept(Chain chain) throws IOException {
-                        Request agent = chain.request().newBuilder().header("User-Agent", config.getUserAgent()).build();
-                        return chain.proceed(agent);
-                    }
+                httpClientBuilder.addNetworkInterceptor(chain -> {
+                    Request agent = chain.request().newBuilder().header("User-Agent", config.getUserAgent()).build();
+                    return chain.proceed(agent);
                 });
             }
 
@@ -172,6 +180,14 @@ public class HttpClientUtils {
                 .tlsVersions(config.getTlsVersions())
                 .build();
               httpClientBuilder.connectionSpecs(Arrays.asList(spec, CLEARTEXT));
+            }
+
+            if (config.isHttp2Disable()) {
+                httpClientBuilder.protocols(Collections.singletonList(Protocol.HTTP_1_1));
+            }
+
+            if(additionalConfig != null) {
+                additionalConfig.accept(httpClientBuilder);
             }
 
             return httpClientBuilder.build();
@@ -185,9 +201,15 @@ public class HttpClientUtils {
         String host = master.getHost();
         if (config.getNoProxy() != null) {
             for (String noProxy : config.getNoProxy()) {
-                if (host.endsWith(noProxy)) {
-                    return null;
-                }
+            if (isIpAddress(noProxy)) {
+              if (new IpAddressMatcher(noProxy).matches(host)) {
+                return null;
+              }
+            } else {
+              if (host.contains(noProxy)) {
+                return null;
+              }
+            }
             }
         }
         String proxy = config.getHttpsProxy();
@@ -198,5 +220,10 @@ public class HttpClientUtils {
             return new URL(proxy);
         }
         return null;
+    }
+
+    private static boolean isIpAddress(String ipAddress) {
+        Matcher ipMatcher = VALID_IPV4_PATTERN.matcher(ipAddress);
+        return ipMatcher.matches();
     }
 }
