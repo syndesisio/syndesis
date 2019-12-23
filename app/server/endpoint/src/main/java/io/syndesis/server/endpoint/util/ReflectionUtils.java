d2d9f2d6614a809cd2b9063dbdafffefd105c146
Zoran Regvart <zoran@regvart.com>
2019-12-23 10:20:42 +0100

    fix(endpoint): all methods for reflective sorting
    This changes the `ReflectionUtils` to include all methods available on a
    class rather than the methods that are declared on a class. This will
    help in finding the `getVersion` method on `Integration` interface.
    
    Fixes #7471

--- app/server/endpoint/src/main/java/io/syndesis/server/endpoint/util/ReflectionUtils.java
+++ app/server/endpoint/src/main/java/io/syndesis/server/endpoint/util/ReflectionUtils.java
@@ -45,7 +45,7 @@ public final class ReflectionUtils {
 
     static Method extractMethod(Class<?> clazz, String fieldName, Class<?>... types) {
         try {
-            Method method = clazz.getDeclaredMethod("get" + fieldName.substring(0, 1).toUpperCase(Locale.US) + fieldName.substring(1));
+            Method method = clazz.getMethod("get" + fieldName.substring(0, 1).toUpperCase(Locale.US) + fieldName.substring(1));
             Class<?> returnType = method.getReturnType();
             for (Class<?> type : types) {
                 if (returnType.isAssignableFrom(type)) {
