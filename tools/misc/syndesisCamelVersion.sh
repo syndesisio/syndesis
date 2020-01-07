4b772b49535b7d337d26d1266104ab3803cc02a6
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2020-01-07 13:30:05 +0000

    fix: Upgrades camel version to released version 2.23.2.fuse-760016

--- tools/misc/syndesisCamelVersion.sh
+++ tools/misc/syndesisCamelVersion.sh
@@ -6,7 +6,7 @@
 #   - ./syndesisCamelVersion.sh <camel version>
 #
 # Example:
-#   - ./syndesisCamelVersion.sh 2.23.2.fuse-760011
+#   - ./syndesisCamelVersion.sh 2.23.2.fuse-760016
 
 
 new_camel=${1:-xxx}
