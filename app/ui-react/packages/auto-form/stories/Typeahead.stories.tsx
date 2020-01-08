824afa13947aa796ab88d6591c2f067dc96229eb
kahboom <rachelyordan@gmail.com>
2020-01-08 13:09:09 +0000

    on page load with default value, use placeholder text. add storybook
    lint fix

--- app/ui-react/packages/auto-form/stories/Typeahead.stories.tsx
+++ app/ui-react/packages/auto-form/stories/Typeahead.stories.tsx
@@ -12,47 +12,30 @@ const stories = storiesOf('Typeahead', module);
 
 stories.add('Creatable Option', () => {
   const definition = {
-    Creatable: {
+    creatable: {
       displayName: 'Pick or name a thing',
       enum: [
         {
           label: 'One Fish',
-          value: 'one',
+          value: 'One Fish',
         },
         {
           label: 'Two Fish',
-          value: 'two',
+          value: 'Two Fish',
         },
         {
           label: 'Red Fish',
-          value: 'three',
+          value: 'Red Fish',
         },
         {
           label: 'Blue Fish',
-          value: 'four',
+          value: 'Blue Fish',
         },
       ],
       labelHint: 'This is shown for the label hint text',
       order: 0,
       type: 'typeahead',
     },
-    Disabled: {
-      displayName: 'Disabled',
-      enum: [
-        {
-          label: 'Blue',
-          value: 'blue'
-        },
-        {
-          label: 'Red',
-          value: 'red'
-        }
-      ],
-      labelHint: 'Testing disabled select',
-      order: 1,
-      type: 'typeahead',
-      disabled: true
-    }
   };
   return (
     <StoryWrapper definition={definition}>
@@ -63,7 +46,7 @@ stories.add('Creatable Option', () => {
           'This property is required'
         )}
         initialValue={object('Initial Value', {
-          Creatable: ['four']
+          creatable: 'Blue Fish'
         })}
         validate={action('validate')}
         onSave={(val, bag) => {
@@ -79,3 +62,48 @@ stories.add('Creatable Option', () => {
   );
 });
 
+
+stories.add('Disabled Option', () => {
+  const definition = {
+    disabled: {
+      displayName: 'Disabled',
+      enum: [
+        {
+          label: 'Blue',
+          value: 'blue'
+        },
+        {
+          label: 'Red',
+          value: 'red'
+        }
+      ],
+      labelHint: 'Testing disabled select',
+      order: 1,
+      type: 'typeahead',
+      disabled: true
+    },
+  };
+  return (
+    <StoryWrapper definition={definition}>
+      <AutoForm
+        definition={object('Definition', definition)}
+        i18nRequiredProperty={text(
+          'i18nRequiredProperty',
+          'This property is required'
+        )}
+        initialValue={object('Initial Value', {
+          disabled: 'blue'
+        })}
+        validate={action('validate')}
+        onSave={(val, bag) => {
+          bag.setSubmitting(false);
+          action('onSave')(val);
+        }}
+      >
+        {({ fields, handleSubmit }) => (
+          <FormWrapper onSubmit={handleSubmit} fields={fields} />
+        )}
+      </AutoForm>
+    </StoryWrapper>
+  );
+});
