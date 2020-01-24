221621ddf1e0cce757576e788594e186611b4d4a
Luis Garcia Acosta <lgarciaac@gmail.com>
2020-01-24 15:11:51 +0100

    chore: add metrics for product and operator version
    Also creates service monitor. Change configuration variable name to avoid collition with package name.

--- install/operator/pkg/apis/syndesis/v1beta1/register.go
+++ install/operator/pkg/apis/syndesis/v1beta1/register.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // NOTE: Boilerplate only.  Ignore this file.
 
 // Package v1beta1 contains API Schema definitions for the syndesis v1beta1 API group
@@ -16,4 +32,6 @@ var (
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+
+	SchemaGroupVersionKind = schema.GroupVersionKind{Group: "syndesis.io", Version: "v1beta1", Kind: "Syndesis"}
 )
