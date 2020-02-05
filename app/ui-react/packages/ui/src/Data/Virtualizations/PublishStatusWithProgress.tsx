89dfb1e2e982d54cce141c9d755554302abba877
Mark Drilling <mdrillin@redhat.com>
2020-02-05 14:48:39 -0600

    TEIIDTOOLS-914 Adapt UI to publishing and deployment changes on backend

--- app/ui-react/packages/ui/src/Data/Virtualizations/PublishStatusWithProgress.tsx
+++ app/ui-react/packages/ui/src/Data/Virtualizations/PublishStatusWithProgress.tsx
@@ -78,7 +78,12 @@ export const PublishStatusWithProgress: React.FunctionComponent<IPublishStatusWi
         />
       )}
       <span className={'publish-status-with-progress_text'}>
-        {props.i18nPublishState}
+        <Label
+          className={'publish-status-with-progress_Label'}
+          type={props.labelType}
+        >
+          {props.i18nPublishState}
+        </Label>
         {props.publishVersion && ` version ${props.publishVersion}`}
       </span>
       {(props.i18nPublishState === 'Stopped' ||
