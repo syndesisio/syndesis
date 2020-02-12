c91c554c2cfcd35c68a704ebffbd996e375cc914
kahboom <rachelyordan@gmail.com>
2020-02-12 12:05:40 +0000

    convert class component to functional

--- app/ui-react/packages/ui/src/Customization/apiClientConnectors/ApiConnectorListView.tsx
+++ app/ui-react/packages/ui/src/Customization/apiClientConnectors/ApiConnectorListView.tsx
@@ -24,59 +24,67 @@ export interface IApiConnectorListViewProps extends IListViewToolbarProps {
   linkCreateApiConnector: string;
 }
 
-export class ApiConnectorListView extends React.Component<
-  IApiConnectorListViewProps
-> {
-  public render() {
-    return (
-      <PageSection>
-        <ListViewToolbar {...this.props}>
-          <div className={'form-group'}>
-            <ButtonLink data-testid={'api-connector-list-view-create-button'}
-                        href={this.props.linkCreateApiConnector}
-                        as={'primary'}>
-              {this.props.i18nLinkCreateApiConnector}
-            </ButtonLink>
-          </div>
-        </ListViewToolbar>
-        {this.props.i18nTitle !== '' && (
-          <Title size="xl">{this.props.i18nTitle}</Title>
-        )}
-        {this.props.i18nDescription !== '' && (
-          <Text
-            dangerouslySetInnerHTML={{ __html: this.props.i18nDescription }}
-          />
-        )}
-        {this.props.children ? (
-          <DataList aria-label={'api connector list'}>{this.props.children}</DataList>
-        ) : (
-          <EmptyState variant={EmptyStateVariant.full}>
-            <EmptyStateIcon icon={AddCircleOIcon} />
-            <Title headingLevel={'h5'} size={'lg'}>
-              {this.props.i18nEmptyStateTitle}
-            </Title>
-            <EmptyStateBody>{this.props.i18nEmptyStateInfo}</EmptyStateBody>
-            <Tooltip
-              position={'top'}
-              content={
-                this.props.i18nLinkCreateApiConnectorTip
-                  ? this.props.i18nLinkCreateApiConnectorTip
-                  : this.props.i18nLinkCreateApiConnector
-              }
-              enableFlip={true}
-            >
-              <>
-                <br/>
-                <ButtonLink data-testid={'api-connector-list-view-empty-state-create-button'}
-                            href={this.props.linkCreateApiConnector}
-                            as={'primary'}>
-                  {this.props.i18nLinkCreateApiConnector}
-                </ButtonLink>
-              </>
-            </Tooltip>
-          </EmptyState>
-        )}
-      </PageSection>
-    );
-  }
-}
+export const ApiConnectorListView: React.FunctionComponent<IApiConnectorListViewProps> = (
+  {
+    children,
+    i18nDescription,
+    i18nEmptyStateInfo,
+    i18nEmptyStateTitle,
+    i18nLinkCreateApiConnector,
+    i18nLinkCreateApiConnectorTip,
+    i18nName,
+    i18nTitle,
+    linkCreateApiConnector,
+    ...rest
+  }) => {
+  return (
+    <PageSection>
+      <ListViewToolbar {...rest}>
+        <div className={'form-group'}>
+          <ButtonLink data-testid={'api-connector-list-view-create-button'}
+                      href={linkCreateApiConnector}
+                      as={'primary'}>
+            {i18nLinkCreateApiConnector}
+          </ButtonLink>
+        </div>
+      </ListViewToolbar>
+      {i18nTitle !== '' && (
+        <Title size="xl">{i18nTitle}</Title>
+      )}
+      {i18nDescription !== '' && (
+        <Text
+          dangerouslySetInnerHTML={{ __html: i18nDescription }}
+        />
+      )}
+      {children ? (
+        <DataList aria-label={'api connector list'}>{children}</DataList>
+      ) : (
+        <EmptyState variant={EmptyStateVariant.full}>
+          <EmptyStateIcon icon={AddCircleOIcon}/>
+          <Title headingLevel={'h5'} size={'lg'}>
+            {i18nEmptyStateTitle}
+          </Title>
+          <EmptyStateBody>{i18nEmptyStateInfo}</EmptyStateBody>
+          <Tooltip
+            position={'top'}
+            content={
+              i18nLinkCreateApiConnectorTip
+                ? i18nLinkCreateApiConnectorTip
+                : i18nLinkCreateApiConnector
+            }
+            enableFlip={true}
+          >
+            <>
+              <br/>
+              <ButtonLink data-testid={'api-connector-list-view-empty-state-create-button'}
+                          href={linkCreateApiConnector}
+                          as={'primary'}>
+                {i18nLinkCreateApiConnector}
+              </ButtonLink>
+            </>
+          </Tooltip>
+        </EmptyState>
+      )}
+    </PageSection>
+  );
+};
