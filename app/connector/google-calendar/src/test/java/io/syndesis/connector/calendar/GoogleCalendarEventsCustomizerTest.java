d477ef42514716f486492488d2d8dc9826a5c8d1
Maria Arias de Reyna <ariasdereyna@redhat.com>
2020-02-19 13:09:02 +0100

    Fix: Nullpointer on tests due to no CamelContext

--- app/connector/google-calendar/src/test/java/io/syndesis/connector/calendar/GoogleCalendarEventsCustomizerTest.java
+++ app/connector/google-calendar/src/test/java/io/syndesis/connector/calendar/GoogleCalendarEventsCustomizerTest.java
@@ -18,9 +18,9 @@ package io.syndesis.connector.calendar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.api.services.calendar.model.Event;
-import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.support.DefaultExchange;
 import org.apache.camel.support.DefaultMessage;
 import org.junit.Test;
@@ -29,8 +29,9 @@ public class GoogleCalendarEventsCustomizerTest {
 
     @Test
     public void shouldConvertGoogleEventToConnectorEventModel() {
-        Exchange exchange = new DefaultExchange((CamelContext) null);
-        Message in = new DefaultMessage((CamelContext) null);
+        DefaultCamelContext cc = new DefaultCamelContext();
+        Exchange exchange = new DefaultExchange(cc);
+        Message in = new DefaultMessage(cc);
         in.setBody(new Event());
         exchange.setIn(in);
 
