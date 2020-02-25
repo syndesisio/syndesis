f1b09652b47cb9bc1c93f5aadb83405ebb740ead
Mark Drilling <mdrillin@redhat.com>
2020-02-25 15:20:24 -0600

    pf3-pf4 CiCd updates
    (cherry picked from commit ca5a5a1452b8bac604de85c1e9160a71b41d1530)

--- app/ui-react/packages/ui/src/Integration/CiCd/CiCdManagePageUI.tsx
+++ app/ui-react/packages/ui/src/Integration/CiCd/CiCdManagePageUI.tsx
@@ -44,160 +44,140 @@ export interface ICiCdManagePageUIProps extends IListViewToolbarProps {
   children: (props: ICiCdManagePageUIChildrenProps) => any;
 }
 
-export interface ICiCdManagePageUIState {
-  showAddDialog: boolean;
-  showEditDialog: boolean;
-  showRemoveDialog: boolean;
-  currentItemName?: string;
-}
+export const CiCdManagePageUI: React.FunctionComponent<ICiCdManagePageUIProps> = props => {
+  const [showAddDialog, setShowAddDialog] = React.useState(false);
+  const [showEditDialog, setShowEditDialog] = React.useState(false);
+  const [showRemoveDialog, setShowRemoveDialog] = React.useState(false);
+  const [currentItemName, setCurrentItemName] = React.useState();
 
-export class CiCdManagePageUI extends React.Component<
-  ICiCdManagePageUIProps,
-  ICiCdManagePageUIState
-> {
-  public constructor(props: ICiCdManagePageUIProps) {
-    super(props);
-    this.state = {
-      showAddDialog: false,
-      showEditDialog: false,
-      showRemoveDialog: false,
-    };
-    this.openAddDialog = this.openAddDialog.bind(this);
-    this.closeAddDialog = this.closeAddDialog.bind(this);
-    this.openEditDialog = this.openEditDialog.bind(this);
-    this.closeEditDialog = this.closeEditDialog.bind(this);
-    this.openRemoveDialog = this.openRemoveDialog.bind(this);
-    this.closeRemoveDialog = this.closeRemoveDialog.bind(this);
-    this.handleSave = this.handleSave.bind(this);
-    this.handleRemoveConfirm = this.handleRemoveConfirm.bind(this);
-  }
-  public handleSave(name: string) {
-    if (this.state.showEditDialog) {
-      this.closeEditDialog();
-      this.props.onEditItem(this.state.currentItemName!, name);
+  const handleSave = (name: string) => {
+    if (showEditDialog) {
+      closeEditDialog();
+      props.onEditItem(currentItemName!, name);
     }
-    if (this.state.showAddDialog) {
-      this.closeAddDialog();
-      this.props.onAddItem(name);
+    if (showAddDialog) {
+      closeAddDialog();
+      props.onAddItem(name);
     }
-    if (this.state.showRemoveDialog) {
-      this.closeRemoveDialog();
-      this.props.onRemoveItem(name);
+    if (showRemoveDialog) {
+      closeRemoveDialog();
+      props.onRemoveItem(name);
     }
-  }
-  public handleRemoveConfirm() {
-    this.handleSave(this.state.currentItemName!);
-  }
-  public openRemoveDialog(name: string) {
-    this.setState({ currentItemName: name, showRemoveDialog: true });
-  }
-  public closeRemoveDialog() {
-    this.setState({ showRemoveDialog: false });
-  }
-  public openAddDialog() {
-    this.setState({ currentItemName: '', showAddDialog: true });
-  }
-  public closeAddDialog() {
-    this.setState({ showAddDialog: false });
-  }
-  public openEditDialog(name: string) {
-    this.setState({ currentItemName: name, showEditDialog: true });
-  }
-  public closeEditDialog() {
-    this.setState({ showEditDialog: false });
-  }
-  public render() {
-    return (
-      <>
-        <SimplePageHeader
-          i18nTitle={this.props.i18nPageTitle}
-          i18nDescription={this.props.i18nPageDescription}
+  };
+  const handleRemoveConfirm = () => {
+    handleSave(currentItemName!);
+  };
+  const openRemoveDialog = (name: string) => {
+    setCurrentItemName(name);
+    setShowRemoveDialog(true);
+  };
+  const closeRemoveDialog = () => {
+    setShowRemoveDialog(false);
+  };
+  const openAddDialog = () => {
+    setCurrentItemName(name);
+    setShowAddDialog(true);
+  };
+  const closeAddDialog = () => {
+    setShowAddDialog(false);
+  };
+  const openEditDialog = (name: string) => {
+    setCurrentItemName(name);
+    setShowEditDialog(true);
+  };
+  const closeEditDialog = () => {
+    setShowEditDialog(false);
+  };
+  return (
+    <>
+      <SimplePageHeader
+        i18nTitle={props.i18nPageTitle}
+        i18nDescription={props.i18nPageDescription}
+      />
+      {showAddDialog && (
+        <CiCdEditDialog
+          key={currentItemName}
+          i18nTitle={props.i18nAddTagDialogTitle}
+          i18nDescription={props.i18nAddTagDialogDescription}
+          tagName={currentItemName!}
+          i18nInputLabel={props.i18nTagInputLabel}
+          i18nSaveButtonText={props.i18nSaveButtonText}
+          i18nCancelButtonText={props.i18nCancelButtonText}
+          i18nNoNameError={props.i18nNoNameError}
+          i18nNameInUseError={props.i18nNameInUseError}
+          validationError={props.nameValidationError}
+          onHide={() => {
+            closeAddDialog();
+            props.onCancelAddItem();
+          }}
+          onSave={handleSave}
+          onValidate={props.onValidateItem}
         />
-        {this.state.showAddDialog && (
-          <CiCdEditDialog
-            key={this.state.currentItemName}
-            i18nTitle={this.props.i18nAddTagDialogTitle}
-            i18nDescription={this.props.i18nAddTagDialogDescription}
-            tagName={this.state.currentItemName!}
-            i18nInputLabel={this.props.i18nTagInputLabel}
-            i18nSaveButtonText={this.props.i18nSaveButtonText}
-            i18nCancelButtonText={this.props.i18nCancelButtonText}
-            i18nNoNameError={this.props.i18nNoNameError}
-            i18nNameInUseError={this.props.i18nNameInUseError}
-            validationError={this.props.nameValidationError}
-            onHide={() => {
-              this.closeAddDialog();
-              this.props.onCancelAddItem();
-            }}
-            onSave={this.handleSave}
-            onValidate={this.props.onValidateItem}
-          />
-        )}
-        {this.state.showEditDialog && (
-          <CiCdEditDialog
-            key={this.state.currentItemName}
-            i18nTitle={this.props.i18nEditTagDialogTitle}
-            i18nDescription={this.props.i18nEditTagDialogDescription}
-            tagName={this.state.currentItemName!}
-            i18nInputLabel={this.props.i18nTagInputLabel}
-            i18nSaveButtonText={this.props.i18nSaveButtonText}
-            i18nCancelButtonText={this.props.i18nCancelButtonText}
-            i18nNoNameError={this.props.i18nNoNameError}
-            i18nNameInUseError={this.props.i18nNameInUseError}
-            validationError={this.props.nameValidationError}
-            onHide={() => {
-              this.closeEditDialog();
-              this.props.onCancelEditItem();
-            }}
-            onSave={this.handleSave}
-            onValidate={this.props.onValidateItem}
-          />
-        )}
-        {this.state.showRemoveDialog && (
-          <ConfirmationDialog
-            buttonStyle={ConfirmationButtonStyle.NORMAL}
-            icon={ConfirmationIconType.DANGER}
-            i18nCancelButtonText={this.props.i18nConfirmCancelButtonText}
-            i18nConfirmButtonText={this.props.i18nConfirmRemoveButtonText}
-            i18nConfirmationMessage={this.props.i18nRemoveConfirmationMessage(
-              this.state.currentItemName!
-            )}
-            i18nTitle={this.props.i18nRemoveConfirmationTitle}
-            i18nDetailsMessage={this.props.i18nRemoveConfirmationDetailMessage}
-            showDialog={this.state.showRemoveDialog}
-            onCancel={this.closeRemoveDialog}
-            onConfirm={this.handleRemoveConfirm}
-          />
-        )}
-        <CiCdListView
-          activeFilters={this.props.activeFilters}
-          currentFilterType={this.props.currentFilterType}
-          currentSortType={this.props.currentSortType}
-          currentValue={this.props.currentValue}
-          filterTypes={this.props.filterTypes}
-          isSortAscending={this.props.isSortAscending}
-          resultsCount={this.props.resultsCount}
-          sortTypes={this.props.sortTypes}
-          onUpdateCurrentValue={this.props.onUpdateCurrentValue}
-          onValueKeyPress={this.props.onValueKeyPress}
-          onFilterAdded={this.props.onFilterAdded}
-          onSelectFilterType={this.props.onSelectFilterType}
-          onFilterValueSelected={this.props.onFilterValueSelected}
-          onRemoveFilter={this.props.onRemoveFilter}
-          onClearFilters={this.props.onClearFilters}
-          onToggleCurrentSortDirection={this.props.onToggleCurrentSortDirection}
-          onUpdateCurrentSortType={this.props.onUpdateCurrentSortType}
-          i18nResultsCount={this.props.i18nResultsCount}
-          i18nAddNewButtonText={this.props.i18nAddNewButtonText}
-          onAddNew={this.openAddDialog}
-        >
-          {this.props.children({
-            openAddDialog: this.openAddDialog,
-            openEditDialog: this.openEditDialog,
-            openRemoveDialog: this.openRemoveDialog,
-          })}
-        </CiCdListView>
-      </>
-    );
-  }
-}
+      )}
+      {showEditDialog && (
+        <CiCdEditDialog
+          key={currentItemName}
+          i18nTitle={props.i18nEditTagDialogTitle}
+          i18nDescription={props.i18nEditTagDialogDescription}
+          tagName={currentItemName!}
+          i18nInputLabel={props.i18nTagInputLabel}
+          i18nSaveButtonText={props.i18nSaveButtonText}
+          i18nCancelButtonText={props.i18nCancelButtonText}
+          i18nNoNameError={props.i18nNoNameError}
+          i18nNameInUseError={props.i18nNameInUseError}
+          validationError={props.nameValidationError}
+          onHide={() => {
+            closeEditDialog();
+            props.onCancelEditItem();
+          }}
+          onSave={handleSave}
+          onValidate={props.onValidateItem}
+        />
+      )}
+      {showRemoveDialog && (
+        <ConfirmationDialog
+          buttonStyle={ConfirmationButtonStyle.NORMAL}
+          icon={ConfirmationIconType.DANGER}
+          i18nCancelButtonText={props.i18nConfirmCancelButtonText}
+          i18nConfirmButtonText={props.i18nConfirmRemoveButtonText}
+          i18nConfirmationMessage={props.i18nRemoveConfirmationMessage(
+            currentItemName!
+          )}
+          i18nTitle={props.i18nRemoveConfirmationTitle}
+          i18nDetailsMessage={props.i18nRemoveConfirmationDetailMessage}
+          showDialog={showRemoveDialog}
+          onCancel={closeRemoveDialog}
+          onConfirm={handleRemoveConfirm}
+        />
+      )}
+      <CiCdListView
+        activeFilters={props.activeFilters}
+        currentFilterType={props.currentFilterType}
+        currentSortType={props.currentSortType}
+        currentValue={props.currentValue}
+        filterTypes={props.filterTypes}
+        isSortAscending={props.isSortAscending}
+        resultsCount={props.resultsCount}
+        sortTypes={props.sortTypes}
+        onUpdateCurrentValue={props.onUpdateCurrentValue}
+        onValueKeyPress={props.onValueKeyPress}
+        onFilterAdded={props.onFilterAdded}
+        onSelectFilterType={props.onSelectFilterType}
+        onFilterValueSelected={props.onFilterValueSelected}
+        onRemoveFilter={props.onRemoveFilter}
+        onClearFilters={props.onClearFilters}
+        onToggleCurrentSortDirection={props.onToggleCurrentSortDirection}
+        onUpdateCurrentSortType={props.onUpdateCurrentSortType}
+        i18nResultsCount={props.i18nResultsCount}
+        i18nAddNewButtonText={props.i18nAddNewButtonText}
+        onAddNew={openAddDialog}
+      >
+        {props.children({
+          openAddDialog,
+          openEditDialog,
+          openRemoveDialog,
+        })}
+      </CiCdListView>
+    </>
+  );
+};
