959f7e1465507eba0bc94b70cbd366ea05ef5ab9
kahboom <rachelyordan@gmail.com>
2020-03-05 18:08:51 +0000

    map activity item step from syndesis package to make table work, add i18n
    additional i18n
    
    fix for table rendering, cleanup
    
    add colors for icons

--- app/ui-react/packages/ui/src/Integration/Activity/IntegrationDetailActivityItem.tsx
+++ app/ui-react/packages/ui/src/Integration/Activity/IntegrationDetailActivityItem.tsx
@@ -1,76 +1,98 @@
 import {
-  //Badge,
   DataListCell,
   DataListContent,
   DataListItem,
   DataListItemCells,
   DataListItemRow,
   DataListToggle,
-  //Flex,
-  //FlexItem,
 } from '@patternfly/react-core';
 import { ErrorCircleOIcon, OkIcon } from '@patternfly/react-icons';
-//import { ListView, Table } from 'patternfly-react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableVariant
+} from '@patternfly/react-table';
+import {
+  global_danger_color_100,
+  global_success_color_100,
+} from '@patternfly/react-tokens';
 import * as React from 'react';
 
 import './IntegrationDetailActivityItem.css';
-import { useState } from "react";
+import { useState } from 'react';
 
 export interface IIntegrationDetailActivityItemProps {
   date: string;
   errorCount: number;
+  i18nError: string;
   i18nErrorsFound: string;
-  i18nHeaderStep?: string;
-  i18nHeaderTime?: string;
-  i18nHeaderDuration?: string;
+  i18nHeaderStep: string;
+  i18nHeaderTime: string;
+  i18nHeaderDuration: string;
   i18nHeaderDurationUnit?: string;
-  i18nHeaderStatus?: string;
-  i18nHeaderOutput?: string;
+  i18nHeaderStatus: string;
+  i18nHeaderOutput: string;
   i18nNoErrors: string;
-  i18nNoOutput?: string;
+  i18nNoOutput: string;
   i18nNoSteps: string;
+  i18nSuccess: string;
   i18nVersion: string;
-  steps: JSX.Element[];
+  steps: any;
   time: string;
   version?: string;
 }
-/*
-
-const headerFormat = (value: string) => <Table.Heading>{value}</Table.Heading>;
-const cellFormat = (value: string) => <Table.Cell>{value}</Table.Cell>;
-const statusCellFormat = (status: string) => (
-  <Table.Cell className="integration-detail-activity-item__status">
-    {status === 'Success' ? (
-      <>
-        <OkIcon /> Success
-      </>
-    ) : (
-      <>
-        <ErrorCircleOIcon /> Error
-      </>
-    )}
-  </Table.Cell>
-);
-const outputCellFormat = (output: string) => (
-  <Table.Cell className="integration-detail-activity-item__output">
-    <pre className="integration-detail-activity-item__output-step-data">
-      {output || 'No output'}
-    </pre>
-  </Table.Cell>
-);
-*/
 
 export const IntegrationDetailActivityItem: React.FC<
   IIntegrationDetailActivityItemProps
 > = ( props ) => {
-  //const toggleExpanded = (id: string) => {};
-
-  // initial visibility of delete dialog
   const [rowExpanded, setRowExpanded] = useState(false);
 
   const doExpand = () => {
     setRowExpanded(!rowExpanded);
   };
+
+  const rows = () => {
+    const newRows = props.steps.map((step: any) => {
+      return [{
+        cells: [
+          step.name,
+          step.date,
+          step.duration,
+          {
+            title: (
+              step.status === 'Success' ? (
+                <>
+                  <OkIcon size={'sm'} color={global_success_color_100.value} />{'  '}{props.i18nSuccess}
+                </>
+              ) : (
+                <>
+                  <ErrorCircleOIcon size={'sm'} color={global_danger_color_100.value} />{'  '}{props.i18nError}
+                </>
+              )
+            )
+          },
+          {
+            title: (
+              <pre className={'integration-detail-activity-item__output-step-data'}>
+                {step.output || props.i18nNoOutput}
+              </pre>
+            )
+          }
+        ],
+      }];
+    });
+
+    return newRows.reduce((a: any, b: any) => a.concat(b), []);
+  };
+
+  const columns = [
+    props.i18nHeaderStep,
+    props.i18nHeaderTime,
+    props.i18nHeaderDuration,
+    props.i18nHeaderStatus,
+    props.i18nHeaderOutput
+  ];
 
   return (
     <DataListItem aria-labelledby="activity item" isExpanded={rowExpanded} className={'integration-detail-activity-item'}>
@@ -78,7 +100,7 @@ export const IntegrationDetailActivityItem: React.FC<
         <DataListToggle
           onClick={doExpand}
           isExpanded={rowExpanded}
-          id="activity-toggle"
+          id="activity-item-toggle"
         />
         <DataListItemCells
           dataListCells={[
@@ -97,135 +119,41 @@ export const IntegrationDetailActivityItem: React.FC<
               <div className={'integration-detail-activity-item__status-item'}>
                 {props.errorCount > 0 ? (
                   <>
-                    <ErrorCircleOIcon />
+                    <ErrorCircleOIcon size={'sm'} color={global_danger_color_100.value} />
                     {'  '}
                     {props.i18nErrorsFound}
                   </>
                 ) : (
                   <>
-                    <OkIcon />
+                    <OkIcon size={'sm'} color={global_success_color_100.value} />
                     {'  '}
                     {props.i18nNoErrors}
                   </>
                 )}
               </div>
-              {/*<Flex>
-                <FlexItem>
-                  <span id="time">Errors</span>
-                </FlexItem>
-                <FlexItem>
-                  <Badge isRead>2</Badge>
-                </FlexItem>
-              </Flex>*/}
             </DataListCell>,
           ]}
         />
       </DataListItemRow>
       <DataListContent
-        aria-label="Primary Content Details"
-        id="ex-expand1"
+        aria-label="Activity Item"
+        id="activity-item"
         isHidden={!rowExpanded}
       >
-        <p>
-          Something goes here.
-        </p>
+        {props.steps ? (
+          <Table
+            aria-label={'integration detail activity item table'}
+            cells={columns}
+            rows={rows()}
+            variant={TableVariant.compact}
+          >
+            <TableHeader />
+            <TableBody />
+          </Table>
+        ) : (
+          <span>{props.i18nNoSteps}</span>
+        )}
       </DataListContent>
     </DataListItem>
-    /*<ListViewItem
-      className={'integration-detail-activity-item'}
-      key={props.time}
-      heading={props.date}
-      description={props.time}
-      additionalInfo={[
-        <ListView.InfoItem key={1}>
-          {props.i18nVersion}
-          &nbsp;
-          {props.version}
-        </ListView.InfoItem>,
-        <ListView.InfoItem key={2}>
-          <div className={'integration-detail-activity-item__status-item'}>
-            {props.errorCount > 0 ? (
-              <>
-                <ErrorCircleOIcon />
-                {'  '}
-                {props.i18nErrorsFound}
-              </>
-            ) : (
-              <>
-                <OkIcon />
-                {'  '}
-                {props.i18nNoErrors}
-              </>
-            )}
-          </div>
-        </ListView.InfoItem>,
-      ]}
-    >
-      {props.steps ? (
-        <Table.PfProvider
-          className={'integration-detail-activity-item__expanded-table'}
-          striped={true}
-          bordered={true}
-          hover={true}
-          columns={[
-            {
-              cell: {
-                formatters: [cellFormat],
-                property: 'step',
-              },
-              header: {
-                formatters: [headerFormat],
-                label: 'Step',
-              },
-            },
-            {
-              cell: {
-                formatters: [cellFormat],
-                property: 'time',
-              },
-              header: {
-                formatters: [headerFormat],
-                label: 'Time',
-              },
-            },
-            {
-              cell: {
-                formatters: [cellFormat],
-                property: 'duration',
-              },
-              header: {
-                formatters: [headerFormat],
-                label: 'Duration',
-              },
-            },
-            {
-              cell: {
-                formatters: [statusCellFormat],
-                property: 'status',
-              },
-              header: {
-                formatters: [headerFormat],
-                label: 'Status',
-              },
-            },
-            {
-              cell: {
-                formatters: [outputCellFormat],
-                property: 'output',
-              },
-              header: {
-                formatters: [headerFormat],
-                label: 'Output',
-              },
-            },
-          ]}
-        >
-          <Table.Header />
-          {props.steps}
-        </Table.PfProvider>
-      ) : (
-        <span>{props.i18nNoSteps}</span>
-      )}
-    </ListViewItem>*/
   );
 };
