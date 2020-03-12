e7d3d2f2820907d9875af81ffe3c51535fb715ab
kahboom <rachelyordan@gmail.com>
2020-03-12 11:58:08 +0000

    switch IntegrationDetailHistoryListViewItemActions to function component
    (cherry picked from commit 21b02870778984756d2dc75d44ae60281c687571)

--- app/ui-react/packages/ui/src/Integration/Details/IntegrationDetailHistoryListViewItemActions.tsx
+++ app/ui-react/packages/ui/src/Integration/Details/IntegrationDetailHistoryListViewItemActions.tsx
@@ -9,47 +9,49 @@ export interface IIntegrationDetailHistoryListViewItemActionsProps {
   integrationId: string;
 }
 
-export class IntegrationDetailHistoryListViewItemActions extends React.Component<
+export const IntegrationDetailHistoryListViewItemActions: React.FunctionComponent<
   IIntegrationDetailHistoryListViewItemActionsProps
-> {
-  public render() {
-    return (
-      <DropdownKebab
-        id={`integration-${this.props.integrationId}-action-menu`}
-        pullRight={true}
-      >
-        {this.props.actions.map((a, index) => {
-          return (
-            <li role={'presentation'} key={index}>
-              {a.href ? (
-                <Link
-                  data-testid={`integration-detail-history-list-view-item-actions-${toValidHtmlId(
-                    a.label.toString()
-                  )}`}
-                  to={a.href}
-                  onClick={a.onClick}
-                  role={'menuitem'}
-                  tabIndex={index + 1}
-                >
-                  {a.label}
-                </Link>
-              ) : (
-                <a
-                  data-testid={`integration-detail-history-list-view-item-actions-${toValidHtmlId(
-                    a.label.toString()
-                  )}`}
-                  href={'javascript:void(0)'}
-                  onClick={a.onClick}
-                  role={'menuitem'}
-                  tabIndex={index + 1}
-                >
-                  {a.label}
-                </a>
-              )}
-            </li>
-          );
-        })}
-      </DropdownKebab>
-    );
-  }
-}
+> = (
+  {
+    actions,
+    integrationId
+  }) => {
+  return (
+    <DropdownKebab
+      id={`integration-${integrationId}-action-menu`}
+      pullRight={true}
+    >
+      {actions.map((a, index) => {
+        return (
+          <li role={'presentation'} key={index}>
+            {a.href ? (
+              <Link
+                data-testid={`integration-detail-history-list-view-item-actions-${toValidHtmlId(
+                  a.label.toString()
+                )}`}
+                to={a.href}
+                onClick={a.onClick}
+                role={'menuitem'}
+                tabIndex={index + 1}
+              >
+                {a.label}
+              </Link>
+            ) : (
+              <a
+                data-testid={`integration-detail-history-list-view-item-actions-${toValidHtmlId(
+                  a.label.toString()
+                )}`}
+                href={'javascript:void(0)'}
+                onClick={a.onClick}
+                role={'menuitem'}
+                tabIndex={index + 1}
+              >
+                {a.label}
+              </a>
+            )}
+          </li>
+        );
+      })}
+    </DropdownKebab>
+  );
+};
