278c698e2aa8dc239da508f4271a057b9819eef4
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2020-03-17 11:44:12 +0100

    feat(connector/kafka): SSL verification support
    * Adding TLS configuration to enable verification over encrypted layer
    * Adding a cleaning method to remove temporary keystore resources
    
    Closes ENTESB-13134

--- app/connector/kafka/src/main/java/io/syndesis/connector/kafka/KafkaVerifierExtension.java
+++ app/connector/kafka/src/main/java/io/syndesis/connector/kafka/KafkaVerifierExtension.java
@@ -16,6 +16,7 @@
 package io.syndesis.connector.kafka
     ;
 
+import io.syndesis.connector.support.util.KeyStoreHelper;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.extension.verifier.DefaultComponentVerifierExtension;
 import org.apache.camel.component.extension.verifier.ResultBuilder;
@@ -28,11 +29,13 @@ import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.syndesis.connector.support.util.ConnectorOptions;
+
 import java.util.Map;
 import java.util.Properties;
 
 /**
  * Component verifier for Kafka Connector.
+ *
  * @author valdar
  */
 public class KafkaVerifierExtension extends DefaultComponentVerifierExtension {
@@ -50,7 +53,7 @@ public class KafkaVerifierExtension extends DefaultComponentVerifierExtension {
     @Override
     protected Result verifyParameters(Map<String, Object> parameters) {
         ResultBuilder builder = ResultBuilder.withStatusAndScope(Result.Status.OK, Scope.PARAMETERS)
-                .error(ResultErrorHelper.requiresOption("brokers", parameters));
+            .error(ResultErrorHelper.requiresOption("brokers", parameters));
 
         return builder.build();
     }
@@ -61,26 +64,24 @@ public class KafkaVerifierExtension extends DefaultComponentVerifierExtension {
     @Override
     protected Result verifyConnectivity(Map<String, Object> parameters) {
         return ResultBuilder.withStatusAndScope(Result.Status.OK, Scope.CONNECTIVITY)
-                .error(parameters, KafkaVerifierExtension::verifyCredentials)
-                .build();
+            .error(parameters, KafkaVerifierExtension::verifyCredentials)
+            .build();
     }
 
     private static void verifyCredentials(ResultBuilder builder, Map<String, Object> parameters) {
         final String brokers = ConnectorOptions.extractOption(parameters, "brokers");
+        final String certificate = ConnectorOptions.extractOption(parameters, "brokerCertificate");
+        final String transportProtocol = ConnectorOptions.extractOption(parameters, "transportProtocol");
 
-        LOG.debug("Validating Kafka connection to {}", brokers);
+        LOG.debug("Validating Kafka connection to {} with protocol {}", brokers, transportProtocol);
         if (ObjectHelper.isNotEmpty(brokers)) {
-            Properties properties = new Properties();
-            properties.put("bootstrap.servers", brokers);
-            properties.put("connections.max.idle.ms", 10000);
-            properties.put("request.timeout.ms", 5000);
-            try (AdminClient client = KafkaAdminClient.create(properties))
-            {
+            // Use a key store helper if a self signed certificate is provided
+            KeyStoreHelper brokerKeyStoreHelper = certificate != null ? new KeyStoreHelper(certificate, "brokerCertificate").store() : null;
+            Properties properties = getKafkaAdminClientConfiguration(brokers, transportProtocol, brokerKeyStoreHelper);
+            try (AdminClient client = KafkaAdminClient.create(properties)) {
                 ListTopicsResult topics = client.listTopics();
                 topics.names().get();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 builder.error(
                     ResultErrorBuilder.withCodeAndDescription(VerificationError.StandardCode.ILLEGAL_PARAMETER_VALUE, "Unable to connect to Kafka broker")
                         .parameterKey("brokers")
@@ -88,6 +89,14 @@ public class KafkaVerifierExtension extends DefaultComponentVerifierExtension {
                         .detail(VerificationError.ExceptionAttribute.EXCEPTION_CLASS, e.getClass().getName())
                         .build()
                 );
+            } finally {
+                if (brokerKeyStoreHelper != null) {
+                    // Clean up temporary resources used by key store
+                    boolean keystoreDeleted = brokerKeyStoreHelper.clean();
+                    if (!keystoreDeleted) {
+                        LOG.warn("Impossible to delete temporary keystore located at " + brokerKeyStoreHelper.getKeyStorePath());
+                    }
+                }
             }
         } else {
             builder.error(
@@ -96,5 +105,25 @@ public class KafkaVerifierExtension extends DefaultComponentVerifierExtension {
                     .build()
             );
         }
+    }
+
+    private static Properties getKafkaAdminClientConfiguration(String brokers, String transportProtocol, KeyStoreHelper brokerKeyStoreHelper) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", brokers);
+        properties.put("connections.max.idle.ms", 10000);
+        properties.put("request.timeout.ms", 5000);
+        if (!"PLAINTEXT".equals(transportProtocol)) {
+            properties.put("security.protocol", "SSL");
+            if (brokerKeyStoreHelper != null) {
+                properties.put("ssl.endpoint.identification.algorithm", "");
+                properties.put("ssl.keystore.location", brokerKeyStoreHelper.getKeyStorePath());
+                properties.put("ssl.keystore.password", brokerKeyStoreHelper.getPassword());
+                properties.put("ssl.key.password", brokerKeyStoreHelper.getPassword());
+                properties.put("ssl.truststore.location", brokerKeyStoreHelper.getKeyStorePath());
+                properties.put("ssl.truststore.password", brokerKeyStoreHelper.getPassword());
+
+            }
+        }
+        return properties;
     }
 }
