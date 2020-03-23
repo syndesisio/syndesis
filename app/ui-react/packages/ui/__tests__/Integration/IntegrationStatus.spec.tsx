645911c7d130d7c0881ffe6e71204c044ae852b5
Stan Lewis <gashcrumb@gmail.com>
2020-03-23 09:51:02 -0400

    chore: migrate from jest-dom to @testing-library/jest-dom
    (cherry picked from commit 859930f21b264c5ea6d5894cf73065161ac6ccc6)

--- app/ui-react/packages/ui/__tests__/Integration/IntegrationStatus.spec.tsx
+++ app/ui-react/packages/ui/__tests__/Integration/IntegrationStatus.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 import { cleanup, render } from '@testing-library/react';
 import { IntegrationStatus } from '../../src/Integration';
 
