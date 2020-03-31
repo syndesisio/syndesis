3802918e712a7846c6b1051845a75768364eb84c
akoserwa <akoserwa@redhat.com>
2020-03-31 19:38:55 +0530

    updating service monitoring & service metric logic generated based operator-sdk v15 plus version

--- install/operator/pkg/cmd/internal/run/run.go
+++ install/operator/pkg/cmd/internal/run/run.go
@@ -17,42 +17,43 @@
 package run
 
 import (
-	"fmt"
-	"runtime"
+    "context"
+    "fmt"
+    "k8s.io/client-go/rest"
+    "runtime"
 
-	"github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus"
 
-	kubemetrics "github.com/operator-framework/operator-sdk/pkg/kube-metrics"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	customMetrics "sigs.k8s.io/controller-runtime/pkg/metrics"
+    kubemetrics "github.com/operator-framework/operator-sdk/pkg/kube-metrics"
+    customMetrics "sigs.k8s.io/controller-runtime/pkg/metrics"
 
-	"github.com/syndesisio/syndesis/install/operator/pkg/syndesis/versions"
-	v1 "k8s.io/api/core/v1"
+    "github.com/syndesisio/syndesis/install/operator/pkg/syndesis/versions"
+    v1 "k8s.io/api/core/v1"
 
-	"github.com/syndesisio/syndesis/install/operator/pkg"
+    "github.com/syndesisio/syndesis/install/operator/pkg"
 
-	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
-	"github.com/operator-framework/operator-sdk/pkg/leader"
-	"github.com/operator-framework/operator-sdk/pkg/log/zap"
-	"github.com/operator-framework/operator-sdk/pkg/metrics"
-	"github.com/operator-framework/operator-sdk/pkg/restmapper"
-	"github.com/pkg/errors"
-	"github.com/spf13/cobra"
-	"github.com/syndesisio/syndesis/install/operator/pkg/apis/syndesis/v1beta1"
-	"github.com/syndesisio/syndesis/install/operator/pkg/cmd/internal"
-	"github.com/syndesisio/syndesis/install/operator/pkg/syndesis/configuration"
-	"github.com/syndesisio/syndesis/install/operator/pkg/util"
-	"k8s.io/apimachinery/pkg/util/intstr"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
+    "github.com/operator-framework/operator-sdk/pkg/k8sutil"
+    "github.com/operator-framework/operator-sdk/pkg/leader"
+    "github.com/operator-framework/operator-sdk/pkg/log/zap"
+    "github.com/operator-framework/operator-sdk/pkg/metrics"
+    "github.com/operator-framework/operator-sdk/pkg/restmapper"
+    "github.com/pkg/errors"
+    "github.com/spf13/cobra"
+    "github.com/syndesisio/syndesis/install/operator/pkg/apis/syndesis/v1beta1"
+    "github.com/syndesisio/syndesis/install/operator/pkg/cmd/internal"
+    "github.com/syndesisio/syndesis/install/operator/pkg/syndesis/configuration"
+    "github.com/syndesisio/syndesis/install/operator/pkg/util"
+    "k8s.io/apimachinery/pkg/util/intstr"
+    "sigs.k8s.io/controller-runtime/pkg/client/config"
+    "sigs.k8s.io/controller-runtime/pkg/manager"
 
-	sdkVersion "github.com/operator-framework/operator-sdk/version"
-	"github.com/syndesisio/syndesis/install/operator/pkg/apis"
-	"github.com/syndesisio/syndesis/install/operator/pkg/controller"
-	"github.com/syndesisio/syndesis/install/operator/pkg/openshift"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
+    sdkVersion "github.com/operator-framework/operator-sdk/version"
+    "github.com/syndesisio/syndesis/install/operator/pkg/apis"
+    "github.com/syndesisio/syndesis/install/operator/pkg/controller"
+    "github.com/syndesisio/syndesis/install/operator/pkg/openshift"
+    logf "sigs.k8s.io/controller-runtime/pkg/log"
 
-	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+    "sigs.k8s.io/controller-runtime/pkg/manager/signals"
 )
 
 var (
@@ -187,42 +188,74 @@ func (o *options) run() error {
 		return err
 	}
 
-	// Setup metrics. Serves Operator/CustomResource GVKs and generates metrics based on those types
-	installationGVK := []schema.GroupVersionKind{v1beta1.SchemaGroupVersionKind}
-
-	// To generate metrics in other namespaces, add the values below.
-	ns := []string{namespace}
-	// Generate and serve custom resource specific metrics.
-	err = kubemetrics.GenerateAndServeCRMetrics(cfg, ns, installationGVK, metricsHost, operatorMetricsPort)
-	if err != nil {
-		return err
-	}
-
-	// Create Service object to expose the metrics port.
-	servicePorts := []v1.ServicePort{
-		{Port: metricsPort, Name: metrics.OperatorPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: metricsPort}},
-		{Port: operatorMetricsPort, Name: metrics.CRPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: operatorMetricsPort}},
-	}
-
-	service, err := metrics.CreateMetricsService(ctx, cfg, servicePorts)
-	if err != nil {
-		log.Info("Could not create metrics Service", "error", err.Error())
-	}
-
-	services := []*v1.Service{service}
-	_, err = metrics.CreateServiceMonitors(cfg, namespace, services)
-	if err != nil {
-		log.Info("Could not create ServiceMonitor object", "error", err.Error())
-		// If this operator is deployed to a cluster without the prometheus-operator running, it will return
-		// ErrServiceMonitorNotPresent, which can be used to safely skip ServiceMonitor creation.
-		if err == metrics.ErrServiceMonitorNotPresent {
-			log.Info("Install prometheus-operator in your cluster to create ServiceMonitor objects", "error", err.Error())
-		}
-	}
+    // Add the Metrics Service
+    addMetrics(ctx, cfg, namespace)
 
 	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+
+// addMetrics will create the Services and Service Monitors to allow the operator export the metrics by using
+// the Prometheus operator
+func addMetrics(ctx context.Context, cfg *rest.Config, namespace string) {
+    if err := serveCRMetrics(cfg); err != nil {
+        if errors.Is(err, k8sutil.ErrRunLocal) {
+            log.Info("Skipping CR metrics server creation; not running in a cluster.")
+            return
+        }
+        log.Info("Could not generate and serve custom resource metrics", "error", err.Error())
+    }
+
+    // Add to the below struct any other metrics ports you want to expose.
+    servicePorts := []v1.ServicePort{
+        {Port: metricsPort, Name: metrics.OperatorPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: metricsPort}},
+        {Port: operatorMetricsPort, Name: metrics.CRPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: operatorMetricsPort}},
+    }
+
+    // Create Service object to expose the metrics port(s).
+    service, err := metrics.CreateMetricsService(ctx, cfg, servicePorts)
+    if err != nil {
+        log.Info("Could not create metrics Service", "error", err.Error())
+    }
+
+    // CreateServiceMonitors will automatically create the prometheus-operator ServiceMonitor resources
+    // necessary to configure Prometheus to scrape metrics from this operator.
+    services := []*v1.Service{service}
+    _, err = metrics.CreateServiceMonitors(cfg, namespace, services)
+    if err != nil {
+        log.Info("Could not create ServiceMonitor object", "error", err.Error())
+        // If this operator is deployed to a cluster without the prometheus-operator running, it will return
+        // ErrServiceMonitorNotPresent, which can be used to safely skip ServiceMonitor creation.
+        if err == metrics.ErrServiceMonitorNotPresent {
+            log.Info("Install prometheus-operator in your cluster to create ServiceMonitor objects", "error", err.Error())
+        }
+    }
+}
+
+// serveCRMetrics gets the Operator/CustomResource GVKs and generates metrics based on those types.
+// It serves those metrics on "http://metricsHost:operatorMetricsPort".
+func serveCRMetrics(cfg *rest.Config) error {
+    // Below function returns filtered operator/CustomResource specific GVKs.
+    // For more control override the below GVK list with your own custom logic.
+    filteredGVK, err := k8sutil.GetGVKsFromAddToScheme(apis.AddToScheme)
+    if err != nil {
+        return err
+    }
+    // Get the namespace the operator is currently deployed in.
+    operatorNs, err := k8sutil.GetOperatorNamespace()
+    if err != nil {
+        return err
+    }
+    // To generate metrics in other namespaces, add the values below.
+    ns := []string{operatorNs}
+    // Generate and serve custom resource specific metrics.
+    err = kubemetrics.GenerateAndServeCRMetrics(cfg, ns, filteredGVK, metricsHost, operatorMetricsPort)
+    if err != nil {
+        return err
+    }
+    return nil
 }
