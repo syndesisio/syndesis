34532b45e158bc8b42c670ee1af2afcc4b6b22ab
Kurt Stam <kstam@redhat.com>
2020-04-21 09:37:54 +0200

    Fix for TodoOpenApiV3_IT #8215

--- app/connector/api-provider/src/test/java/io/syndesis/connector/apiprovider/OnExceptionHandlerTest.java
+++ app/connector/api-provider/src/test/java/io/syndesis/connector/apiprovider/OnExceptionHandlerTest.java
@@ -50,7 +50,7 @@ public class OnExceptionHandlerTest {
 
         CamelContext context = new DefaultCamelContext();
         Exchange exchange = new ExchangeBuilder(context).build();
-        exchange.setException(e);
+        exchange.setProperty(Exchange.EXCEPTION_CAUGHT, e);
 
         ApiProviderOnExceptionHandler handler = new ApiProviderOnExceptionHandler();
         handler.setProperties(configuredProperties);
@@ -75,7 +75,7 @@ public class OnExceptionHandlerTest {
                 "ENTITY_NOT_FOUND_ERROR", "entity not found");
 
         Exchange exchange = new ExchangeBuilder(new DefaultCamelContext()).build();
-        exchange.setException(e);
+        exchange.setProperty(Exchange.EXCEPTION_CAUGHT, e);
 
         ApiProviderOnExceptionHandler handler = new ApiProviderOnExceptionHandler();
         handler.setProperties(configuredProperties);
