36965c8e103d9347b82c3bab333e63a73794c41d
kahboom <rachelyordan@gmail.com>
2020-05-06 10:20:19 +0100

    add table row UI component for abstraction, update API call for useExtensions
    update typings

---
+++ app/ui-react/packages/ui/src/Integration/Editor/IntegrationEditorExtensionTable.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import { useEffect } from 'react';
+import { IntegrationEditorExtensionTableRows } from './IntegrationEditorExtensionTableRows';
+
+interface IExtensionProps {
+  description: string;
+  extensionId: string;
+  lastUpdated: number;
+  name: string;
+}
+
+export interface IIntegrationEditorExtensionTableProps {
+  /**
+   * List of extensions available as provided from
+   * the API.
+   */
+  extensionsAvailable: IExtensionProps[];
+  i18nHeaderDescription: string;
+  i18nHeaderLastUpdated: string;
+  i18nHeaderName: string;
+  i18nTableDescription: string;
+  i18nTableName: string;
+  /**
+   * When a change is detected and a row is selected,
+   * both the UI row state and @syndesis or story component
+   * state are updated. This is the function that is
+   * called to maintain the latter.
+   * @param extensionId
+   * @param selected
+   */
+  onSelect: (extensionIds: string[]) => void;
+  /**
+   * These are provided by the API, and determine
+   * which rows will be selected on page load.
+   * The state of these is maintained separately
+   * from the UI's state of the rows, because the format
+   * required for PF react table is different to that
+   * expected by the API once the integration is saved.
+   */
+  preSelectedExtensionIds: string[];
+}
+
+export const IntegrationEditorExtensionTable: React.FunctionComponent<IIntegrationEditorExtensionTableProps> = (
+  {
+    extensionsAvailable,
+    i18nHeaderDescription,
+    i18nHeaderLastUpdated,
+    i18nHeaderName,
+    i18nTableDescription,
+    i18nTableName,
+    onSelect,
+    preSelectedExtensionIds
+  }) => {
+  /**
+   * Table state for array of IDs for selected extensions,
+   * starting with the preselected list provided by the API.
+   */
+  const [selectedExtensionIds, setSelectedExtensionIds] = React.useState<string[]>(
+    preSelectedExtensionIds
+  );
+
+  const handleSelectExtension = (extensionId: string) => {
+    /**
+     * Make a shallow copy of selectedExtensions array
+     */
+    const tempArray = selectedExtensionIds.slice();
+    if (!tempArray.includes(extensionId)) {
+      tempArray.push(extensionId);
+    }
+
+    setSelectedExtensionIds(tempArray);
+  };
+
+  const handleDeselectExtension = (extensionId: string) => {
+    /**
+     * Make a shallow copy of selectedExtensions array,
+     * then find the index of the selected id.
+     */
+    const tempArray = selectedExtensionIds.filter(id => id !== extensionId);
+    setSelectedExtensionIds(tempArray);
+  };
+
+  const onTableChange = (extensionId: string, isSelected: boolean) => {
+    if (isSelected) {
+      handleSelectExtension(extensionId);
+    } else {
+      handleDeselectExtension(extensionId);
+    }
+  };
+
+  const onTableChangeAll = (newList: string[]) => {
+    setSelectedExtensionIds(newList);
+  };
+
+  const callOnSelect = React.useCallback(() => {
+    onSelect(selectedExtensionIds);
+  }, [onSelect, selectedExtensionIds]);
+
+  useEffect(() => {
+    callOnSelect();
+  }, [callOnSelect]);
+
+  return (
+    <IntegrationEditorExtensionTableRows
+      extensionsAvailable={extensionsAvailable}
+      extensionIdsSelected={preSelectedExtensionIds}
+      i18nHeaderDescription={i18nHeaderDescription}
+      i18nHeaderLastUpdated={i18nHeaderLastUpdated}
+      i18nHeaderName={i18nHeaderName}
+      i18nTableDescription={i18nTableDescription}
+      i18nTableName={i18nTableName}
+      onSelect={onTableChange}
+      onSelectAll={onTableChangeAll}
+    />
+  );
+}
---
+++ app/ui-react/packages/ui/src/Integration/Editor/IntegrationEditorExtensionTableRows.tsx
@@ -0,0 +1,172 @@
+import {
+  IRow,
+  Table,
+  TableBody,
+  TableHeader,
+  TableVariant
+} from '@patternfly/react-table';
+import * as React from 'react';
+
+interface IExtensionProps {
+  description: string;
+  extensionId: string;
+  lastUpdated: number;
+  name: string;
+  selected?: boolean;
+}
+
+export interface IIntegrationEditorExtensionTableRowsProps {
+  /**
+   * List of extensions available as provided from
+   * the API.
+   */
+  extensionsAvailable: IExtensionProps[];
+  /**
+   * These are provided by the API, and determine
+   * which rows will be selected on page load.
+   * The state of these is maintained separately
+   * from the UI's state of the rows, because the format
+   * required for PF react table is different to that
+   * expected by the API once the integration is saved.
+   */
+  extensionIdsSelected: string[];
+  i18nHeaderDescription: string;
+  i18nHeaderLastUpdated: string;
+  i18nHeaderName: string;
+  i18nTableDescription: string;
+  i18nTableName: string;
+  /**
+   * When a change is detected and a row is selected,
+   * both the UI row state and @syndesis or story component
+   * state are updated. This is the function that is
+   * called to maintain the latter.
+   * @param extensionId
+   * @param selected
+   */
+  onSelect: (extensionId: string, selected: boolean) => void;
+  onSelectAll: (extensionIds: string[]) => void;
+}
+
+export const IntegrationEditorExtensionTableRows: React.FunctionComponent<IIntegrationEditorExtensionTableRowsProps> = (
+  {
+    extensionsAvailable,
+    extensionIdsSelected,
+    i18nHeaderDescription,
+    i18nHeaderLastUpdated,
+    i18nHeaderName,
+    i18nTableDescription,
+    i18nTableName,
+    onSelect,
+    onSelectAll
+  }) => {
+  const columns = [
+    i18nHeaderName,
+    i18nHeaderDescription,
+    i18nHeaderLastUpdated,
+  ];
+
+  let rows;
+
+  const getTableRows = React.useCallback(() => {
+    const tableRows: IRow[] = [];
+
+    for (const extension of extensionsAvailable) {
+      /**
+       * Format date
+       */
+      // const formattedDate = extension.lastUpdated.toString();
+      // const formattedDate = extension.lastUpdated ? new Date(extension.lastUpdated).toLocaleString() : ''
+      /**
+       * Create format for PF tables
+       * Check for pre-selected extensions by their IDs.
+       * The list of extensions used by the table includes `cells`
+       * and `selected`, which are not props of the extensions
+       * themselves, and only used to determine state of the
+       * row in the UI.
+       * We must also maintain a list of extension IDs that
+       * have been selected, to provide back to the API.
+       * The story or @syndesis module maintains the state for
+       * this list separately as `selectedExtensionIds`
+       */
+      tableRows.push({
+        cells: [extension.name, extension.description, extension.lastUpdated],
+        selected: extensionIdsSelected.includes(extension.extensionId)
+      });
+    }
+
+    return tableRows.length > 0 ? tableRows : [];
+  }, [extensionIdsSelected, extensionsAvailable]);
+
+  const rowsList = React.useMemo(getTableRows, [getTableRows]);
+
+  /**
+   * Fired when a row is selected.
+   * @param event
+   * @param isSelected
+   * @param rowIndex
+   * @param rowData
+   */
+
+  const onTableRowChange = (
+    event: any,
+    isSelected: boolean,
+    rowIndex: number,
+    rowData: any
+  ) => {
+    let newIdSelectionArray: string[] = [];
+
+    if (rowIndex === -1) {
+      /**
+       * Handle "select all" in rows
+       */
+      rows = rowsList.map(oneRow => {
+        oneRow.selected = isSelected;
+        return oneRow;
+      });
+
+      if (isSelected) {
+        newIdSelectionArray = extensionsAvailable.map(extension => extension.extensionId);
+        onSelectAll(newIdSelectionArray);
+      } else {
+        newIdSelectionArray = [];
+        /**
+         * Callback with new data to
+         * eventually be POSTed to the API
+         */
+        onSelectAll(newIdSelectionArray);
+      }
+    } else {
+      /**
+       * Handle single selection in rows
+       */
+      const { id } = rowData;
+
+      rows = [...rowsList];
+      rows[rowIndex].selected = isSelected;
+
+      /**
+       * Callback with new data to
+       * eventually be POSTed to the API
+       */
+      onSelect(id.title, isSelected);
+    }
+  };
+
+  return (
+    <>
+      <h2>{i18nTableName}</h2>
+      <p>{i18nTableDescription}</p>
+      <Table aria-label="Integration Extension Table"
+             canSelectAll={true}
+             cells={columns}
+             data-testid={'integration-extension-list-table'}
+             onSelect={onTableRowChange}
+             rows={rowsList}
+             variant={TableVariant.compact}
+      >
+        <TableHeader/>
+        <TableBody/>
+      </Table>
+    </>
+  );
+}
