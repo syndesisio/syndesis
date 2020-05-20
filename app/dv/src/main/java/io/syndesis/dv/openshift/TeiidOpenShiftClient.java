d6f595f20b542435319a5692e46be4d8ede0e8ff
shawkins <shawkins@redhat.com>
2020-05-20 16:34:41 -0400

    refactor(dv): follow-up to initial fix
    removing throws from javadocs
    preserving the three-valued logic in teiidopenshift client

--- app/dv/src/main/java/io/syndesis/dv/openshift/TeiidOpenShiftClient.java
+++ app/dv/src/main/java/io/syndesis/dv/openshift/TeiidOpenShiftClient.java
@@ -994,19 +994,18 @@ public class TeiidOpenShiftClient {
         return null;
     }
 
-    private static boolean asBoolean(DeploymentCondition cond) {
+    private static boolean statusEquals(DeploymentCondition cond, String value) {
         if (cond == null) {
             return false;
         }
-
-        return Boolean.valueOf(cond.getStatus());
+        return cond.getStatus().equals(value);
     }
 
     /**
      * We'll consider things progressing if true or unknown
      */
     private static boolean isDeploymentProgressing(DeploymentCondition progressing) {
-        return !asBoolean(progressing);
+        return !statusEquals(progressing, "False");
     }
 
     /**
@@ -1014,9 +1013,8 @@ public class TeiidOpenShiftClient {
      */
     private static boolean isDeploymentAvailable(DeploymentCondition available,
             DeploymentCondition progressing) {
-        return asBoolean(available) &&
-                (asBoolean(progressing)
-                        || progressing == null
+        return statusEquals(available, "True") &&
+                (!statusEquals(progressing, "False")
                         || available.getLastTransitionTime().compareTo(progressing.getLastTransitionTime()) > 0);
     }
 
