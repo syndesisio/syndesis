61b01cd0a466732e02d05aca26a811a001430500
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2020-05-28 12:13:51 +0200

    fix(connector/rest-swagger): update security definition
    * Adding a method to scan the specification and remove the security methods not selected by the user before flushing to file system specificationURI file
    * Included a few unit test cases
    * Refactored RestSwaggerConnectorIntegrationTest to let others reuse readSpecification method
    
    Closes ENTESB-12189

--- app/connector/rest-swagger/src/main/java/io/syndesis/connector/rest/swagger/SpecificationResourceCustomizer.java
+++ app/connector/rest-swagger/src/main/java/io/syndesis/connector/rest/swagger/SpecificationResourceCustomizer.java
@@ -20,21 +20,33 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import io.syndesis.connector.support.util.ConnectorOptions;
 import io.syndesis.integration.component.proxy.ComponentProxyComponent;
 import io.syndesis.integration.component.proxy.ComponentProxyCustomizer;
-
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class SpecificationResourceCustomizer implements ComponentProxyCustomizer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SpecificationResourceCustomizer.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Override
     public void customize(final ComponentProxyComponent component, final Map<String, Object> options) {
         consumeOption(options, "specification", specificationObject -> {
-            final String specification = (String) specificationObject;
-
             try {
+                final String authenticationType = ConnectorOptions.extractOption(options, "authenticationType");
+                final String specification = updateSecuritySpecification((String) specificationObject, authenticationType);
+
                 final File tempSpecification = File.createTempFile("rest-swagger", ".json");
                 tempSpecification.deleteOnExit();
                 final String swaggerSpecificationPath = tempSpecification.getAbsolutePath();
@@ -44,10 +56,64 @@ public final class SpecificationResourceCustomizer implements ComponentProxyCust
                 }
 
                 options.put("specificationUri", "file:" + swaggerSpecificationPath);
+            } catch (final JsonProcessingException jsonException) {
+                throw new IllegalStateException("Unable to parse openapi specification", jsonException);
             } catch (final IOException e) {
                 throw new IllegalStateException("Unable to persist the specification to filesystem", e);
             }
         });
     }
 
+    /**
+     * Scan the specification and leave only the security method picked by user, if any present.
+     * The change is required to avoid exposure of security configuration (such as apikey) through other channels not requested by user,
+     * for example, query parameters when the user only wants to provide api key through http headers.
+     *
+     * @param specification                    the swagger/openapi original specification
+     * @param securityDefinitionSelectedByUser the securityDefinition picked by the user
+     * @return the updated swagger/openapi specification or the original specification
+     */
+    private static String updateSecuritySpecification(final String specification, final String securityDefinitionSelectedByUser) throws JsonProcessingException {
+        final Optional<String> securityDefinitionName = getNameFromDefinition(securityDefinitionSelectedByUser);
+        if (securityDefinitionName.isPresent()) {
+            JsonNode rootNode = OBJECT_MAPPER.readTree(specification);
+            List<JsonNode> securities = rootNode.findValues("security");
+            if (!securities.isEmpty()) {
+                LOG.info("Updating specification to accept only {} user selected security method", securityDefinitionName.get());
+                for (JsonNode endpointSecurity : securities) {
+                    if (endpointSecurity.isArray()) {
+                        ArrayNode securityArray = (ArrayNode) endpointSecurity;
+                        for (int i = 0; i < securityArray.size(); i++) {
+                            String securityName = securityArray.get(i).fieldNames().next();
+                            if (!securityDefinitionName.get().equals(securityName)) {
+                                securityArray.remove(i);
+                            }
+                        }
+                    } else {
+                        // Security section must be an array
+                        throw new IllegalArgumentException("Swagger/OpenAPI specification requires endpoint security to be an array of elements!");
+                    }
+                }
+                return OBJECT_MAPPER.writeValueAsString(rootNode);
+            }
+        }
+
+        // no changes
+        LOG.debug("Specification was provided with no security method");
+        return specification;
+    }
+
+    /**
+     * Get the name from the security definition or an empty value
+     *
+     * @param securityDefinitionSelectedByUser with format type:name (ie, apiKey: api-key-security)
+     * @return the name of the definition or an empty value
+     */
+    private static Optional<String> getNameFromDefinition(String securityDefinitionSelectedByUser) {
+        if (securityDefinitionSelectedByUser != null && securityDefinitionSelectedByUser.indexOf(':') > 0) {
+            return Optional.of(securityDefinitionSelectedByUser.substring(securityDefinitionSelectedByUser.indexOf(':') + 1).trim());
+        } else {
+            return Optional.empty();
+        }
+    }
 }
