a571c690be907b4ddf1527cd272a3b000bd3ff7f
Christie Molloy <cmolloy@redhat.com>
2020-06-05 16:37:48 -0400

    add the loader component

--- app/ui-react/packages/ui/src/Layout/AppLayout.tsx
+++ app/ui-react/packages/ui/src/Layout/AppLayout.tsx
@@ -19,6 +19,7 @@ import * as React from 'react';
 import { HelpDropdown } from '../Shared/HelpDropdown';
 import { AppLayoutContext } from './AppLayoutContext';
 import { AppTopMenu } from './AppTopMenu';
+import { LoadingPage } from '@rh-uxd/integration-react';
 
 export interface ILayoutBase {
   avatar?: string;
@@ -84,6 +85,7 @@ export const AppLayout: React.FunctionComponent<ILayoutBase> = ({
     setCurViewportWidth(props.windowSize);
   };
   const [isTabletView, setIsTabletView] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
   const LARGE_VIEWPORT_BREAKPOINT = parseInt(
     global_breakpoint_lg.value.substring(
       0,
@@ -94,105 +96,117 @@ export const AppLayout: React.FunctionComponent<ILayoutBase> = ({
   React.useEffect(() => {
     setIsTabletView(curViewportWidth <= LARGE_VIEWPORT_BREAKPOINT);
   }, [curViewportWidth]);
+
+  const delayState = () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  };
+
+  delayState();
+
   return (
     <AppLayoutContext.Provider
       value={{
         showBreadcrumb,
       }}
     >
-      <Page
-        skipToContent={PageSkipToContent}
-        onPageResize={onPageResize}
-        header={
-          <PageHeader
-            logo={pictograph}
-            logoProps={{ onClick: logoOnClick }}
-            toolbar={
-              <Toolbar>
-                <ToolbarGroup
-                  className={css(
-                    accessibleStyles.screenReader,
-                    accessibleStyles.visibleOnLg
-                  )}
-                >
-                  <ToolbarItem>
-                    <HelpDropdown
-                      isTabletView={isTabletView}
-                      className="syn-help-dropdown"
-                      isOpen={false}
-                      launchSupportPage={onSelectSupport}
-                      launchAboutModal={onShowAboutModal}
-                      launchSampleIntegrationTutorials={
-                        onSelectSampleIntegrationTutorials
-                      }
-                      launchConnectorsGuide={onSelectConnectorsGuide}
-                      launchUserGuide={onSelectUserGuide}
-                      launchContactUs={onSelectContactUs}
-                      additionalDropdownItems={[
-                        <DropdownSeparator
-                          key="separator"
-                          className="pf-u-display-none-on-lg"
-                        />,
+      <div style={{position: 'relative'}}>
+        { isLoading && <LoadingPage appName="Syndesis"/>}
+        <Page
+          skipToContent={PageSkipToContent}
+          onPageResize={onPageResize}
+          header={
+            <PageHeader
+              logo={pictograph}
+              logoProps={{ onClick: logoOnClick }}
+              toolbar={
+                <Toolbar>
+                  <ToolbarGroup
+                    className={css(
+                      accessibleStyles.screenReader,
+                      accessibleStyles.visibleOnLg
+                    )}
+                  >
+                    <ToolbarItem>
+                      <HelpDropdown
+                        isTabletView={isTabletView}
+                        className="syn-help-dropdown"
+                        isOpen={false}
+                        launchSupportPage={onSelectSupport}
+                        launchAboutModal={onShowAboutModal}
+                        launchSampleIntegrationTutorials={
+                          onSelectSampleIntegrationTutorials
+                        }
+                        launchConnectorsGuide={onSelectConnectorsGuide}
+                        launchUserGuide={onSelectUserGuide}
+                        launchContactUs={onSelectContactUs}
+                        additionalDropdownItems={[
+                          <DropdownSeparator
+                            key="separator"
+                            className="pf-u-display-none-on-lg"
+                          />,
+                          <DropdownItem
+                            className={'pf-u-display-none-on-lg'}
+                            key={`mobile-${logoutItem.key}`}
+                            onClick={logoutItem.onClick}
+                          >
+                            <button
+                              type="button"
+                              role="menuitem"
+                              id={`mobile-${logoutItem.id}`}
+                              data-testid={`mobile-${logoutItem.id}`}
+                              className={logoutItem.className}
+                            >
+                              {logoutItem.children}
+                            </button>
+                          </DropdownItem>,
+                        ]}
+                      />
+                    </ToolbarItem>
+                    <ToolbarItem className="pf-u-display-none pf-u-display-block-on-lg">
+                      <AppTopMenu username={username}>
                         <DropdownItem
-                          className={'pf-u-display-none-on-lg'}
-                          key={`mobile-${logoutItem.key}`}
+                          key={logoutItem.key}
                           onClick={logoutItem.onClick}
                         >
                           <button
                             type="button"
                             role="menuitem"
-                            id={`mobile-${logoutItem.id}`}
-                            data-testid={`mobile-${logoutItem.id}`}
+                            id={logoutItem.id}
+                            data-testid={logoutItem.id}
                             className={logoutItem.className}
                           >
                             {logoutItem.children}
                           </button>
-                        </DropdownItem>,
-                      ]}
-                    />
-                  </ToolbarItem>
-                  <ToolbarItem className="pf-u-display-none pf-u-display-block-on-lg">
-                    <AppTopMenu username={username}>
-                      <DropdownItem
-                        key={logoutItem.key}
-                        onClick={logoutItem.onClick}
-                      >
-                        <button
-                          type="button"
-                          role="menuitem"
-                          id={logoutItem.id}
-                          data-testid={logoutItem.id}
-                          className={logoutItem.className}
-                        >
-                          {logoutItem.children}
-                        </button>
-                      </DropdownItem>
-                    </AppTopMenu>
-                  </ToolbarItem>
-                </ToolbarGroup>
-              </Toolbar>
-            }
-            avatar={avatar && <Avatar src={avatar} alt="User Avatar" />}
-            showNavToggle={true}
-            onNavToggle={isMobileView ? onNavToggleMobile : onNavToggle}
-            isNavOpen={isMobileView ? isNavOpenMobile : showNavigation}
-          />
-        }
-        sidebar={
-          <PageSidebar
-            theme="dark"
-            nav={
-              <Nav theme="dark">
-                <NavList>{verticalNav}</NavList>
-              </Nav>
-            }
-            isNavOpen={isMobileView ? isNavOpenMobile : showNavigation}
-          />
-        }
-        breadcrumb={breadcrumb}
-      >
-        {children}
-      </Page>
+                        </DropdownItem>
+                      </AppTopMenu>
+                    </ToolbarItem>
+                  </ToolbarGroup>
+                </Toolbar>
+              }
+              avatar={avatar && <Avatar src={avatar} alt="User Avatar" />}
+              showNavToggle={true}
+              onNavToggle={isMobileView ? onNavToggleMobile : onNavToggle}
+              isNavOpen={isMobileView ? isNavOpenMobile : showNavigation}
+            />
+          }
+          sidebar={
+            <PageSidebar
+              theme="dark"
+              nav={
+                <Nav theme="dark">
+                  <NavList>{verticalNav}</NavList>
+                </Nav>
+              }
+              isNavOpen={isMobileView ? isNavOpenMobile : showNavigation}
+            />
+          }
+          breadcrumb={breadcrumb}
+        >
+          {children}
+        </Page>
+      </div>
     </AppLayoutContext.Provider>
   );
 };
