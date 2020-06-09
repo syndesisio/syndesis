ae336e2119a760e2d43c68d5489d231bc4dab498
Kurt Stam <kstam@redhat.com>
2020-06-09 14:27:44 +0200

    PMD

--- app/server/dao/src/main/java/io/syndesis/server/dao/manager/DataManager.java
+++ app/server/dao/src/main/java/io/syndesis/server/dao/manager/DataManager.java
@@ -85,8 +85,9 @@ public class DataManager implements DataAccessObjectRegistry {
     @Value("${deployment.file:io/syndesis/server/dao/deployment.json}")
     @SuppressWarnings({"PMD.ImmutableField", "PMD.FinalFieldCouldBeStatic"}) // @Value cannot be applied to final properties
     private final String dataFileName = "io/syndesis/server/dao/deployment.json";
-    private static final String demoDataFileName = "io/syndesis/server/dao/demo-data.json";
+    private static final String DEMO_DATA_FILE_NAME = "io/syndesis/server/dao/demo-data.json";
     @Value("${deployment.load-demo-data:true}")
+    @SuppressWarnings("FieldCanBeFinal")
     private boolean loadDemoData = true;
 
     private final List<DataAccessObject<?>> dataAccessObjects = new ArrayList<>();
@@ -125,7 +126,7 @@ public class DataManager implements DataAccessObjectRegistry {
             loadData(this.dataFileName);
         }
         if (loadDemoData) {
-            loadData(demoDataFileName);
+            loadData(DEMO_DATA_FILE_NAME);
         }
     }
 
