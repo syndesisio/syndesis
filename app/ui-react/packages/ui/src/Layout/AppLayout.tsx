3fc5c4031262ca9045d81ea30e052aeb0b9daab3
Christie Molloy <cmolloy@redhat.com>
2020-06-19 15:43:14 -0400

    use lazy loading to add the page element

--- app/ui-react/packages/ui/src/Layout/AppLayout.tsx
+++ app/ui-react/packages/ui/src/Layout/AppLayout.tsx
@@ -1,101 +1,12 @@
-import accessibleStyles from '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
-import {
-  Avatar,
-  DropdownItem,
-  DropdownSeparator,
-  Nav,
-  NavList,
-  Page,
-  PageHeader,
-  PageSidebar,
-  SkipToContent,
-  Toolbar,
-  ToolbarGroup,
-  ToolbarItem,
-} from '@patternfly/react-core';
-import { css } from '@patternfly/react-styles';
-import { global_breakpoint_lg } from '@patternfly/react-tokens';
-import * as React from 'react';
-import { HelpDropdown } from '../Shared/HelpDropdown';
-import { AppLayoutContext } from './AppLayoutContext';
-import { AppTopMenu } from './AppTopMenu';
 import { LoadingPage } from '@rh-uxd/integration-react';
+import React, { Suspense } from 'react';
+import './AppLayout.css';
 
-export interface ILayoutBase {
-  avatar?: string;
-  pictograph: any;
-  verticalNav: any[];
-  logoOnClick: () => void;
-  logoutItem: {
-    key: string;
-    onClick: () => Promise<any>;
-    id: string;
-    className?: string;
-    children: string;
-  };
-  showNavigation: boolean;
-  username: string;
-  onNavigationCollapse(): void;
-  onNavigationExpand(): void;
-  onShowAboutModal(): void;
-  onSelectSupport(): void;
-  onSelectSampleIntegrationTutorials(): void;
-  onSelectUserGuide(): void;
-  onSelectConnectorsGuide(): void;
-  onSelectContactUs(): void;
-}
 
-export const AppLayout: React.FunctionComponent<ILayoutBase> = ({
-  avatar,
-  pictograph,
-  verticalNav,
-  logoOnClick,
-  showNavigation,
-  logoutItem,
-  onNavigationCollapse,
-  onNavigationExpand,
-  onShowAboutModal,
-  onSelectSupport,
-  onSelectConnectorsGuide,
-  onSelectContactUs,
-  onSelectSampleIntegrationTutorials,
-  onSelectUserGuide,
-  username,
-  children,
-}) => {
-  const onNavToggle = showNavigation
-    ? onNavigationCollapse
-    : onNavigationExpand;
+export const AppLayout: React.FunctionComponent = ({...props}) => {
 
-  const [breadcrumb, setHasBreadcrumb] = React.useState(null);
-  const showBreadcrumb = (b: any) => setHasBreadcrumb(b);
-  const PageSkipToContent = (
-    <SkipToContent href="#main-content-page-layout-default-nav">
-      Skip to Content
-    </SkipToContent>
-  );
-  const [isMobileView, setIsMobileView] = React.useState(false);
-  const [isNavOpenMobile, setIsNavOpenMobile] = React.useState(false);
-  const onNavToggleMobile = () => {
-    setIsNavOpenMobile(!isNavOpenMobile);
-  };
-  const [curViewportWidth, setCurViewportWidth] = React.useState(1024);
-  const onPageResize = (props: { mobileView: boolean; windowSize: number }) => {
-    setIsMobileView(props.mobileView);
-    setCurViewportWidth(props.windowSize);
-  };
-  const [isTabletView, setIsTabletView] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(true);
-  const LARGE_VIEWPORT_BREAKPOINT = parseInt(
-    global_breakpoint_lg.value.substring(
-      0,
-      global_breakpoint_lg.value.indexOf('px')
-    ),
-    10
-  );
-  React.useEffect(() => {
-    setIsTabletView(curViewportWidth <= LARGE_VIEWPORT_BREAKPOINT);
-  }, [curViewportWidth]);
+  const PageComponent = React.lazy(() => import(('./AppPage')));
 
   const delayState = () => {
     setTimeout(() => {
@@ -106,107 +17,10 @@ export const AppLayout: React.FunctionComponent<ILayoutBase> = ({
   delayState();
 
   return (
-    <AppLayoutContext.Provider
-      value={{
-        showBreadcrumb,
-      }}
-    >
-      <div style={{position: 'relative'}}>
-        { isLoading && <LoadingPage appName="Syndesis"/>}
-        <Page
-          skipToContent={PageSkipToContent}
-          onPageResize={onPageResize}
-          header={
-            <PageHeader
-              logo={pictograph}
-              logoProps={{ onClick: logoOnClick }}
-              toolbar={
-                <Toolbar>
-                  <ToolbarGroup
-                    className={css(
-                      accessibleStyles.screenReader,
-                      accessibleStyles.visibleOnLg
-                    )}
-                  >
-                    <ToolbarItem>
-                      <HelpDropdown
-                        isTabletView={isTabletView}
-                        className="syn-help-dropdown"
-                        isOpen={false}
-                        launchSupportPage={onSelectSupport}
-                        launchAboutModal={onShowAboutModal}
-                        launchSampleIntegrationTutorials={
-                          onSelectSampleIntegrationTutorials
-                        }
-                        launchConnectorsGuide={onSelectConnectorsGuide}
-                        launchUserGuide={onSelectUserGuide}
-                        launchContactUs={onSelectContactUs}
-                        additionalDropdownItems={[
-                          <DropdownSeparator
-                            key="separator"
-                            className="pf-u-display-none-on-lg"
-                          />,
-                          <DropdownItem
-                            className={'pf-u-display-none-on-lg'}
-                            key={`mobile-${logoutItem.key}`}
-                            onClick={logoutItem.onClick}
-                          >
-                            <button
-                              type="button"
-                              role="menuitem"
-                              id={`mobile-${logoutItem.id}`}
-                              data-testid={`mobile-${logoutItem.id}`}
-                              className={logoutItem.className}
-                            >
-                              {logoutItem.children}
-                            </button>
-                          </DropdownItem>,
-                        ]}
-                      />
-                    </ToolbarItem>
-                    <ToolbarItem className="pf-u-display-none pf-u-display-block-on-lg">
-                      <AppTopMenu username={username}>
-                        <DropdownItem
-                          key={logoutItem.key}
-                          onClick={logoutItem.onClick}
-                        >
-                          <button
-                            type="button"
-                            role="menuitem"
-                            id={logoutItem.id}
-                            data-testid={logoutItem.id}
-                            className={logoutItem.className}
-                          >
-                            {logoutItem.children}
-                          </button>
-                        </DropdownItem>
-                      </AppTopMenu>
-                    </ToolbarItem>
-                  </ToolbarGroup>
-                </Toolbar>
-              }
-              avatar={avatar && <Avatar src={avatar} alt="User Avatar" />}
-              showNavToggle={true}
-              onNavToggle={isMobileView ? onNavToggleMobile : onNavToggle}
-              isNavOpen={isMobileView ? isNavOpenMobile : showNavigation}
-            />
-          }
-          sidebar={
-            <PageSidebar
-              theme="dark"
-              nav={
-                <Nav theme="dark">
-                  <NavList>{verticalNav}</NavList>
-                </Nav>
-              }
-              isNavOpen={isMobileView ? isNavOpenMobile : showNavigation}
-            />
-          }
-          breadcrumb={breadcrumb}
-        >
-          {children}
-        </Page>
-      </div>
-    </AppLayoutContext.Provider>
+    <div style={{position: 'relative'}}>
+      <Suspense fallback={ isLoading && <LoadingPage appName="Syndesis"/>}>
+        <PageComponent {...props}/>
+      </Suspense>
+    </div>
   );
 };
---
+++ app/ui-react/packages/ui/src/Layout/AppPage.tsx
@@ -0,0 +1,200 @@
+import accessibleStyles from '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
+import {
+  Avatar,
+  DropdownItem,
+  DropdownSeparator,
+  Nav,
+  NavList,
+  Page,
+  PageHeader,
+  PageSidebar,
+  SkipToContent,
+  Toolbar,
+  ToolbarGroup,
+  ToolbarItem,
+} from '@patternfly/react-core';
+import { css } from '@patternfly/react-styles';
+import { global_breakpoint_lg } from '@patternfly/react-tokens';
+import React from 'react';
+import { HelpDropdown } from '../Shared/HelpDropdown';
+import { AppLayoutContext } from './AppLayoutContext';
+import { AppTopMenu } from './AppTopMenu';
+import './AppLayout.css';
+
+export interface ILayoutBase {
+  avatar?: string;
+  pictograph: any;
+  verticalNav: any[];
+  logoOnClick: () => void;
+  logoutItem: {
+    key: string;
+    onClick: () => Promise<any>;
+    id: string;
+    className?: string;
+    children: string;
+  };
+  showNavigation: boolean;
+  username: string;
+  onNavigationCollapse(): void;
+  onNavigationExpand(): void;
+  onShowAboutModal(): void;
+  onSelectSupport(): void;
+  onSelectSampleIntegrationTutorials(): void;
+  onSelectUserGuide(): void;
+  onSelectConnectorsGuide(): void;
+  onSelectContactUs(): void;
+}
+
+export const AppPage: React.FunctionComponent<ILayoutBase> = ({
+  avatar,
+  pictograph,
+  verticalNav,
+  logoOnClick,
+  showNavigation,
+  logoutItem,
+  onNavigationCollapse,
+  onNavigationExpand,
+  onShowAboutModal,
+  onSelectSupport,
+  onSelectConnectorsGuide,
+  onSelectContactUs,
+  onSelectSampleIntegrationTutorials,
+  onSelectUserGuide,
+  username,
+  children,
+}) => {
+  const onNavToggle = showNavigation
+    ? onNavigationCollapse
+    : onNavigationExpand;
+
+  const [breadcrumb, setHasBreadcrumb] = React.useState(null);
+  const showBreadcrumb = (b: any) => setHasBreadcrumb(b);
+  const PageSkipToContent = (
+    <SkipToContent href="#main-content-page-layout-default-nav">
+      Skip to Content
+    </SkipToContent>
+  );
+  const [isMobileView, setIsMobileView] = React.useState(false);
+  const [isNavOpenMobile, setIsNavOpenMobile] = React.useState(false);
+  const onNavToggleMobile = () => {
+    setIsNavOpenMobile(!isNavOpenMobile);
+  };
+  const [curViewportWidth, setCurViewportWidth] = React.useState(1024);
+  const onPageResize = (props: { mobileView: boolean; windowSize: number }) => {
+    setIsMobileView(props.mobileView);
+    setCurViewportWidth(props.windowSize);
+  };
+  const [isTabletView, setIsTabletView] = React.useState(false);
+  const LARGE_VIEWPORT_BREAKPOINT = parseInt(
+    global_breakpoint_lg.value.substring(
+      0,
+      global_breakpoint_lg.value.indexOf('px')
+    ),
+    10
+  );
+  React.useEffect(() => {
+    setIsTabletView(curViewportWidth <= LARGE_VIEWPORT_BREAKPOINT);
+  }, [curViewportWidth]);
+
+  return (
+    <AppLayoutContext.Provider
+      value={{
+        showBreadcrumb,
+      }}
+    >
+      <Page
+        skipToContent={PageSkipToContent}
+        onPageResize={onPageResize}
+        header={
+          <PageHeader
+            logo={pictograph}
+            logoProps={{ onClick: logoOnClick }}
+            toolbar={
+              <Toolbar>
+                <ToolbarGroup
+                  className={css(
+                    accessibleStyles.screenReader,
+                    accessibleStyles.visibleOnLg
+                  )}
+                >
+                  <ToolbarItem>
+                    <HelpDropdown
+                      isTabletView={isTabletView}
+                      className="syn-help-dropdown"
+                      isOpen={false}
+                      launchSupportPage={onSelectSupport}
+                      launchAboutModal={onShowAboutModal}
+                      launchSampleIntegrationTutorials={
+                        onSelectSampleIntegrationTutorials
+                      }
+                      launchConnectorsGuide={onSelectConnectorsGuide}
+                      launchUserGuide={onSelectUserGuide}
+                      launchContactUs={onSelectContactUs}
+                      additionalDropdownItems={[
+                        <DropdownSeparator
+                          key="separator"
+                          className="pf-u-display-none-on-lg"
+                        />,
+                        <DropdownItem
+                          className={'pf-u-display-none-on-lg'}
+                          key={`mobile-${logoutItem.key}`}
+                          onClick={logoutItem.onClick}
+                        >
+                          <button
+                            type="button"
+                            role="menuitem"
+                            id={`mobile-${logoutItem.id}`}
+                            data-testid={`mobile-${logoutItem.id}`}
+                            className={logoutItem.className}
+                          >
+                            {logoutItem.children}
+                          </button>
+                        </DropdownItem>,
+                      ]}
+                    />
+                  </ToolbarItem>
+                  <ToolbarItem className="pf-u-display-none pf-u-display-block-on-lg">
+                    <AppTopMenu username={username}>
+                      <DropdownItem
+                        key={logoutItem.key}
+                        onClick={logoutItem.onClick}
+                      >
+                        <button
+                          type="button"
+                          role="menuitem"
+                          id={logoutItem.id}
+                          data-testid={logoutItem.id}
+                          className={logoutItem.className}
+                        >
+                          {logoutItem.children}
+                        </button>
+                      </DropdownItem>
+                    </AppTopMenu>
+                  </ToolbarItem>
+                </ToolbarGroup>
+              </Toolbar>
+            }
+            avatar={avatar && <Avatar src={avatar} alt="User Avatar" />}
+            showNavToggle={true}
+            onNavToggle={isMobileView ? onNavToggleMobile : onNavToggle}
+            isNavOpen={isMobileView ? isNavOpenMobile : showNavigation}
+          />
+        }
+        sidebar={
+          <PageSidebar
+            theme="dark"
+            nav={
+              <Nav theme="dark">
+                <NavList>{verticalNav}</NavList>
+              </Nav>
+            }
+            isNavOpen={isMobileView ? isNavOpenMobile : showNavigation}
+          />
+        }
+        breadcrumb={breadcrumb}
+      >
+        {children}
+      </Page>
+    </AppLayoutContext.Provider>
+  );
+};
