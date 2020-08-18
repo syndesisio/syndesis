bf8aa92fc87235a58decf187474c75a6419e3e32
David Arnold <dar@xoe.solutions>
2020-08-17 20:40:01 -0500

    chore: reduce nix-shell environment to the bare minimum

--- shell.nix
+++ shell.nix
@@ -1,5 +1,13 @@
+{
+  nixpkgs ? builtins.fetchGit {
+    url = https://github.com/nixos/nixpkgs-channels;
+    ref = "nixpkgs-unstable";
+    rev = "2d6cbbe4627f6fe4a179c681537b0a3e4f59b732";
+  }
+}:
+
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import nixpkgs {config = {}; overlays = [];};
 in
 pkgs.mkShell {
   buildInputs = [
@@ -8,9 +16,8 @@ pkgs.mkShell {
     pkgs.go
     pkgs.just
     pkgs.kube3d
-    # pkgs.kubebox
     pkgs.kubectl
-    pkgs.kubespy
+    pkgs.kustomize
     pkgs.minikube
     pkgs.minishift
     pkgs.nodejs
