09de685faf418c86a77b33c43eb44bf7c357dd9d
DavHau <hsngrmpf+github@gmail.com>
2020-08-21 22:00:31 +0700

    improve: fetch nixpkgs via tarball (more efficient)

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,5 +1,5 @@
-import (builtins.fetchGit {
-  url = https://github.com/nixos/nixpkgs-channels;
-  ref = "nixpkgs-unstable";
-  rev = "16fc531784ac226fb268cc59ad573d2746c109c1";
+import (builtins.fetchTarball {
+  url = https://github.com/nixos/nixpkgs-channels/tarball/16fc531784ac226fb268cc59ad573d2746c109c1;
+  # hash obtained via `nix-prefetch-url --unpack https://github.com/nixos/nixpkgs-channels/tarball/{commit}`
+  sha256 = "0qw1jpdfih9y0dycslapzfp8bl4z7vfg9c7qz176wghwybm4sx0a";
 }) { config = {}; overlays = []; }
