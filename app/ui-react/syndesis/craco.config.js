a0e3f321f4ce5e7d297c6e278c8c48e7a8cea185
Stan Lewis <gashcrumb@gmail.com>
2020-08-24 10:35:13 -0400

    chore: clean up some monaco/dv removal leftovers for ENTESB-14421

--- app/ui-react/syndesis/craco.config.js
+++ app/ui-react/syndesis/craco.config.js
@@ -1,4 +1,3 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = function({ env, paths }) {
@@ -24,17 +23,6 @@ module.exports = function({ env, paths }) {
           ],
         },
       }),
-      alias: {
-        vscode: require.resolve(
-          'monaco-languageclient/lib/vscode-compatibility'
-        ),
-      },
-      plugins: [
-        new MonacoWebpackPlugin({
-          // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-          languages: ['sql', 'pgsql'],
-        }),
-      ],
     },
     jest: {
       configure: (jestConfig, { env, paths, resolve, rootDir }) => ({
