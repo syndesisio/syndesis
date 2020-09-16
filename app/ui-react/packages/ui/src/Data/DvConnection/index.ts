6ede6de972df52d88a02d2c3359fa0aab226d8d2
Stan Lewis <gashcrumb@gmail.com>
2020-09-16 08:49:33 -0400

    chore: pull out some DV leftovers

--- app/ui-react/packages/ui/src/Data/DvConnection/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './DvConnectionCard';
-export * from './DvConnectionsGrid';
-export * from './DvConnectionsGridCell';
-export * from './DvConnectionSkeleton';
-export * from './DvConnectionStatus';
-export * from './DvConnectionsListView';
-export * from './DvConnectionsToolbarSkeleton';
