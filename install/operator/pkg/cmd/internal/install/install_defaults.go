0cfb97854ec12b815a898e1b50b7dfab75d8a16d
Claudio Miranda <claudio@claudius.com.br>
2021-03-25 12:52:18 -0300

    ENTESB-15883 Update Postgresql to productized 12

--- install/operator/pkg/cmd/internal/install/install_defaults.go
+++ install/operator/pkg/cmd/internal/install/install_defaults.go
@@ -1,5 +1,5 @@
 package install
 
-const defaultDatabaseImage = "registry.redhat.io/rhscl/postgresql-10-rhel7:1-59"
+const defaultDatabaseImage = "registry.redhat.io/rhscl/postgresql-12-rhel7"
 const productizedBuild = true
 const productVersion = "7.9"
