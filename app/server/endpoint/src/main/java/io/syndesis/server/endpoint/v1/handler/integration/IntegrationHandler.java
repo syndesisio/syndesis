9d2237dec145c692e5789c21dcfce4158ba87411
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2021-04-07 14:03:34 +0200

    Revert "feat(server): custom labels validation"
    This reverts commit 0f93784aad43623edd60a158d21305e81060bcb3.

--- app/server/endpoint/src/main/java/io/syndesis/server/endpoint/v1/handler/integration/IntegrationHandler.java
+++ app/server/endpoint/src/main/java/io/syndesis/server/endpoint/v1/handler/integration/IntegrationHandler.java
@@ -30,7 +30,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 
-import io.syndesis.common.util.Labels;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -99,17 +98,8 @@ public class IntegrationHandler extends BaseHandler implements Lister<Integratio
         final Integration updatedIntegration = new Integration.Builder().createFrom(encryptedIntegration)
             .createdAt(System.currentTimeMillis()).build();
 
-        validateLabels(updatedIntegration);
-
         // Create the the integration.
         return getDataManager().create(updatedIntegration);
-    }
-
-    private static void validateLabels(Integration integration){
-        integration.getLabels().forEach((k, v) -> {
-            Labels.validateKey(k);
-            Labels.validate(v);
-        });
     }
 
     @Override
