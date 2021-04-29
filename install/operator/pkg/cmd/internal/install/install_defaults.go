0a83474bf784299c16f3dc832a5432e3d4160db6
Claudio Miranda <claudio@claudius.com.br>
2021-04-29 15:33:20 -0300

    ENTESB-16261 Use rhel8 based images for fuse-online 7.9

--- install/operator/pkg/cmd/internal/install/install_defaults.go
+++ install/operator/pkg/cmd/internal/install/install_defaults.go
@@ -1,5 +1,5 @@
 package install
 
-const defaultDatabaseImage = "registry.redhat.io/rhscl/postgresql-12-rhel7"
+const defaultDatabaseImage = "registry.redhat.io/rhel8/postgresql-12:1"
 const productizedBuild = true
 const productVersion = "7.9"
