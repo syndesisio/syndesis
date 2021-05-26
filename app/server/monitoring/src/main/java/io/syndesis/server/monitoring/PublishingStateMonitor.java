85e1472fbfee2a52ffd5d0c37ae7d3987a896499
Maria Arias de Reyna <ariasdereyna@redhat.com>
2021-05-26 13:59:20 +0200

    codesmell: Reducing Cognitive Complexity To make it more readable

--- app/server/monitoring/src/main/java/io/syndesis/server/monitoring/PublishingStateMonitor.java
+++ app/server/monitoring/src/main/java/io/syndesis/server/monitoring/PublishingStateMonitor.java
@@ -73,97 +73,125 @@ public class PublishingStateMonitor implements StateHandler {
     }
 
     @Override
-    @SuppressWarnings("PMD.NPathComplexity")
     public void accept(IntegrationDeployment integrationDeployment) {
+
+        final IntegrationDeploymentState targetState = integrationDeployment.getTargetState();
+
+        // We are only interested if the state is supposed to be published
+        // Stopped integrations are not "accepted" here
+        // because this is the PublishingStateMonitor.
+        if (!targetState.equals(Published)) {
+            return;
+        }
 
         final String integrationId = integrationDeployment.getIntegrationId().get();
         final String version = String.valueOf(integrationDeployment.getVersion());
         final String compositeId = IntegrationDeployment.compositeId(integrationId, integrationDeployment.getVersion());
-        final IntegrationDeploymentState targetState = integrationDeployment.getTargetState();
 
-        // is it being published?
-        if (targetState.equals(Published)) {
+        String podName = null;
+        final PodList podList = getDeploymentPodList(integrationId, version);
 
-            IntegrationDeploymentDetailedState detailedState = null;
-            String podName = null;
-            LinkType linkType = null;
+        // work backwards, in reverse order: deployed pod, deployer pod, build pod, default to assembling
 
-            // work backwards, in reverse order: deployed pod, deployer pod, build pod, default to assembling
-            // 1. look for deployed pod
-            final PodList podList = getDeploymentPodList(integrationId, version);
-            if (!podList.getItems().isEmpty()) {
+        // 1. is it already deployed?
+        if (!podList.getItems().isEmpty()) {
+            lookForIntegrationPod(integrationId, version, compositeId, podList);
+        } else {
+            //Not deployed yet, must be on some building phase
 
-                // TODO: handle pod scaling in the future
-                final Pod pod = podList.getItems().get(0);
-                podName = pod.getMetadata().getName();
-                // check if deployment is ready
-                if (Readiness.isPodReady(pod)) {
-                    // no details needed once deployed successfully!!!
-                    // NOTE that this won't happen always, the state polling window may miss this
-                    // TODO need a cleanup handler to remove successfully published state details??
-                    deleteStateDetails(compositeId);
-                } else {
-                    linkType = getPodUrls(pod);
-                    // pending deployment pod
-                    detailedState = EVENTS == linkType ? IntegrationDeploymentDetailedState.DEPLOYING : IntegrationDeploymentDetailedState.STARTING;
-                }
+            // 2. look for deployer pod
+            podName = lookForDeployerPod(integrationId, version, compositeId);
 
-            } else {
-                // 2. look for deployer pod
-                final Optional<ReplicationController> replicationController = getReplicationController(integrationId,
-                        version);
-                if (replicationController.isPresent()) {
-                    podName = replicationController.get().getMetadata().getAnnotations().get(DEPLOYER_POD_NAME_ANNOTATION);
-                    if (podName != null) {
-                        final Pod deployerPod = getPod(podName);
-                        if (deployerPod != null) {
-                            // with a deployer pod, detailed state is always deploying
-                            linkType = getPodUrls(deployerPod);
-                            detailedState = IntegrationDeploymentDetailedState.DEPLOYING;
-                        } else {
-                            // clear podName, since it doesn't exist yet
-                            podName = null;
-                        }
-                    }
-                }
-
-                // 3. look for build pod, if deployer pod not found
-                if (podName == null) {
-                    final Optional<Build> build = getBuild(integrationId, version);
-                    if (build.isPresent()) {
-
-                        podName = build.get().getMetadata().getAnnotations().get(BUILD_POD_NAME_ANNOTATION);
-                        if (podName != null) {
-                            final Pod pod = getPod(podName);
-                            if (pod != null) {
-                                linkType = getPodUrls(pod);
-                                // pending deployment pod
-                                detailedState = EVENTS == linkType ? IntegrationDeploymentDetailedState.ASSEMBLING : IntegrationDeploymentDetailedState.BUILDING;
-                            } else {
-                                // clear podName, since it doesn't exist yet
-                                podName = null;
-                            }
-                        }
-                    }
-                }
-
-                if (detailedState == null) {
-                    // 4. default initial state, with no event or log urls!!!
-                    detailedState = IntegrationDeploymentDetailedState.ASSEMBLING;
-                }
+            // 3. No deployer pod found, look for build pod
+            if (podName == null) {
+                podName = lookForBuildPod(integrationId, version, compositeId);
             }
 
-            if (detailedState != null) {
-                IntegrationDeploymentStateDetails stateDetails = getStateDetails(integrationId, version, compositeId,
-                        detailedState, podName, linkType);
-                if (dataManager.fetch(IntegrationDeploymentStateDetails.class, compositeId) != null) {
-                    dataManager.update(stateDetails);
-                } else {
-                    dataManager.create(stateDetails);
-                }
+            // 4. Nothing found, it must be on a very initial phase
+            if (podName == null) {
+                    updateIntegrationState(integrationId, version, compositeId,
+                            IntegrationDeploymentDetailedState.ASSEMBLING, null, null);
             }
         }
 
+    }
+
+    private String lookForIntegrationPod(String integrationId, String version, String compositeId, PodList podList) {
+        // TODO: handle pod scaling in the future
+        final Pod pod = podList.getItems().get(0);
+        String podName = pod.getMetadata().getName();
+        // check if deployment is ready
+        if (Readiness.isPodReady(pod)) {
+            // no details needed once deployed successfully!!!
+            // NOTE that this won't happen always, the state polling window may miss this
+            // TODO need a cleanup handler to remove successfully published state details??
+            deleteStateDetails(compositeId);
+        } else {
+            LinkType linkType = getPodUrls(pod);
+            // pending deployment pod
+            IntegrationDeploymentDetailedState detailedState = EVENTS == linkType ? IntegrationDeploymentDetailedState.DEPLOYING : IntegrationDeploymentDetailedState.STARTING;
+            updateIntegrationState(integrationId, version, compositeId, detailedState, podName, linkType);
+        }
+        return podName;
+    }
+
+    private String lookForDeployerPod(String integrationId, String version, String compositeId) {
+        String podName = null;
+        final Optional<ReplicationController> replicationController = getReplicationController(integrationId,
+            version);
+        if (replicationController.isPresent()) {
+            podName = replicationController.get().getMetadata().getAnnotations().get(DEPLOYER_POD_NAME_ANNOTATION);
+            if (podName != null) {
+                final Pod deployerPod = getPod(podName);
+                if (deployerPod != null) {
+                    // with a deployer pod, detailed state is always deploying
+                    LinkType linkType = getPodUrls(deployerPod);
+                    IntegrationDeploymentDetailedState detailedState = IntegrationDeploymentDetailedState.DEPLOYING;
+                    updateIntegrationState(integrationId, version, compositeId, detailedState, podName, linkType);
+                } else {
+                    // clear podName, since it doesn't exist yet
+                    podName = null;
+                }
+            }
+        }
+        return podName;
+    }
+
+    private String lookForBuildPod(String integrationId, String version, String compositeId) {
+        final Optional<Build> build = getBuild(integrationId, version);
+        String podName = null;
+        if (build.isPresent()) {
+
+            podName = build.get().getMetadata().getAnnotations().get(BUILD_POD_NAME_ANNOTATION);
+            if (podName != null) {
+                final Pod pod = getPod(podName);
+                if (pod != null) {
+                    LinkType linkType = getPodUrls(pod);
+                    // pending deployment pod
+                    IntegrationDeploymentDetailedState detailedState = EVENTS == linkType ? IntegrationDeploymentDetailedState.ASSEMBLING : IntegrationDeploymentDetailedState.BUILDING;
+                    updateIntegrationState(integrationId, version, compositeId, detailedState, podName, linkType);
+                } else {
+                    // clear podName, since it doesn't exist yet
+                    podName = null;
+                }
+            }
+
+        }
+        return podName;
+    }
+
+    private void updateIntegrationState(String integrationId, String version, String compositeId,
+                                            IntegrationDeploymentDetailedState detailedState, String podName,
+                                            LinkType linkType) {
+        if (detailedState != null) {
+            IntegrationDeploymentStateDetails stateDetails = getStateDetails(integrationId, version, compositeId,
+                detailedState, podName, linkType);
+            if (dataManager.fetch(IntegrationDeploymentStateDetails.class, compositeId) != null) {
+                dataManager.update(stateDetails);
+            } else {
+                dataManager.create(stateDetails);
+            }
+        }
     }
 
     private IntegrationDeploymentStateDetails getStateDetails(String integrationId, String version, String
