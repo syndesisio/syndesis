5fc4f9ada0506900d3a8fd8b43172e0c8a582ebe
kahboom <rachelyordan@gmail.com>
2021-07-02 15:02:19 +0100

    add required headingLevel prop for IntegrationBoard

--- app/ui-react/packages/ui/src/Dashboard/IntegrationBoard.tsx
+++ app/ui-react/packages/ui/src/Dashboard/IntegrationBoard.tsx
@@ -20,84 +20,87 @@ export interface IIntegrationBoardProps {
   i18nTitle: string;
 }
 
-export const IntegrationBoard: React.FunctionComponent<IIntegrationBoardProps> = ({
-  pendingIntegrations,
-  runningIntegrations,
-  stoppedIntegrations,
-  i18nIntegrationStatePending,
-  i18nIntegrationStateRunning,
-  i18nIntegrationStateStopped,
-  i18nIntegrations,
-  i18nTitle,
-}) => {
-  const data = [
-    {
-      x: i18nIntegrationStateRunning,
-      y: runningIntegrations,
-    },
-    {
-      x: i18nIntegrationStateStopped,
-      y: stoppedIntegrations,
-    },
-    {
-      x: i18nIntegrationStatePending,
-      y: pendingIntegrations,
-    },
-  ];
-  const legendData = [
-    {
-      name: `${i18nIntegrationStateRunning}: ${runningIntegrations}`,
-    },
-    {
-      name: `${i18nIntegrationStateStopped}: ${stoppedIntegrations}`,
-    },
-    {
-      name: `${i18nIntegrationStatePending}: ${pendingIntegrations}`,
-    },
-  ];
-  const total = `${runningIntegrations +
-    stoppedIntegrations +
-    pendingIntegrations}`;
+export const IntegrationBoard: React.FunctionComponent<IIntegrationBoardProps> =
+  ({
+    pendingIntegrations,
+    runningIntegrations,
+    stoppedIntegrations,
+    i18nIntegrationStatePending,
+    i18nIntegrationStateRunning,
+    i18nIntegrationStateStopped,
+    i18nIntegrations,
+    i18nTitle,
+  }) => {
+    const data = [
+      {
+        x: i18nIntegrationStateRunning,
+        y: runningIntegrations,
+      },
+      {
+        x: i18nIntegrationStateStopped,
+        y: stoppedIntegrations,
+      },
+      {
+        x: i18nIntegrationStatePending,
+        y: pendingIntegrations,
+      },
+    ];
+    const legendData = [
+      {
+        name: `${i18nIntegrationStateRunning}: ${runningIntegrations}`,
+      },
+      {
+        name: `${i18nIntegrationStateStopped}: ${stoppedIntegrations}`,
+      },
+      {
+        name: `${i18nIntegrationStatePending}: ${pendingIntegrations}`,
+      },
+    ];
+    const total = `${
+      runningIntegrations + stoppedIntegrations + pendingIntegrations
+    }`;
 
-  return (
-    <Card data-testid={'dashboard-integration-board'}>
-      <CardHeader>
-        <Title size={'md'}>{i18nTitle}</Title>
-      </CardHeader>
-      <CardBody>
-        <Grid>
-          <GridItem span={6} offset={3}>
-            <div
-              style={{
-                // these values are tied to how the chart figures out it's
-                // sizing, best to leave them here
-                height: '150px',
-                width: '275px',
-              }}
-            >
-              <ChartDonut
-                data-test-id="integration-board"
-                constrainToVisibleArea={true}
-                data={data}
-                subTitle={i18nIntegrations}
-                title={total}
-                labels={({ datum }) => `${datum.x}: ${datum.y}`}
-                legendData={legendData}
-                legendOrientation="vertical"
-                legendPosition="right"
-                padding={{
-                  bottom: 20,
-                  left: 20,
-                  right: 145, // Adjusted to accommodate legend
-                  top: 20,
+    return (
+      <Card data-testid={'dashboard-integration-board'}>
+        <CardHeader>
+          <Title size={'md'} headingLevel={'h3'}>
+            {i18nTitle}
+          </Title>
+        </CardHeader>
+        <CardBody>
+          <Grid>
+            <GridItem span={6} offset={3}>
+              <div
+                style={{
+                  // these values are tied to how the chart figures out it's
+                  // sizing, best to leave them here
+                  height: '150px',
+                  width: '275px',
                 }}
-                height={150}
-                width={275}
-              />
-            </div>
-          </GridItem>
-        </Grid>
-      </CardBody>
-    </Card>
-  );
-};
+              >
+                <ChartDonut
+                  data-test-id="integration-board"
+                  constrainToVisibleArea={true}
+                  data={data}
+                  subTitle={i18nIntegrations}
+                  title={total}
+                  labels={({ datum }) => `${datum.x}: ${datum.y}`}
+                  legendData={legendData}
+                  legendOrientation="vertical"
+                  legendPosition="right"
+                  padding={{
+                    bottom: 20,
+                    left: 20,
+                    right: 145, // Adjusted to accommodate legend
+                    top: 20,
+                  }}
+                  height={150}
+                  width={275}
+                />
+              </div>
+            </GridItem>
+          </Grid>
+        </CardBody>
+      </Card>
+    );
+  };
