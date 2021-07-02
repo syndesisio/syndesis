9a7fddc4f891e8ba237497afb4b09389d126a1a7
kahboom <rachelyordan@gmail.com>
2021-07-02 15:25:09 +0100

    add required headingLevel prop for ConnectorAuthorization

--- app/ui-react/packages/ui/src/Connection/ConnectorAuthorization.tsx
+++ app/ui-react/packages/ui/src/Connection/ConnectorAuthorization.tsx
@@ -1,4 +1,10 @@
-import { Card, CardBody, CardFooter, CardHeader, Title } from '@patternfly/react-core';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Title,
+} from '@patternfly/react-core';
 import * as React from 'react';
 import { ButtonLink, Loader } from '../Layout';
 
@@ -10,34 +16,33 @@ export interface IConnectorAuthorizationProps {
   isConnecting: boolean;
 }
 
-export const ConnectorAuthorization: React.FunctionComponent<
-  IConnectorAuthorizationProps
-> = ({
-  i18nTitle,
-  i18nDescription,
-  i18nConnectButton,
-  isConnecting,
-  onConnect,
-}) => (
-  <Card
-    style={{
-      margin: 'auto',
-      maxWidth: 600,
-    }}
-  >
-    <CardHeader>
-      <Title size="lg" className="metrics-uptime__header">
-        <div>{i18nTitle}</div>
-      </Title>
-    </CardHeader>
-    <CardBody>
-      <p>{i18nDescription}</p>
-    </CardBody>
-    <CardFooter>
-      <ButtonLink onClick={onConnect} disabled={isConnecting} as={'primary'}>
-        {i18nConnectButton}{' '}
-        {isConnecting && <Loader size={'xs'} inline={true} />}
-      </ButtonLink>
-    </CardFooter>
-  </Card>
-);
+export const ConnectorAuthorization: React.FunctionComponent<IConnectorAuthorizationProps> =
+  ({
+    i18nTitle,
+    i18nDescription,
+    i18nConnectButton,
+    isConnecting,
+    onConnect,
+  }) => (
+    <Card
+      style={{
+        margin: 'auto',
+        maxWidth: 600,
+      }}
+    >
+      <CardHeader>
+        <Title size="lg" className="metrics-uptime__header" headingLevel={'h3'}>
+          <div>{i18nTitle}</div>
+        </Title>
+      </CardHeader>
+      <CardBody>
+        <p>{i18nDescription}</p>
+      </CardBody>
+      <CardFooter>
+        <ButtonLink onClick={onConnect} disabled={isConnecting} as={'primary'}>
+          {i18nConnectButton}{' '}
+          {isConnecting && <Loader size={'xs'} inline={true} />}
+        </ButtonLink>
+      </CardFooter>
+    </Card>
+  );
