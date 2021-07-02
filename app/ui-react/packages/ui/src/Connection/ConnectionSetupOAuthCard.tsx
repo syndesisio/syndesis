42b760838b7ed046ad7cc48725b99a06617fe8f9
kahboom <rachelyordan@gmail.com>
2021-07-02 15:55:23 +0100

    add headingLevel to Title for ConnectionSetupOAuthCard

--- app/ui-react/packages/ui/src/Connection/ConnectionSetupOAuthCard.tsx
+++ app/ui-react/packages/ui/src/Connection/ConnectionSetupOAuthCard.tsx
@@ -1,4 +1,10 @@
-import { Card, CardBody, CardFooter, CardHeader, Title } from '@patternfly/react-core';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Title,
+} from '@patternfly/react-core';
 import * as H from '@syndesis/history';
 import * as React from 'react';
 import { ButtonLink } from '../Layout';
@@ -11,45 +17,44 @@ export interface IConnectionSetupOAuthProps {
   oauthSettingsHref: string;
 }
 
-export const ConnectionSetupOAuthCard: React.FunctionComponent<
-  IConnectionSetupOAuthProps
-> = ({
-  i18nTitle,
-  i18nDescription,
-  i18nOAuthSettingsButton,
-  backHref,
-  oauthSettingsHref,
-}) => (
-  <Card
-    style={{
-      margin: 'auto',
-      maxWidth: 600,
-    }}
-  >
-    <CardHeader>
-      <Title size="lg">
-        <div>{i18nTitle}</div>
-      </Title>
-    </CardHeader>
-    <CardBody>
-      <p>{i18nDescription}</p>
-    </CardBody>
-    <CardFooter>
-      <ButtonLink
-        data-testid={'connection-creator-back-button'}
-        href={backHref}
-        className={'wizard-pf-back'}
-      >
-        <i className="fa fa-angle-left" /> Back
-      </ButtonLink>
-      &nbsp;
-      <ButtonLink
-        data-testid={'connection-creator-settings'}
-        href={oauthSettingsHref}
-        as={'primary'}
-      >
-        {i18nOAuthSettingsButton}
-      </ButtonLink>
-    </CardFooter>
-  </Card>
-);
+export const ConnectionSetupOAuthCard: React.FunctionComponent<IConnectionSetupOAuthProps> =
+  ({
+    i18nTitle,
+    i18nDescription,
+    i18nOAuthSettingsButton,
+    backHref,
+    oauthSettingsHref,
+  }) => (
+    <Card
+      style={{
+        margin: 'auto',
+        maxWidth: 600,
+      }}
+    >
+      <CardHeader>
+        <Title size="lg" headingLevel={'h3'}>
+          <div>{i18nTitle}</div>
+        </Title>
+      </CardHeader>
+      <CardBody>
+        <p>{i18nDescription}</p>
+      </CardBody>
+      <CardFooter>
+        <ButtonLink
+          data-testid={'connection-creator-back-button'}
+          href={backHref}
+          className={'wizard-pf-back'}
+        >
+          <i className="fa fa-angle-left" /> Back
+        </ButtonLink>
+        &nbsp;
+        <ButtonLink
+          data-testid={'connection-creator-settings'}
+          href={oauthSettingsHref}
+          as={'primary'}
+        >
+          {i18nOAuthSettingsButton}
+        </ButtonLink>
+      </CardFooter>
+    </Card>
+  );
