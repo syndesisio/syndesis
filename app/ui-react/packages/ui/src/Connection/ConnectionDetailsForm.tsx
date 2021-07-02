384782b545331ef875cea3786adce7f0c73caa8f
kahboom <rachelyordan@gmail.com>
2021-07-02 14:56:20 +0100

    update headingLevel for ConnectionDetailsForm

--- app/ui-react/packages/ui/src/Connection/ConnectionDetailsForm.tsx
+++ app/ui-react/packages/ui/src/Connection/ConnectionDetailsForm.tsx
@@ -91,95 +91,98 @@ export interface IConnectionDetailsFormProps {
   onStartEditing: () => void;
 }
 
-export const ConnectionDetailsForm: React.FunctionComponent<IConnectionDetailsFormProps> = ({
-  children,
-  i18nCancelLabel,
-  i18nEditLabel,
-  i18nSaveLabel,
-  i18nTitle,
-  i18nValidateLabel,
-  isValid,
-  isWorking,
-  hasProperties,
-  validationResults,
-  handleSubmit,
-  onValidate,
-  isEditing,
-  onCancelEditing,
-  onStartEditing,
-}) => (
-  <PageSection>
-    <Container>
-      <div className="row row-cards-pf">
-        <Card>
-          <CardHeader>
-            <Title size="2xl">{i18nTitle}</Title>
-          </CardHeader>
-          <CardBody>
-            <Form
-              isHorizontal={true}
-              data-testid={'connection-details-form'}
-              onSubmit={handleSubmit}
-            >
-              {validationResults.map((e, idx) => (
-                <Alert
-                  title={e.message}
-                  key={idx}
-                  type={e.type === ERROR ? WARNING : e.type}
-                />
-              ))}
-              {children}
-              <div>
-                {isEditing ? (
-                  <Button
-                    data-testid={'connection-details-form-validate-button'}
-                    variant="secondary"
-                    disabled={isWorking || !isValid}
-                    onClick={onValidate}
-                  >
-                    {isWorking ? <Loader size={'sm'} inline={true} /> : null}
-                    {i18nValidateLabel}
-                  </Button>
-                ) : (
-                  <>
-                    {hasProperties && (
-                      <Button
-                        data-testid={'connection-details-form-edit-button'}
-                        variant="primary"
-                        onClick={onStartEditing}
-                      >
-                        {i18nEditLabel}
-                      </Button>
-                    )}
-                  </>
-                )}
-              </div>
-            </Form>
-          </CardBody>
-          {isEditing && (
-            <CardFooter>
-              <Button
-                data-testid={'connection-details-form-cancel-button'}
-                variant="secondary"
-                className="connection-details-form__editButton"
-                disabled={isWorking}
-                onClick={onCancelEditing}
+export const ConnectionDetailsForm: React.FunctionComponent<IConnectionDetailsFormProps> =
+  ({
+    children,
+    i18nCancelLabel,
+    i18nEditLabel,
+    i18nSaveLabel,
+    i18nTitle,
+    i18nValidateLabel,
+    isValid,
+    isWorking,
+    hasProperties,
+    validationResults,
+    handleSubmit,
+    onValidate,
+    isEditing,
+    onCancelEditing,
+    onStartEditing,
+  }) => (
+    <PageSection>
+      <Container>
+        <div className="row row-cards-pf">
+          <Card>
+            <CardHeader>
+              <Title size="2xl" headingLevel={'h3'}>
+                {i18nTitle}
+              </Title>
+            </CardHeader>
+            <CardBody>
+              <Form
+                isHorizontal={true}
+                data-testid={'connection-details-form'}
+                onSubmit={handleSubmit}
               >
-                {i18nCancelLabel}
-              </Button>
-              <Button
-                data-testid={'connection-details-form-save-button'}
-                variant="primary"
-                className="connection-details-form__editButton"
-                disabled={isWorking || !isValid}
-                onClick={handleSubmit}
-              >
-                {i18nSaveLabel}
-              </Button>
-            </CardFooter>
-          )}
-        </Card>
-      </div>
-    </Container>
-  </PageSection>
-);
+                {validationResults.map((e, idx) => (
+                  <Alert
+                    title={e.message}
+                    key={idx}
+                    type={e.type === ERROR ? WARNING : e.type}
+                  />
+                ))}
+                {children}
+                <div>
+                  {isEditing ? (
+                    <Button
+                      data-testid={'connection-details-form-validate-button'}
+                      variant="secondary"
+                      disabled={isWorking || !isValid}
+                      onClick={onValidate}
+                    >
+                      {isWorking ? <Loader size={'sm'} inline={true} /> : null}
+                      {i18nValidateLabel}
+                    </Button>
+                  ) : (
+                    <>
+                      {hasProperties && (
+                        <Button
+                          data-testid={'connection-details-form-edit-button'}
+                          variant="primary"
+                          onClick={onStartEditing}
+                        >
+                          {i18nEditLabel}
+                        </Button>
+                      )}
+                    </>
+                  )}
+                </div>
+              </Form>
+            </CardBody>
+            {isEditing && (
+              <CardFooter>
+                <Button
+                  data-testid={'connection-details-form-cancel-button'}
+                  variant="secondary"
+                  className="connection-details-form__editButton"
+                  disabled={isWorking}
+                  onClick={onCancelEditing}
+                >
+                  {i18nCancelLabel}
+                </Button>
+                <Button
+                  data-testid={'connection-details-form-save-button'}
+                  variant="primary"
+                  className="connection-details-form__editButton"
+                  disabled={isWorking || !isValid}
+                  onClick={handleSubmit}
+                >
+                  {i18nSaveLabel}
+                </Button>
+              </CardFooter>
+            )}
+          </Card>
+        </div>
+      </Container>
+    </PageSection>
+  );
