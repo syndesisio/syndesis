e4815b4b921e8e589a3b205a07fb853104bf796f
kahboom <rachelyordan@gmail.com>
2021-07-05 16:18:40 +0100

    adj cell width in IntegrationDetailActivityItem

--- app/ui-react/packages/ui/src/Integration/Activity/IntegrationDetailActivityItem.tsx
+++ app/ui-react/packages/ui/src/Integration/Activity/IntegrationDetailActivityItem.tsx
@@ -43,10 +43,8 @@ export interface IIntegrationDetailActivityItemProps {
   version?: string;
 }
 
-export const IntegrationDetailActivityItem: React.FC<
-  IIntegrationDetailActivityItemProps
-> = (
-  {
+export const IntegrationDetailActivityItem: React.FC<IIntegrationDetailActivityItemProps> =
+  ({
     date,
     errorCount,
     i18nError,
@@ -54,7 +52,7 @@ export const IntegrationDetailActivityItem: React.FC<
     i18nHeaderStep,
     i18nHeaderTime,
     i18nHeaderDuration,
-    i18nHeaderStatus ,
+    i18nHeaderStatus,
     i18nHeaderOutput,
     i18nNoErrors,
     i18nNoOutput,
@@ -63,137 +61,181 @@ export const IntegrationDetailActivityItem: React.FC<
     i18nVersion,
     steps,
     time,
-    version
-  } ) => {
-  const [rowExpanded, setRowExpanded] = useState(false);
+    version,
+  }) => {
+    const [rowExpanded, setRowExpanded] = useState(false);
 
-  const doExpand = () => {
-    setRowExpanded(!rowExpanded);
-  };
+    const doExpand = () => {
+      setRowExpanded(!rowExpanded);
+    };
 
-  const rows = () => {
-    const newRows = steps.map((step: any) => {
-      return [{
-        cells: [
-          step.name,
-          step.time,
-          step.duration,
+    const rows = () => {
+      const newRows = steps.map((step: any) => {
+        return [
           {
-            title: (
-              step.status === 'Success' ? (
-                <>
-                  <OkIcon size={'sm'} color={global_success_color_100.value} />{'  '}{i18nSuccess}
-                </>
-              ) : (
-                <>
-                  <ErrorCircleOIcon size={'sm'} color={global_danger_color_100.value} />{'  '}{i18nError}
-                </>
-              )
-            )
+            cells: [
+              step.name,
+              step.time,
+              step.duration,
+              {
+                title:
+                  step.status === 'Success' ? (
+                    <>
+                      <OkIcon
+                        size={'sm'}
+                        color={global_success_color_100.value}
+                      />
+                      {'  '}
+                      {i18nSuccess}
+                    </>
+                  ) : (
+                    <>
+                      <ErrorCircleOIcon
+                        size={'sm'}
+                        color={global_danger_color_100.value}
+                      />
+                      {'  '}
+                      {i18nError}
+                    </>
+                  ),
+              },
+              {
+                title: (
+                  <pre
+                    className={
+                      'integration-detail-activity-item__output-step-data'
+                    }
+                  >
+                    {step.output || i18nNoOutput}
+                  </pre>
+                ),
+              },
+            ],
           },
-          {
-            title: (
-              <pre className={'integration-detail-activity-item__output-step-data'}>
-                {step.output || i18nNoOutput}
-              </pre>
-            )
-          }
+        ];
+      });
+
+      return newRows.reduce((a: any, b: any) => a.concat(b), []);
+    };
+
+    const columns = [
+      {
+        columnTransforms: [cellWidth(10)],
+        title: i18nHeaderStep,
+      },
+      {
+        columnTransforms: [cellWidth(15)],
+        title: i18nHeaderTime,
+      },
+      {
+        columnTransforms: [cellWidth(10)],
+        title: i18nHeaderDuration,
+      },
+      {
+        columnTransforms: [cellWidth(10)],
+        title: i18nHeaderStatus,
+      },
+      {
+        columnTransforms: [
+          () => ({
+            className: 'integration-detail-activity-item__output-step-data',
+          }),
+          cellWidth(100),
         ],
-      }];
-    });
+        title: i18nHeaderOutput,
+      },
+    ];
 
-    return newRows.reduce((a: any, b: any) => a.concat(b), []);
-  };
-
-  const columns = [
-    {
-      columnTransforms: [cellWidth(10)],
-      title: i18nHeaderStep,
-    },
-    {
-      columnTransforms: [cellWidth(15)],
-      title: i18nHeaderTime,
-    },
-    {
-      columnTransforms: [cellWidth(7)],
-      title: i18nHeaderDuration,
-    },
-    {
-      columnTransforms: [cellWidth(10)],
-      title: i18nHeaderStatus,
-    },
-    {
-      columnTransforms: [() => ({ className: 'integration-detail-activity-item__output-step-data'}), cellWidth('max') ],
-      title: i18nHeaderOutput,
-    }
-  ];
-
-  return (
-    <DataListItem
-      aria-labelledby="activity item"
-      isExpanded={rowExpanded}
-      className={'integration-detail-activity-item'}
-      data-testid={'integration-detail-activity-item'}
-    >
-      <DataListItemRow>
-        <DataListToggle
-          onClick={doExpand}
-          isExpanded={rowExpanded}
-          id="activity-item-toggle"
-          data-testid={'integration-detail-activity-item-toggle'}
-        />
-        <DataListItemCells
-          dataListCells={[
-            <DataListCell id="activity-date" data-testid={'integration-detail-activity-item-date'} key="date">
-              {date}
-            </DataListCell>,
-            <DataListCell id="activity-time" data-testid={'integration-detail-activity-item-time'} key="time">
-              {time}
-            </DataListCell>,
-            <DataListCell id="activity-version" data-testid={'integration-detail-activity-item-version'} key="version">
-              {i18nVersion}
-              &nbsp;
-              {version}
-            </DataListCell>,
-            <DataListCell id="activity-errors" data-testid={'integration-detail-activity-item-errors'} key="errors">
-              <div className={'integration-detail-activity-item__status-item'}>
-                {errorCount > 0 ? (
-                  <>
-                    <ErrorCircleOIcon size={'sm'} color={global_danger_color_100.value} />
-                    {'  '}
-                    {i18nErrorsFound}
-                  </>
-                ) : (
-                  <>
-                    <OkIcon size={'sm'} color={global_success_color_100.value} />
-                    {'  '}
-                    {i18nNoErrors}
-                  </>
-                )}
-              </div>
-            </DataListCell>,
-          ]}
-        />
-      </DataListItemRow>
-      <DataListContent
-        aria-label="Activity Item"
-        id="activity-item"
-        isHidden={!rowExpanded}
+    return (
+      <DataListItem
+        aria-labelledby="activity item"
+        isExpanded={rowExpanded}
+        className={'integration-detail-activity-item'}
+        data-testid={'integration-detail-activity-item'}
       >
-        {steps ? (
-          <Table
-            aria-label={'integration detail activity item table'}
-            cells={columns}
-            rows={rows()}
-            variant={TableVariant.compact}
-          >
-            <TableHeader />
-            <TableBody />
-          </Table>
-        ) : (
-          <span>{i18nNoSteps}</span>
-        )}
-      </DataListContent>
-    </DataListItem>
-  );
-};
+        <DataListItemRow>
+          <DataListToggle
+            onClick={doExpand}
+            isExpanded={rowExpanded}
+            id="activity-item-toggle"
+            data-testid={'integration-detail-activity-item-toggle'}
+          />
+          <DataListItemCells
+            dataListCells={[
+              <DataListCell
+                id="activity-date"
+                data-testid={'integration-detail-activity-item-date'}
+                key="date"
+              >
+                {date}
+              </DataListCell>,
+              <DataListCell
+                id="activity-time"
+                data-testid={'integration-detail-activity-item-time'}
+                key="time"
+              >
+                {time}
+              </DataListCell>,
+              <DataListCell
+                id="activity-version"
+                data-testid={'integration-detail-activity-item-version'}
+                key="version"
+              >
+                {i18nVersion}
+                &nbsp;
+                {version}
+              </DataListCell>,
+              <DataListCell
+                id="activity-errors"
+                data-testid={'integration-detail-activity-item-errors'}
+                key="errors"
+              >
+                <div
+                  className={'integration-detail-activity-item__status-item'}
+                >
+                  {errorCount > 0 ? (
+                    <>
+                      <ErrorCircleOIcon
+                        size={'sm'}
+                        color={global_danger_color_100.value}
+                      />
+                      {'  '}
+                      {i18nErrorsFound}
+                    </>
+                  ) : (
+                    <>
+                      <OkIcon
+                        size={'sm'}
+                        color={global_success_color_100.value}
+                      />
+                      {'  '}
+                      {i18nNoErrors}
+                    </>
+                  )}
+                </div>
+              </DataListCell>,
+            ]}
+          />
+        </DataListItemRow>
+        <DataListContent
+          aria-label="Activity Item"
+          id="activity-item"
+          isHidden={!rowExpanded}
+        >
+          {steps ? (
+            <Table
+              aria-label={'integration detail activity item table'}
+              cells={columns}
+              rows={rows()}
+              variant={TableVariant.compact}
+            >
+              <TableHeader />
+              <TableBody />
+            </Table>
+          ) : (
+            <span>{i18nNoSteps}</span>
+          )}
+        </DataListContent>
+      </DataListItem>
+    );
+  };
