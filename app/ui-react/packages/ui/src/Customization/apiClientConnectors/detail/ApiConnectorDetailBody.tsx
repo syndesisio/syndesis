028aff4c762db6ba583eef8b774d908ab19d1753
kahboom <rachelyordan@gmail.com>
2021-07-05 17:44:42 +0100

    add headingLevel to ApiConnectorDetailBody

--- app/ui-react/packages/ui/src/Customization/apiClientConnectors/detail/ApiConnectorDetailBody.tsx
+++ app/ui-react/packages/ui/src/Customization/apiClientConnectors/detail/ApiConnectorDetailBody.tsx
@@ -63,112 +63,121 @@ export interface IApiConnectorDetailBodyProps {
   propertyKeys: string[];
 }
 
-export const ApiConnectorDetailBody: React.FunctionComponent<IApiConnectorDetailBodyProps> = ({
-  address,
-  basePath,
-  description,
-  handleSubmit,
-  host,
-  i18nLabelAddress,
-  i18nLabelBaseUrl,
-  i18nLabelDescription,
-  i18nLabelHost,
-  i18nLabelName,
-  i18nCancelLabel,
-  i18nEditLabel,
-  i18nNameHelper,
-  i18nRequiredText,
-  i18nSaveLabel,
-  i18nTitle,
-  icon,
-  name,
-  propertyKeys,
-}) => {
-  const [configured, setConfigured] = React.useState<IApiConnectorDetailValues>(
-    { address, basePath, description, host, icon, name }
-  );
-  const [isEditing, setIsEditing] = React.useState(false);
+export const ApiConnectorDetailBody: React.FunctionComponent<IApiConnectorDetailBodyProps> =
+  ({
+    address,
+    basePath,
+    description,
+    handleSubmit,
+    host,
+    i18nLabelAddress,
+    i18nLabelBaseUrl,
+    i18nLabelDescription,
+    i18nLabelHost,
+    i18nLabelName,
+    i18nCancelLabel,
+    i18nEditLabel,
+    i18nNameHelper,
+    i18nRequiredText,
+    i18nSaveLabel,
+    i18nTitle,
+    icon,
+    name,
+    propertyKeys,
+  }) => {
+    const [configured, setConfigured] =
+      React.useState<IApiConnectorDetailValues>({
+        address,
+        basePath,
+        description,
+        host,
+        icon,
+        name,
+      });
+    const [isEditing, setIsEditing] = React.useState(false);
 
-  const onCancelEditing = () => {
-    setConfigured({ address, basePath, description, host, icon, name });
-    setIsEditing(false);
-  };
+    const onCancelEditing = () => {
+      setConfigured({ address, basePath, description, host, icon, name });
+      setIsEditing(false);
+    };
 
-  const onHandleChange = (fieldName: string, value: string) => {
-    setConfigured({ ...configured, [fieldName]: value });
-  };
+    const onHandleChange = (fieldName: string, value: string) => {
+      setConfigured({ ...configured, [fieldName]: value });
+    };
 
-  const onStartEditing = () => {
-    setIsEditing(true);
-  };
+    const onStartEditing = () => {
+      setIsEditing(true);
+    };
 
-  const onSubmit = () => {
-    setIsEditing(false);
-    handleSubmit(configured);
-  };
+    const onSubmit = () => {
+      setIsEditing(false);
+      handleSubmit(configured);
+    };
 
-  return (
-    <PageSection data-testid={'api-connector-detail-body'}>
-      <Card>
-        <CardHeader>
-          <Title size={'2xl'}>{i18nTitle}</Title>
-        </CardHeader>
-        <CardBody>
-          {isEditing ? (
-            <ApiConnectorDetailConfigEdit
-              handleOnChange={onHandleChange}
-              i18nLabelAddress={i18nLabelAddress}
-              i18nLabelBaseUrl={i18nLabelBaseUrl}
-              i18nLabelDescription={i18nLabelDescription}
-              i18nLabelHost={i18nLabelHost}
-              i18nLabelName={i18nLabelName}
-              i18nNameHelper={i18nNameHelper}
-              i18nRequiredText={i18nRequiredText}
-              properties={configured}
-              propertyKeys={propertyKeys}
-            />
-          ) : (
-            <ApiConnectorDetailConfig
-              i18nLabelAddress={i18nLabelAddress}
-              i18nLabelBaseUrl={i18nLabelBaseUrl}
-              i18nLabelDescription={i18nLabelDescription}
-              i18nLabelHost={i18nLabelHost}
-              i18nLabelName={i18nLabelName}
-              properties={configured}
-              propertyKeys={propertyKeys}
-            />
-          )}
-        </CardBody>
-        <CardFooter>
-          {isEditing ? (
-            <>
+    return (
+      <PageSection data-testid={'api-connector-detail-body'}>
+        <Card>
+          <CardHeader>
+            <Title size={'2xl'} headingLevel={'h2'}>
+              {i18nTitle}
+            </Title>
+          </CardHeader>
+          <CardBody>
+            {isEditing ? (
+              <ApiConnectorDetailConfigEdit
+                handleOnChange={onHandleChange}
+                i18nLabelAddress={i18nLabelAddress}
+                i18nLabelBaseUrl={i18nLabelBaseUrl}
+                i18nLabelDescription={i18nLabelDescription}
+                i18nLabelHost={i18nLabelHost}
+                i18nLabelName={i18nLabelName}
+                i18nNameHelper={i18nNameHelper}
+                i18nRequiredText={i18nRequiredText}
+                properties={configured}
+                propertyKeys={propertyKeys}
+              />
+            ) : (
+              <ApiConnectorDetailConfig
+                i18nLabelAddress={i18nLabelAddress}
+                i18nLabelBaseUrl={i18nLabelBaseUrl}
+                i18nLabelDescription={i18nLabelDescription}
+                i18nLabelHost={i18nLabelHost}
+                i18nLabelName={i18nLabelName}
+                properties={configured}
+                propertyKeys={propertyKeys}
+              />
+            )}
+          </CardBody>
+          <CardFooter>
+            {isEditing ? (
+              <>
+                <Button
+                  data-testid={'connector-details-form-save-button'}
+                  variant={'primary'}
+                  isDisabled={!configured.name}
+                  onClick={onSubmit}
+                >
+                  {i18nSaveLabel}
+                </Button>{' '}
+                <Button
+                  data-testid={'connector-details-form-cancel-button'}
+                  variant={'secondary'}
+                  onClick={onCancelEditing}
+                >
+                  {i18nCancelLabel}
+                </Button>
+              </>
+            ) : (
               <Button
-                data-testid={'connector-details-form-save-button'}
+                data-testid={'connector-details-form-edit-button'}
                 variant={'primary'}
-                isDisabled={!configured.name}
-                onClick={onSubmit}
+                onClick={onStartEditing}
               >
-                {i18nSaveLabel}
-              </Button>{' '}
-              <Button
-                data-testid={'connector-details-form-cancel-button'}
-                variant={'secondary'}
-                onClick={onCancelEditing}
-              >
-                {i18nCancelLabel}
+                {i18nEditLabel}
               </Button>
-            </>
-          ) : (
-            <Button
-              data-testid={'connector-details-form-edit-button'}
-              variant={'primary'}
-              onClick={onStartEditing}
-            >
-              {i18nEditLabel}
-            </Button>
-          )}
-        </CardFooter>
-      </Card>
-    </PageSection>
-  );
-};
+            )}
+          </CardFooter>
+        </Card>
+      </PageSection>
+    );
+  };
