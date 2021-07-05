98049aa39b45d821c0d268cd10919c8aee12c4d7
kahboom <rachelyordan@gmail.com>
2021-07-05 15:45:31 +0100

    gutter->hasGutter bool for Int Detail History List

--- app/ui-react/packages/ui/src/Integration/Details/IntegrationDetailHistoryListView.tsx
+++ app/ui-react/packages/ui/src/Integration/Details/IntegrationDetailHistoryListView.tsx
@@ -25,67 +25,67 @@ export interface IIntegrationDetailHistoryListViewProps {
   publishLabel?: string | JSX.Element;
 }
 
-export const IntegrationDetailHistoryListView: React.FunctionComponent<IIntegrationDetailHistoryListViewProps> = ({
-  children,
-  editHref,
-  editLabel,
-  hasHistory,
-  isDraft,
-  i18nTextDraft,
-  i18nTextHistory,
-  publishAction,
-  publishHref,
-  publishLabel,
-}) => (
-  <PageSection>
-    <Stack gutter={'md'}>
-      {isDraft && (
-        <StackItem>
-          <Card>
-            <CardBody>
-              <Split gutter={'md'}>
-                <SplitItem>
-                  <Title headingLevel="h5" size={'md'}>
-                    {i18nTextDraft}
-                  </Title>
-                </SplitItem>
-                <SplitItem isFilled={true}>&nbsp;</SplitItem>
-                <SplitItem>
-                  <ButtonLink
-                    data-testid={
-                      'integration-detail-history-list-view-publish-button'
-                    }
-                    to={publishHref}
-                    onClick={publishAction}
-                    children={publishLabel}
-                  />
-                  &nbsp;
-                  &nbsp;
-                  <ButtonLink
-                    data-testid={
-                      'integration-detail-history-list-view-edit-button'
-                    }
-                    href={editHref}
-                    children={editLabel}
-                  />
-                </SplitItem>
-              </Split>
-            </CardBody>
-          </Card>
-        </StackItem>
-      )}
-      {children && hasHistory && (
-        <StackItem isFilled={true}>
-          <Card>
-            <CardHeader>{i18nTextHistory}</CardHeader>
-            <CardBody>
-              <DataList aria-label={'integration detail history list'}>
-                {children}
-              </DataList>
-            </CardBody>
-          </Card>
-        </StackItem>
-      )}
-    </Stack>
-  </PageSection>
-);
+export const IntegrationDetailHistoryListView: React.FunctionComponent<IIntegrationDetailHistoryListViewProps> =
+  ({
+    children,
+    editHref,
+    editLabel,
+    hasHistory,
+    isDraft,
+    i18nTextDraft,
+    i18nTextHistory,
+    publishAction,
+    publishHref,
+    publishLabel,
+  }) => (
+    <PageSection>
+      <Stack hasGutter={true}>
+        {isDraft && (
+          <StackItem>
+            <Card>
+              <CardBody>
+                <Split hasGutter={true}>
+                  <SplitItem>
+                    <Title headingLevel="h5" size={'md'}>
+                      {i18nTextDraft}
+                    </Title>
+                  </SplitItem>
+                  <SplitItem isFilled={true}>&nbsp;</SplitItem>
+                  <SplitItem>
+                    <ButtonLink
+                      data-testid={
+                        'integration-detail-history-list-view-publish-button'
+                      }
+                      to={publishHref}
+                      onClick={publishAction}
+                      children={publishLabel}
+                    />
+                    &nbsp; &nbsp;
+                    <ButtonLink
+                      data-testid={
+                        'integration-detail-history-list-view-edit-button'
+                      }
+                      href={editHref}
+                      children={editLabel}
+                    />
+                  </SplitItem>
+                </Split>
+              </CardBody>
+            </Card>
+          </StackItem>
+        )}
+        {children && hasHistory && (
+          <StackItem isFilled={true}>
+            <Card>
+              <CardHeader>{i18nTextHistory}</CardHeader>
+              <CardBody>
+                <DataList aria-label={'integration detail history list'}>
+                  {children}
+                </DataList>
+              </CardBody>
+            </Card>
+          </StackItem>
+        )}
+      </Stack>
+    </PageSection>
+  );
