8071af4197b623a6b16db3e08653e9d4bb1c5fe6
kahboom <rachelyordan@gmail.com>
2021-07-05 17:25:48 +0100

    add headingLevel to Title for ExtensionListView

--- app/ui-react/packages/ui/src/Customization/extensions/ExtensionListView.tsx
+++ app/ui-react/packages/ui/src/Customization/extensions/ExtensionListView.tsx
@@ -6,7 +6,7 @@ import {
   EmptyStateVariant,
   Text,
   Title,
-  Tooltip
+  Tooltip,
 } from '@patternfly/react-core';
 import { AddCircleOIcon } from '@patternfly/react-icons';
 import * as H from '@syndesis/history';
@@ -26,8 +26,8 @@ export interface IExtensionListViewProps extends IListViewToolbarProps {
   linkImportExtension: H.LocationDescriptor;
 }
 
-export const ExtensionListView: React.FunctionComponent<IExtensionListViewProps> = (
-  {
+export const ExtensionListView: React.FunctionComponent<IExtensionListViewProps> =
+  ({
     children,
     i18nDescription,
     i18nEmptyStateInfo,
@@ -39,52 +39,59 @@ export const ExtensionListView: React.FunctionComponent<IExtensionListViewProps>
     i18nTitle,
     linkImportExtension,
     ...rest
-  }
-) => {
-  return (
-    <PageSection>
-      <ListViewToolbar {...rest}>
-        <div className={'form-group'}>
-          <ButtonLink data-testid={'extension-list-view-import-button'}
-                      href={linkImportExtension}
-                      as={'primary'}>
-            {i18nLinkImportExtension}
-          </ButtonLink>
-        </div>
-      </ListViewToolbar>
-      {i18nTitle !== '' && <Title size={'lg'}>{i18nTitle}</Title>}
-      {i18nDescription !== '' && (
-        <Text dangerouslySetInnerHTML={{ __html: i18nDescription }}/>
-      )}
-      {children ? (
-        <DataList aria-label={'extension list'}>{children}</DataList>
-      ) : (
-        <EmptyState variant={EmptyStateVariant.full}>
-          <EmptyStateIcon icon={AddCircleOIcon}/>
-          <Title headingLevel={'h5'} size={'lg'}>
-            {i18nEmptyStateTitle}
+  }) => {
+    return (
+      <PageSection>
+        <ListViewToolbar {...rest}>
+          <div className={'form-group'}>
+            <ButtonLink
+              data-testid={'extension-list-view-import-button'}
+              href={linkImportExtension}
+              as={'primary'}
+            >
+              {i18nLinkImportExtension}
+            </ButtonLink>
+          </div>
+        </ListViewToolbar>
+        {i18nTitle !== '' && (
+          <Title size={'lg'} headingLevel={'h1'}>
+            {i18nTitle}
           </Title>
-          <EmptyStateBody>{i18nEmptyStateInfo}</EmptyStateBody>
-          <Tooltip
-            position={'top'}
-            content={
-              i18nLinkImportExtensionTip
-                ? i18nLinkImportExtensionTip
-                : i18nLinkImportExtension
-            }
-            enableFlip={true}
-          >
-            <>
-              <br/>
-              <ButtonLink data-testid={'extension-list-view-empty-state-import-button'}
-                          href={linkImportExtension}
-                          as={'primary'}>
-                {i18nLinkImportExtension}
-              </ButtonLink>
-            </>
-          </Tooltip>
-        </EmptyState>
-      )}
-    </PageSection>
-  );
-};
+        )}
+        {i18nDescription !== '' && (
+          <Text dangerouslySetInnerHTML={{ __html: i18nDescription }} />
+        )}
+        {children ? (
+          <DataList aria-label={'extension list'}>{children}</DataList>
+        ) : (
+          <EmptyState variant={EmptyStateVariant.full}>
+            <EmptyStateIcon icon={AddCircleOIcon} />
+            <Title headingLevel={'h5'} size={'lg'}>
+              {i18nEmptyStateTitle}
+            </Title>
+            <EmptyStateBody>{i18nEmptyStateInfo}</EmptyStateBody>
+            <Tooltip
+              position={'top'}
+              content={
+                i18nLinkImportExtensionTip
+                  ? i18nLinkImportExtensionTip
+                  : i18nLinkImportExtension
+              }
+              enableFlip={true}
+            >
+              <>
+                <br />
+                <ButtonLink
+                  data-testid={'extension-list-view-empty-state-import-button'}
+                  href={linkImportExtension}
+                  as={'primary'}
+                >
+                  {i18nLinkImportExtension}
+                </ButtonLink>
+              </>
+            </Tooltip>
+          </EmptyState>
+        )}
+      </PageSection>
+    );
+  };
