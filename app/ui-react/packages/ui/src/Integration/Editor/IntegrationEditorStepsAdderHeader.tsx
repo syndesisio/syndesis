20edc2bc55b9aa97c682f9a5561e70bb57e118c4
kahboom <rachelyordan@gmail.com>
2021-07-05 17:02:24 +0100

    rm TitleLevel from IntEditor StepsAdderHeader

--- app/ui-react/packages/ui/src/Integration/Editor/IntegrationEditorStepsAdderHeader.tsx
+++ app/ui-react/packages/ui/src/Integration/Editor/IntegrationEditorStepsAdderHeader.tsx
@@ -1,13 +1,15 @@
-import { Text, TextContent, Title, TitleLevel } from '@patternfly/react-core';
+import { Text, TextContent, Title } from '@patternfly/react-core';
 import * as React from 'react';
 
-export const IntegrationEditorStepsAdderHeader: React.FunctionComponent = () => (
-  <TextContent>
-    <Title size={'2xl'} headingLevel={TitleLevel.h1}>
-      Add to Integration
-    </Title>
-    <Text>
-      You can continue adding steps and connections to your integration as well.
-    </Text>
-  </TextContent>
-);
+export const IntegrationEditorStepsAdderHeader: React.FunctionComponent =
+  () => (
+    <TextContent>
+      <Title size={'2xl'} headingLevel={'h1'}>
+        Add to Integration
+      </Title>
+      <Text>
+        You can continue adding steps and connections to your integration as
+        well.
+      </Text>
+    </TextContent>
+  );
