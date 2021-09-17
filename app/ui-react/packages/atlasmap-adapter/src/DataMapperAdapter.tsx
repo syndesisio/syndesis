d73046d605a35ab86a4c94cf98dff58c5f4365fe
Michael Spaxman <seanforyou23@gmail.com>
2021-09-17 17:05:57 -0400

    [cleanup] import atlasmap base styles, use better logLevel

--- app/ui-react/packages/atlasmap-adapter/src/DataMapperAdapter.tsx
+++ app/ui-react/packages/atlasmap-adapter/src/DataMapperAdapter.tsx
@@ -76,7 +76,7 @@ export const DataMapperAdapter: React.FunctionComponent<IDataMapperAdapterProps>
     console.log('Atlasmap document', JSON.stringify(externalDocument));
     return (
       <AtlasmapProvider
-        logLevel={'info'}
+        logLevel={'warn'}
         baseJSONInspectionServiceUrl={baseJSONInspectionServiceUrl}
         baseJavaInspectionServiceUrl={baseJavaInspectionServiceUrl}
         baseMappingServiceUrl={baseMappingServiceUrl}
