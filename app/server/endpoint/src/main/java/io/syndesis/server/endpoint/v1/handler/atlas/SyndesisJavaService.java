be96973bff67f2a103d61d417338464dbf3992fe
Tomohisa Igarashi <tm.igarashi@gmail.com>
2021-09-17 17:59:29 -0400

    fix: AtlasMap maven classpath request endpoint has been removed

--- app/server/endpoint/src/main/java/io/syndesis/server/endpoint/v1/handler/atlas/SyndesisJavaService.java
+++ app/server/endpoint/src/main/java/io/syndesis/server/endpoint/v1/handler/atlas/SyndesisJavaService.java
@@ -16,35 +16,13 @@
 package io.syndesis.server.endpoint.v1.handler.atlas;
 
 import io.atlasmap.java.service.JavaService;
-import io.atlasmap.java.v2.MavenClasspathResponse;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.io.InputStream;
 
 @Component
 @ConditionalOnProperty(name="atlas.enabled", havingValue = "true", matchIfMissing = true)
 @Path("/atlas/java")
 public class SyndesisJavaService extends JavaService {
-
-    /**
-     * Stub out mavenclasspath processing for now.
-     */
-    @Override
-    @POST
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.APPLICATION_JSON})
-    @Path("/mavenclasspath")
-    public Response generateClasspath(InputStream request) {
-        MavenClasspathResponse response = new MavenClasspathResponse();
-        response.setExecutionTime(0L);
-        response.setClasspath("");
-        return Response.ok().entity(response).build();
-    }
 }
